95181020048b4fdd20001e4eea71b8b115c1b343
Andrew Hunter <andrew@logicalshift.co.uk>
2022-04-27 00:11:33 +0100

    Add the basic code for registering a channel

--- src/scene/scene_core.rs
+++ src/scene/scene_core.rs
@@ -1,21 +1,27 @@
+use super::entity::*;
+
 use crate::error::*;
 use crate::entity_id::*;
+use crate::entity_channel::*;
 use crate::message::*;
 
 use futures::prelude::*;
 use futures::stream::{BoxStream};
 
+use std::collections::{HashMap};
+
 ///
 /// The scene core represents the state shared between all entities in a scene
 ///
 pub struct SceneCore {
-
+    /// The entities that are available in this core
+    entities: HashMap<EntityId, Entity>,
 }
 
 impl Default for SceneCore {
     fn default() -> SceneCore {
         SceneCore {
-
+            entities: HashMap::new(),
         }
     }
 }
@@ -26,11 +32,23 @@ impl SceneCore {
     ///
     pub fn create_entity<TMessage, TResponse, TFn, TFnFuture>(&mut self, entity_id: EntityId, runtime: TFn) -> Result<(), CreateEntityError>
     where
-        TMessage:   Send,
-        TResponse:  Send,
+        TMessage:   'static + Send,
+        TResponse:  'static + Send,
         TFn:        Send + FnOnce(BoxStream<'static, Message<TMessage, TResponse>>) -> TFnFuture,
         TFnFuture:  Send + Future<Output = ()>,
     {
+        // Create the entity representation for this item
+        let (channel, receiver) = EntityChannel::new(5);
+        let entity              = self.entities.entry(entity_id).or_insert_with(|| Entity::default());
+
+        entity.register_channel(channel)?;
+
+        // Start the future running
+        let future              = runtime(receiver.boxed());
+        let future              = future.boxed();
+
+        // TODO: Queue a request in the runtime that we will run the entity
+
         todo!()
     }
 
