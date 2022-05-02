01c0bee16c466b3040d9425df7727f39d8c0eeca
Andrew Hunter <andrew@logicalshift.co.uk>
2022-05-02 12:32:42 +0100

    Add a way to create an Any channel from an entity core

--- src/scene/entity_core.rs
+++ src/scene/entity_core.rs
@@ -1,3 +1,4 @@
+use crate::any_entity_channel::*;
 use crate::simple_entity_channel::*;
 
 use ::desync::scheduler::*;
@@ -9,7 +10,10 @@ use std::any::{Any, TypeId};
 /// Stores the data associated with an entity
 ///
 pub struct EntityCore {
-    /// The channel for sending requests to this entity, stored in an 'Any' box
+    /// A conversion channel, which has the same response type but the message type is `Box<dyn Any + Send>`. This is of type `BoxedMessageChannel<TResponse>`
+    create_conversion_channel: Box<dyn Send + Fn() -> AnyEntityChannel>,
+
+    /// The channel for sending requests to this entity, stored in an 'Any' box. This is of type `SimpleEntityChannel<TMessage, TResponse>`
     channel: Box<dyn Send + Any>,
 
     /// The queue used for running the entity (this runs the entities main future)
@@ -28,10 +32,14 @@ impl EntityCore {
         TMessage:   'static + Send,
         TResponse:  'static + Send,
     {
+        let conversion_channel          = channel.clone();
+        let create_conversion_channel   = move || { AnyEntityChannel::from_channel(conversion_channel.clone()) };
+
         EntityCore {
-            channel:            Box::new(channel),
-            queue:              scheduler().create_job_queue(),
-            message_type_id:    TypeId::of::<TMessage>(),
+            create_conversion_channel:  Box::new(create_conversion_channel),
+            channel:                    Box::new(channel),
+            queue:                      scheduler().create_job_queue(),
+            message_type_id:            TypeId::of::<TMessage>(),
         }
     }
 
@@ -65,5 +73,12 @@ impl EntityCore {
 
         // Clone it to create the channel for the receiver
         Some(channel.clone())
+    }
+
+    ///
+    /// Returns the channel with polymorphic messages and responses. The messages here unwrap to `Option<TMessage>` and `Option<TResponse>`
+    ///
+    pub fn attach_channel_any(&self) -> AnyEntityChannel {
+        (self.create_conversion_channel)()
     }
 }
