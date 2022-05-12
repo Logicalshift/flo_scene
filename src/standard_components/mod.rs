b2cc94b5fdbdf619c983dcc28470a2621e0cc6b8
Andrew Hunter <andrew@logicalshift.co.uk>
2022-05-12 10:08:33 +0100

    Add some TODOs for various plans for the library

--- src/standard_components/mod.rs
+++ src/standard_components/mod.rs
@@ -1,3 +1,13 @@
+// TODO: component to generate 'tick' messages whenever channels go from full to empty (and we're not processing a tick)
+// TODO: component to generate timed events
+// TODO: scripting component
+// TODO: HTTP server component
+// TODO: JSON streaming component
+// TODO: named pipe component (+ component to introduce the contents of a named pipe as components)
+// TODO: component to stop the scene (and other components?)
+
+// TODO: component to contain properties/bindings (not really standard as we expect the user to create this)
+
 mod entity_ids;
 mod entity_registry;
 
