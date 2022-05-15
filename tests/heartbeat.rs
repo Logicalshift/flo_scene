56d515e7e9339d88809afa87f664a9c67914d1e8
Andrew Hunter <andrew@logicalshift.co.uk>
2022-05-15 11:37:18 +0100

    Add a test of the new heartbeat service

---
+++ tests/heartbeat.rs
@@ -0,0 +1,100 @@
+use flo_scene::*;
+use flo_scene::test::*;
+
+use ::desync::*;
+use futures::prelude::*;
+use futures::channel::mpsc;
+
+#[test]
+fn open_heartbeat_channel() {
+    let scene = Scene::default();
+
+    // Create a test for this scene
+    scene.create_entity(TEST_ENTITY, move |mut msg| async move {
+        // Whenever a test is requested...
+        while let Some(msg) = msg.next().await {
+            let msg: Message<(), Vec<SceneTestResult>> = msg;
+
+            // Try to open the channel to the heartbeat entity and ensure that it's there
+            let channel = scene_send_to::<HeartbeatRequest, ()>(HEARTBEAT);
+
+            if channel.is_ok() {
+                msg.respond(vec![SceneTestResult::Ok]).ok();
+            } else {
+                msg.respond(vec![SceneTestResult::FailedWithMessage(format!("{:?}", channel.err()))]).ok();
+            }
+        }
+    }).unwrap();
+
+    // Test the scene we just set up
+    test_scene(scene);
+}
+
+#[test]
+fn receive_heartbeat_after_message() {
+    let scene               = Scene::default();
+    let receive_heartbeat   = EntityId::new();
+
+    // Receive_heartbeat either receives a heartbeat or a message
+    #[derive(Copy, Clone, PartialEq, Debug)]
+    enum TestRequest {
+        Message,
+        Heartbeat,
+    }
+
+    impl From<Heartbeat> for TestRequest {
+        fn from(_: Heartbeat) -> TestRequest {
+            TestRequest::Heartbeat
+        }
+    }
+
+    // Create an entity that forwards its requests to another stream
+    let (sender, receiver) = mpsc::channel(100);
+    scene.create_entity(receive_heartbeat, |mut msg| async move {
+        let mut sender = sender;
+
+        while let Some(msg) = msg.next().await {
+            let msg: Message<TestRequest, ()> = msg;
+
+            sender.send(*msg).await.ok();
+            msg.respond(()).ok();
+        }
+    }).unwrap();
+
+    // Create a test for this scene
+    scene.create_entity(TEST_ENTITY, move |mut msg| async move {
+        let mut receiver = Some(receiver);
+        let background = Desync::new(());
+
+        // Ask the heartbeat entity to send heartbeats to our test entity
+        scene_send::<_, ()>(HEARTBEAT, HeartbeatRequest::RequestHeartbeat(receive_heartbeat)).await.unwrap();
+
+        // Whenever a test is requested...
+        while let Some(msg) = msg.next().await {
+            let msg: Message<(), Vec<SceneTestResult>> = msg;
+
+            // Send a message to the test request
+            scene_send::<_, ()>(receive_heartbeat, TestRequest::Message).await.unwrap();
+
+            // The test itself will prevent a heartbeat (as we're processing a message), so run in the background (the background thread needs to consume the receiver)
+            let mut receiver = receiver.take().unwrap();
+            background.future_desync(|_| async move {
+                // Look for a Message -> Heartbeat pattern in the result from the receiver
+                let mut received_message = false;
+                while let Some(test_request) = receiver.next().await {
+                    println!("{:?}", test_request);
+
+                    if test_request == TestRequest::Message {
+                        received_message = true;
+                    } else if test_request == TestRequest::Heartbeat && received_message {
+                        msg.respond(vec![SceneTestResult::Ok]).ok();
+                        return;
+                    }
+                }
+            }.boxed()).detach();
+        }
+    }).unwrap();
+
+    // Test the scene we just set up
+    test_scene(scene);
+}
