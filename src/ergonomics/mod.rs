f1aa505c8a47310ed209102d444e410a19aac7e9
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-04 20:06:46 +0100

    Add a placeholder for some convenience methods for creating properties

--- src/ergonomics/mod.rs
+++ src/ergonomics/mod.rs
@@ -1,5 +1,7 @@
 mod entity_channel_ext;
 
 pub use self::entity_channel_ext::*;
+#[cfg(feature="properties")] pub use self::property_bindings::*;
 
 #[cfg(feature="test-scene")] pub mod test;
+#[cfg(feature="properties")] mod property_bindings;
