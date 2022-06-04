8831202ee57eb0d766a024ec1866491acb36f263
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-04 20:09:13 +0100

    Properties is only working intermittently: add a 'race' test for the 'follow' case
    Problem seems to still be that the 'created test entity' and 'create property' messages can arrive out of order

--- tests/races.rs
+++ tests/races.rs
@@ -144,3 +144,45 @@ fn race_retrieve_existing_entities() {
         println!("*** FINISH ITER {}", i);
     }
 }
+
+#[test]
+#[cfg(feature="properties")]
+fn follow_string_property() {
+    for i in 1..1000 {
+        println!("*** ITER {}", i);
+
+        let scene = Scene::default();
+
+        // Create a test for this scene
+        scene.create_entity(TEST_ENTITY, move |_context, mut msg| async move {
+            // Whenever a test is requested...
+            while let Some(msg) = msg.next().await {
+                let msg: Message<(), Vec<SceneTestResult>> = msg;
+
+                // Create a channel to the properties object
+                let mut channel                         = properties_channel::<String>(PROPERTIES, &SceneContext::current()).await.unwrap();
+
+                // Create a string property
+                let (string_sender, string_receiver)    = mpsc::channel(5);
+                let (string_sink, string_stream)        = property_stream();
+                channel.send_without_waiting(PropertyRequest::CreateProperty(PropertyDefinition::new(TEST_ENTITY, "TestString", string_receiver.boxed()))).await.unwrap();
+                channel.send_without_waiting(PropertyRequest::Follow(PropertyReference::new(TEST_ENTITY, "TestString"), string_sink)).await.unwrap();
+
+                // If we send a value to the property, it should show up on the property stream
+                let mut string_sender   = string_sender;
+                string_sender.send("Test".to_string()).await.unwrap();
+
+                let mut string_stream   = string_stream;
+                let set_value           = string_stream.next().await;
+                println!("  Received {:?}", set_value);
+
+                msg.respond(vec![
+                    (set_value == Some("Test".to_string())).into()
+                ]).ok();
+            }
+        }).unwrap();
+
+        // Test the scene we just set up
+        test_scene(scene);
+    }
+}
