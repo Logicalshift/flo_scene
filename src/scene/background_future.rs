46d583f4a731d24efd0c83579840a4d86c8f3364
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-08 09:45:03 +0100

    In BackgroundFuture::poll, loop until all the futures are asleep, in case they wake each other up

--- src/scene/background_future.rs
+++ src/scene/background_future.rs
@@ -93,71 +93,78 @@ impl Future for BackgroundFuture {
     type Output = ();
 
     fn poll(mut self: Pin<&mut Self>, context: &mut Context) -> Poll<()> {
-        let awake_futures = {
-            // Some weird rust lifetime stuff: partial borrows won't work in a pin, so we need to create a direct self reference
-            // ... then we need partial borrows of two fields within self (core and futures) to perform this operation
-            let also_self       = &mut *self;
-            let self_futures    = &mut also_self.futures;
-            let self_core       = &also_self.core;
-            let mut core        = self_core.lock().unwrap();
+        loop {
+            let awake_futures = {
+                // Some weird rust lifetime stuff: partial borrows won't work in a pin, so we need to create a direct self reference
+                // ... then we need partial borrows of two fields within self (core and futures) to perform this operation
+                let also_self       = &mut *self;
+                let self_futures    = &mut also_self.futures;
+                let self_core       = &also_self.core;
+                let mut core        = self_core.lock().unwrap();
 
-            // Update the waker so next time something needs to wake us up, poll will be called again
-            core.waker = Some(context.waker().clone());
+                // Update the waker so next time something needs to wake us up, poll will be called again
+                core.waker = Some(context.waker().clone());
 
-            // Terminate all of the futures if the core is stopped
-            if core.stopped {
-                // Clear any futures from this item
-                self_futures.iter_mut().for_each(|future| *future = None);
+                // Terminate all of the futures if the core is stopped
+                if core.stopped {
+                    // Clear any futures from this item
+                    self_futures.iter_mut().for_each(|future| *future = None);
 
-                // No more work to do (we don't poll any futures any more)
-                return Poll::Ready(());
-            }
-
-            if core.new_futures {
-                // Extend the futures in this struct so it's the same length as the core
-                while self_futures.len() < core.futures.len() {
-                    self_futures.push(None);
+                    // No more work to do (we don't poll any futures any more)
+                    return Poll::Ready(());
                 }
 
-                // Load futures from the core
-                for (idx, future) in core.futures.iter_mut().enumerate() {
-                    if let CoreFutureState::NewFuture(_) = future {
-                        // Swap the future out of the array (marking it as 'active' so a new future won't get written to this slot)
-                        let mut new_future = CoreFutureState::Active;
-                        mem::swap(&mut new_future, future);
+                if core.new_futures {
+                    // Extend the futures in this struct so it's the same length as the core
+                    while self_futures.len() < core.futures.len() {
+                        self_futures.push(None);
+                    }
 
-                        // Load into the list of futures to poll
-                        if let CoreFutureState::NewFuture(new_future) = new_future {
-                            (*self_futures)[idx] = Some(new_future);
+                    // Load futures from the core
+                    for (idx, future) in core.futures.iter_mut().enumerate() {
+                        if let CoreFutureState::NewFuture(_) = future {
+                            // Swap the future out of the array (marking it as 'active' so a new future won't get written to this slot)
+                            let mut new_future = CoreFutureState::Active;
+                            mem::swap(&mut new_future, future);
+
+                            // Load into the list of futures to poll
+                            if let CoreFutureState::NewFuture(new_future) = new_future {
+                                (*self_futures)[idx] = Some(new_future);
+                            }
                         }
                     }
+
+                    // No more new futures
+                    core.new_futures = false;
                 }
 
-                // No more new futures
-                core.new_futures = false;
+                // Load the list of awake futures from the core
+                core.awake_futures.drain().collect::<Vec<_>>()
+            };
+
+            // Stop once there are no awake futures left
+            if awake_futures.is_empty() {
+                break;
             }
 
-            // Load the list of awake futures from the core
-            core.awake_futures.drain().collect::<Vec<_>>()
-        };
+            // Poll the awake futures
+            for awake_idx in awake_futures.into_iter() {
+                // Make a context for this future to mark it as awake
+                let waker           = Arc::new(BackgroundWaker { future_idx: awake_idx, core: Arc::clone(&self.core) });
+                let future_waker    = task::waker(waker);
+                let mut context     = task::Context::from_waker(&future_waker);
 
-        // Poll the awake futures
-        for awake_idx in awake_futures.into_iter() {
-            // Make a context for this future to mark it as awake
-            let waker           = Arc::new(BackgroundWaker { future_idx: awake_idx, core: Arc::clone(&self.core) });
-            let future_waker    = task::waker(waker);
-            let mut context     = task::Context::from_waker(&future_waker);
+                if let Some(future) = &mut self.futures[awake_idx] {
+                    // Poll the future
+                    match future.poll_unpin(&mut context) {
+                        Poll::Pending   => { }
+                        Poll::Ready(()) => {
+                            // Release the future
+                            self.futures[awake_idx] = None;
 
-            if let Some(future) = &mut self.futures[awake_idx] {
-                // Poll the future
-                match future.poll_unpin(&mut context) {
-                    Poll::Pending   => { }
-                    Poll::Ready(()) => {
-                        // Release the future
-                        self.futures[awake_idx] = None;
-
-                        // Mark the future as 'available' in the core
-                        self.core.lock().unwrap().futures[awake_idx] = CoreFutureState::Unused;
+                            // Mark the future as 'available' in the core
+                            self.core.lock().unwrap().futures[awake_idx] = CoreFutureState::Unused;
+                        }
                     }
                 }
             }
