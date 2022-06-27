1e67423ebc9592bfc838f00769d3325c2dca5dff
Andrew Hunter <andrew@logicalshift.co.uk>
2022-06-27 20:39:37 +0100

    Test run_in_background using the context value passed in to the entity

--- tests/run_in_background.rs
+++ tests/run_in_background.rs
@@ -52,6 +52,52 @@ fn say_hello_in_background() {
 }
 
 #[test]
+fn say_hello_in_background_using_context() {
+    let scene                                       = Scene::empty();
+    let hello_entity                                = EntityId::new();
+    let (mut string_sender, mut string_receiver)    = mpsc::channel(5);
+    let (mut relay_sender, mut relay_receiver)      = mpsc::channel(5);
+
+    // Create an entity that monitors string_receiver in the background
+    scene.create_entity(hello_entity, |context, mut msg| async move {
+        context.run_in_background(async move {
+            while let Some(string) = string_receiver.next().await {
+                relay_sender.send(string).await.ok();
+            }
+        }).unwrap();
+
+        // Messages don't really matter here
+        while let Some(msg) = msg.next().await {
+            let msg: Message<String, String> = msg;
+
+            msg.respond("???".to_string()).ok();
+        }
+    }).unwrap();
+
+    // Create a test for this scene
+    scene.create_entity(TEST_ENTITY, move |_context, mut msg| async move {
+        // Whenever a test is requested...
+        while let Some(msg) = msg.next().await {
+            let msg: Message<(), Vec<SceneTestResult>> = msg;
+
+            // Send a message to the background future
+            string_sender.send("Hello".to_string()).await.ok();
+
+            // Should receive another message from the receiver
+            let received = relay_receiver.next().await;
+
+            // Wait for the response, and succeed if the result is 'world'
+            msg.respond(vec![
+                (received == Some("Hello".to_string())).into()
+            ]).unwrap();
+        }
+    }).unwrap();
+
+    // Test the scene we just set up
+    test_scene(scene);
+}
+
+#[test]
 fn background_has_current_scene() {
     let scene   = Scene::empty();
 
