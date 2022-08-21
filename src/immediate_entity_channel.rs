12110c5e0a892a0b4548d589f15b7a1111a7e233
Andrew Hunter <andrew@logicalshift.co.uk>
2022-08-21 19:11:55 +0100

    Just implement 'send immediate' version of the 'send message' code for the entity channel
    This is slightly simpler, but will only support the simpler more common 'response ignored' type of message

--- src/immediate_entity_channel.rs
+++ src/immediate_entity_channel.rs
@@ -7,17 +7,12 @@ use crate::entity_channel::*;
 ///
 pub trait ImmediateEntityChannel : Send + EntityChannel {
     ///
-    /// Sends a message to the channel and retrieves the response
-    ///
-    fn send_imm(&mut self, message: Self::Message) -> Result<Self::Response, EntityChannelError>;
-
-    ///
-    /// Sends a message to a channel where we don't want to wait for a response
+    /// Sends a message to a channel immediately (blocking the current thread if the queue is full)
     ///
     /// This is most useful for cases where the response is '()' - indeed, the version in `SceneContext` only supports
     /// this version. Not waiting for a response is often a faster way to dispatch messages, and also prevents deadlocks
     /// in the event that the message triggers a callback to the original entity. This also doesn't generate an error
     /// in the event the channel drops the message without responding to it.
     ///
-    fn send_imm_without_waiting(&mut self, message: Self::Message) -> Result<(), EntityChannelError>;
+    fn send_immediate(&mut self, message: Self::Message) -> Result<(), EntityChannelError>;
 }
