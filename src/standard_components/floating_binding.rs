ce2c7e824be6e2922c7fd5f42098b1ffa9fd4e13
Andrew Hunter <andrew@logicalshift.co.uk>
2022-09-01 23:08:15 +0100

    Implement the basic trait functions

--- src/standard_components/floating_binding.rs
+++ src/standard_components/floating_binding.rs
@@ -1,6 +1,20 @@
 use flo_binding::*;
+use flo_binding::releasable::*;
+use flo_binding::binding_context::*;
 
 use std::sync::*;
+
+///
+/// A floating binding is associated with a binding target that will be bound at some unspecified point in the future
+///
+/// The design of this structure allows the binding to be used directly, or as a waypoint for retrieving the final
+/// `BindRef` either as a future or by blocking. In single-threaded contexts, the version using a future is much to
+/// be preferred.
+///
+pub struct FloatingBinding<TValue> {
+    /// The core of this binding (shared with the target)
+    core: Arc<Mutex<FloatingBindingCore<TValue>>>,
+}
 
 ///
 /// Core shared between a floating binding and its target
@@ -8,6 +22,12 @@ use std::sync::*;
 struct FloatingBindingCore<TValue> {
     /// The binding, once it has been supplied by the remote object
     binding: FloatingState<BindRef<TValue>>,
+
+    /// If there are any notifications in 'when_bound', this will pass on the notification
+    binding_watcher: Option<Box<dyn Releasable>>,
+
+    /// The actions to notify when the binding is updated
+    when_bound: Vec<ReleasableNotifiable>,
 }
 
 ///
@@ -18,7 +38,7 @@ pub enum FloatingState<TValue> {
     /// The binding has not been set by the target yet
     Waiting,
 
-    /// The target never bound the value
+    /// The target never bound the value before it was dropped
     Abandoned,
 
     /// The value was not available from the target
@@ -65,34 +85,74 @@ impl<TValue> Into<Option<TValue>> for FloatingState<TValue> {
     }
 }
 
-///
-/// A floating binding is associated with a binding target that will be bound at some unspecified point in the future
-///
-/// The design of this structure allows the binding to be used directly, or as a waypoint for retrieving the final
-/// `BindRef` either as a future or by blocking. In single-threaded contexts, the version using a future is much to
-/// be preferred.
-///
-pub struct FloatingBinding<TValue> {
-    /// The core of this binding (shared with the target)
-    core: Arc<Mutex<FloatingBindingCore<TValue>>>,
+impl<TValue> Clone for FloatingBinding<TValue> {
+    fn clone(&self) -> FloatingBinding<TValue> {
+        FloatingBinding {
+            core: Arc::clone(&self.core),
+        }
+    }
 }
 
 impl<TValue> Changeable for FloatingBinding<TValue> {
     fn when_changed(&self, what: Arc<dyn Notifiable>) -> Box<dyn Releasable> {
-        unimplemented!()
+        let mut core = self.core.lock().unwrap();
+
+        // We notify via the core if the binding has not been set, or the binding itself if it has
+        match &core.binding {
+            FloatingState::Waiting      => {
+                // The remote value hasn't arrived yet: notify via this object
+                let releasable = ReleasableNotifiable::new(what);
+                core.when_bound.push(releasable.clone_as_owned());
+
+                Box::new(releasable)
+            },
+
+            FloatingState::Value(value) => {
+                // Remote value is available: notify from here
+                value.when_changed(what)
+            }
+
+            _                           => { 
+                // Error state: we won't notify at all
+                Box::new(ReleasableNotifiable::new(what))
+            }
+        }
     }
 }
 
-impl<TValue> Bound for FloatingBinding<TValue> {
+impl<TValue> Bound for FloatingBinding<TValue> 
+where
+    TValue: 'static,
+{
     type Value = FloatingState<TValue>;
 
     #[inline]
     fn get(&self) -> Self::Value {
-        unimplemented!()
+        // Fetch the value contained within the core (don't keep the lock in case of re-entrancy)
+        let core_value = { self.core.lock().unwrap().binding.clone() };
+
+        // Fetch the contents of the bindref
+        match core_value {
+            FloatingState::Abandoned    => FloatingState::Abandoned,
+            FloatingState::Missing      => FloatingState::Missing,
+            FloatingState::Value(value) => FloatingState::Value(value.get()),
+
+            FloatingState::Waiting      => {
+                // Only add this to the binding context if the value can change and isn't a BindRef yet (if the bindref has been supplied, then it becomes the dependency)
+                BindingContext::add_dependency(self.clone());
+                FloatingState::Waiting
+            },
+        }
     }
 
     #[inline]
     fn watch(&self, what: Arc<dyn Notifiable>) -> Arc<dyn Watcher<Self::Value>> {
-        unimplemented!()
+        let mut core                = self.core.lock().unwrap();
+        let watch_binding           = self.clone();
+        let (watcher, notifiable)   = NotifyWatcher::new(move || watch_binding.get(), what);
+
+        core.when_bound.push(notifiable);
+
+        Arc::new(watcher)
     }
 }
