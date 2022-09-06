bc000bc4c7e1fcf019e58f7b0226133e5b271b3c
Andrew Hunter <andrew@logicalshift.co.uk>
2022-09-06 21:07:58 +0100

    Convert the races tests
    The new properties is failing the 'follow string property' test; it's possible to request a property before it has finished being created, it seems

--- tests/races.rs
+++ tests/races.rs
@@ -2,12 +2,10 @@ use flo_scene::*;
 use flo_scene::test::*;
 
 use futures::prelude::*;
-use futures::stream;
+use futures::channel::oneshot;
 use futures::channel::mpsc;
 
 use flo_binding::*;
-
-use std::sync::*;
 
 use std::collections::{HashSet};
 
@@ -18,34 +16,39 @@ fn race_stream_completion() {
         println!();
         println!("*** STREAM COMPLETION ITER {}", i);
 
-        let scene               = Scene::empty();
-        let stream_entity       = EntityId::new();
-        let streamed_strings    = Arc::new(Mutex::new(vec![]));
+        let scene                           = Scene::empty();
+        let hello_entity                    = EntityId::new();
+        let (hello_sender, hello_receiver)  = mpsc::channel(1);
 
-        // Create an entity that receives a stream of strings and stores them in streamed_strings
-        let store_strings = Arc::clone(&streamed_strings);
-        scene.create_stream_entity(stream_entity, StreamEntityResponseStyle::RespondAfterProcessing, move |_context, mut strings| async move {
-            while let Some(string) = strings.next().await {
-                store_strings.lock().unwrap().push(string);
+        // Create an entity that says 'World' in response 'Hello'
+        scene.create_entity(hello_entity, |_context, mut msg| async move {
+            let mut hello_sender = hello_sender;
+
+            while let Some(msg) = msg.next().await {
+                let msg: String = msg;
+
+                if msg == "Hello".to_string() {
+                    hello_sender.send("World".to_string()).await.unwrap();
+                } else {
+                    hello_sender.send("???".to_string()).await.unwrap();
+                }
             }
         }).unwrap();
 
-        // Test sends a couple of strings and then reads them back again
-        scene.create_entity(TEST_ENTITY, move |_context, mut messages| async move {
-            while let Some(msg) = messages.next().await {
-                let msg: Message<(), Vec<SceneTestResult>> = msg;
+        // Create a test for this scene
+        scene.create_entity(TEST_ENTITY, move |_context, mut msg| async move {
+            let mut hello_receiver = hello_receiver;
 
-                // Stream in some stirngs
-                scene_send_stream(stream_entity, stream::iter(vec!["Hello".to_string(), "World".to_string()])).unwrap().await;
+            // Whenever a test is requested...
+            while let Some(msg) = msg.next().await {
+                let SceneTestRequest(mut msg) = msg;
 
-                // Re-read them from the store
-                let strings: Vec<String> = streamed_strings.lock().unwrap().clone();
+                // Send a 'Hello' message in response
+                scene_send_without_waiting(hello_entity, "Hello".to_string()).await.unwrap();
+                let world = hello_receiver.next().await.unwrap();
 
-                if strings == vec!["Hello".to_string(), "World".to_string()] {
-                    msg.respond(vec![SceneTestResult::Ok]).unwrap();
-                } else {
-                    msg.respond(vec![SceneTestResult::FailedWithMessage(format!("Strings retrieved: {:?}", strings))]).unwrap();
-                }
+                // Wait for the response, and succeed if the result is 'world'
+                msg.send_without_waiting((world == "World".to_string()).into()).await.unwrap();
             }
         }).unwrap();
 
@@ -72,12 +75,12 @@ fn race_retrieve_existing_entities() {
             println!("    Hello entity starting");
 
             while let Some(msg) = msg.next().await {
-                let msg: Message<String, String> = msg;
+                let (msg, response): (String, oneshot::Sender<String>) = msg;
 
                 if *msg == "Hello".to_string() {
-                    msg.respond("World".to_string()).unwrap();
+                    response.send("World".to_string()).unwrap();
                 } else {
-                    msg.respond("???".to_string()).unwrap();
+                    response.send("???".to_string()).unwrap();
                 }
             }
         }).unwrap();
@@ -88,10 +91,9 @@ fn race_retrieve_existing_entities() {
             println!("    Add one entity starting");
 
             while let Some(msg) = msg.next().await {
-                let msg: Message<u64, u64> = msg;
-                let val = *msg;
+                let (val, response): (u64, oneshot::Sender<u64>) = msg;
 
-                msg.respond(val + 1).unwrap();
+                response.send(val + 1).unwrap();
             }
         }).unwrap();
 
@@ -102,7 +104,7 @@ fn race_retrieve_existing_entities() {
 
             // Whenever a test is requested...
             while let Some(msg) = msg.next().await {
-                let msg: Message<(), Vec<SceneTestResult>> = msg;
+                let SceneTestRequest(mut msg) = msg;
 
                 println!("  Start test...");
 
@@ -111,7 +113,7 @@ fn race_retrieve_existing_entities() {
                 let entity_monitor      = EntityId::new();
 
                 println!("  Create sender entity...");
-                scene_create_stream_entity(entity_monitor, StreamEntityResponseStyle::default(), move |_context, mut messages| async move {
+                scene_create_entity(entity_monitor, move |_context, mut messages| async move {
                     let mut sender = sender;
 
                     println!("  Sender: waiting for messages");
@@ -127,7 +129,7 @@ fn race_retrieve_existing_entities() {
                 // Ask the entity registry to monitor the entities in the scene
                 println!("  Request tracking...");
                 let entity_monitor_channel = scene_send_to(entity_monitor).unwrap();
-                scene_send::<_, ()>(ENTITY_REGISTRY, EntityRegistryRequest::TrackEntities(entity_monitor_channel)).await.unwrap();
+                scene_send_without_waiting(ENTITY_REGISTRY, EntityRegistryRequest::TrackEntities(entity_monitor_channel)).await.unwrap();
                 println!("  Tracking requested...");
 
                 // The 'hello_entity' ID should get sent back to us (pre-existing at the time tracking started)
@@ -145,7 +147,7 @@ fn race_retrieve_existing_entities() {
 
                     if received == expected {
                         // Success when we get both entities back again
-                        msg.respond(vec![SceneTestResult::Ok]).unwrap();
+                        msg.send_without_waiting(SceneTestResult::Ok).await.unwrap();
                         break;
                     }
                 }
@@ -173,12 +175,12 @@ fn race_close_entity() {
         // Create an entity that says 'World' in response 'Hello'
         scene.create_entity(hello_entity, |_context, mut msg| async move {
             while let Some(msg) = msg.next().await {
-                let msg: Message<String, String> = msg;
+                let (msg, response): (String, oneshot::Sender<String>) = msg;
 
-                if *msg == "Hello".to_string() {
-                    msg.respond("World".to_string()).unwrap();
+                if msg == "Hello".to_string() {
+                    response.send("World".to_string()).unwrap();
                 } else {
-                    msg.respond("???".to_string()).unwrap();
+                    response.send("???".to_string()).unwrap();
                 }
             }
 
@@ -192,15 +194,15 @@ fn race_close_entity() {
 
             // Whenever a test is requested...
             while let Some(msg) = msg.next().await {
-                let msg: Message<(), Vec<SceneTestResult>> = msg;
+                let SceneTestRequest(mut msg) = msg;
 
                 // Request registry updates
                 let (update_registry, registry_updates) = SimpleEntityChannel::new(TEST_ENTITY, 1000);
-                scene_send::<_, ()>(ENTITY_REGISTRY, EntityRegistryRequest::TrackEntities(update_registry.boxed())).await.unwrap();
+                scene_send_without_waiting(ENTITY_REGISTRY, EntityRegistryRequest::TrackEntities(update_registry.boxed())).await.unwrap();
 
                 // Open a channel to the entity
                 println!("  Opening channel");
-                let mut hello_channel = scene_send_to::<String, String>(hello_entity).unwrap();
+                let mut hello_channel = scene_send_to::<(String, oneshot::Sender<String>)>(hello_entity).unwrap();
 
                 // Close the entity
                 println!("  Closing entity");
@@ -208,7 +210,9 @@ fn race_close_entity() {
 
                 // Should no longer be able to send to the main channel
                 println!("  Sending test message");
-                let world = hello_channel.send("Hello".to_string()).await;
+                let (world_sender, world) = oneshot::channel();
+                let send_err    = hello_channel.send_without_waiting(("Hello".to_string(), world_sender)).await;
+                let world       = world.await;
 
                 // 'is_shutdown' should signal
                 println!("  Receiving shutdown");
@@ -219,7 +223,7 @@ fn race_close_entity() {
                 let mut registry_updates = registry_updates;
                 while let Some(msg) = registry_updates.next().await {
                     println!("    Registry update");
-                    if *msg == EntityUpdate::DestroyedEntity(hello_entity) {
+                    if msg == EntityUpdate::DestroyedEntity(hello_entity) {
                         println!("    Destroyed our entity");
                         break;
                     }
@@ -228,9 +232,8 @@ fn race_close_entity() {
                 // Wait for the response, and succeed if the result is 'world'
                 println!("Checking response ({:?})", world);
 
-                msg.respond(vec![
-                    world.is_err().into(),
-                ]).unwrap();
+                msg.send_without_waiting(send_err.is_err().into()).await.unwrap();
+                msg.send_without_waiting(world.is_err().into()).await.unwrap();
 
                 println!("Test finished");
             }
@@ -257,7 +260,7 @@ fn race_follow_string_property() {
         scene.create_entity(TEST_ENTITY, move |_context, mut msg| async move {
             // Whenever a test is requested...
             while let Some(msg) = msg.next().await {
-                let msg: Message<(), Vec<SceneTestResult>> = msg;
+                let SceneTestRequest(mut msg) = msg;
 
                 // Create a channel to the properties object
                 println!("Request properties channel");
@@ -272,7 +275,7 @@ fn race_follow_string_property() {
                 println!("Follow test entity property");
 
                 let (property_binding, target) = FloatingBinding::new();
-                channel.send(PropertyRequest::Get(PropertyReference::new(TEST_ENTITY, "TestString"), target)).await.unwrap();
+                channel.send_without_waiting(PropertyRequest::Get(PropertyReference::new(TEST_ENTITY, "TestString"), target)).await.unwrap();
                 let property_binding = property_binding.wait_for_binding().await.unwrap();
 
                 // If we send a value to the property, it should show up on the property stream
@@ -287,9 +290,7 @@ fn race_follow_string_property() {
                 let set_value           = string_stream.next().await;
                 println!("  Received {:?}", set_value);
 
-                msg.respond(vec![
-                    (set_value == Some("Test".to_string())).into()
-                ]).ok();
+                msg.send_without_waiting((set_value == Some("Test".to_string())).into()).await.ok();
             }
         }).unwrap();
 
