2b5e67735eef4465112ac4da7f42ae710fee9e16
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-13 10:36:27 +0100

    Add some simple tests for the new recipe type

---
+++ tests/recipe.rs
@@ -0,0 +1,101 @@
+use flo_scene::*;
+use flo_scene::test::*;
+
+use futures::prelude::*;
+use futures::executor;
+use uuid::*;
+
+use std::time::{Duration};
+
+pub const ECHO_ENTITY: EntityId = EntityId::well_known(uuid!["D8E25F3A-37C4-431B-B2AB-BFB3C449ECE2"]);
+
+///
+/// Request for the ECHO_ENTITY
+///
+pub enum EchoRequest {
+    Send(String),
+    Receive(BoxedEntityChannel<'static, String>),
+}
+
+///
+/// Create a scene with an ECHO_ENTITY that 
+///
+fn echo_scene() -> Scene {
+    let scene = Scene::default();
+
+    scene.create_entity(ECHO_ENTITY, |_context, mut msg| async move {
+        let mut receivers = vec![];
+
+        while let Some(msg) = msg.next().await {
+            match msg {
+                EchoRequest::Receive(channel) => {
+                    // Add a new receiver for the echo messages
+                    receivers.push(channel);
+                }
+
+                EchoRequest::Send(message) => {
+                    // Send to all channels (test entity, so we don't care about closed channels)
+                    for channel in receivers.iter_mut() {
+                        channel.send(message.clone()).await.ok();
+                    }
+                }
+            }
+        }
+    }).unwrap();
+
+    scene
+}
+
+#[test]
+pub fn complete_recipe() {
+    let scene = echo_scene();
+
+    test_scene_with_recipe(scene, Recipe::new()
+        .expect(vec![
+            "Hello".to_string(),
+            "World".to_string(),
+        ])
+        .after_sending_messages(ECHO_ENTITY,
+            |response_channel| {
+                vec![
+                    EchoRequest::Receive(response_channel),
+                    EchoRequest::Send("Hello".to_string()),
+                    EchoRequest::Send("World".to_string()),
+                ]
+            }
+        )
+    );
+}
+
+#[test]
+pub fn fail_recipe() {
+    let scene           = echo_scene();
+    let failing_recipe  = Recipe::new()
+        .expect(vec![
+            "Hello".to_string(),
+            "World".to_string(),
+        ])
+        .after_sending_messages(ECHO_ENTITY,
+            |response_channel| {
+                vec![
+                    EchoRequest::Receive(response_channel),
+                    EchoRequest::Send("Something".to_string()),
+                    EchoRequest::Send("Else".to_string()),
+                ]
+            }
+        );
+
+    let context = scene.context();
+    let result  = async move {
+        failing_recipe.run_with_timeout(context, Duration::from_secs(10)).await
+    }.boxed_local();
+
+    // Run the scene alongside the recipe
+    let scene               = scene.run().map(|_| Err(RecipeError::SceneStopped)).boxed();
+
+    let test_result         = future::select_all(vec![result, scene]);
+    let (test_result, _ ,_) = executor::block_on(test_result);
+
+    assert!(test_result.is_err());
+    assert!(test_result.unwrap_err() == RecipeError::UnexpectedResponse);
+}
