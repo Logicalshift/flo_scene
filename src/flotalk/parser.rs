367588559b5659ece96c48cdf9cf561f0c06b5ac
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-16 21:24:59 +0100

    Add a ParserResult to contain the result of parsing an expression along with any extra metadata it might have

--- src/flotalk/parser.rs
+++ src/flotalk/parser.rs
@@ -10,8 +10,23 @@ use futures::stream;
 use std::sync::*;
 
 ///
+/// A parser result
+///
+#[derive(Clone, PartialEq, Debug)]
+pub struct ParserResult<TResult> {
+    /// The parsed value
+    pub value: TResult,
+
+    /// The location where this result was generated from
+    pub location: TalkLocation,
+
+    /// The text that was matched for this result
+    pub matched: Arc<String>,
+}
+
+///
 /// Parses a flotalk stream
 ///
-pub fn parse_flotalk<'a>(input_stream: impl 'a + Send + Stream<Item=char>) -> impl 'a + Send + Stream<Item=Result<(TalkLocation, TalkExpression), (TalkLocation, TalkParseError)>> {
+pub fn parse_flotalk<'a>(input_stream: impl 'a + Send + Stream<Item=char>) -> impl 'a + Send + Stream<Item=Result<ParserResult<TalkExpression>, ParserResult<TalkParseError>>> {
     stream::empty()
 }
