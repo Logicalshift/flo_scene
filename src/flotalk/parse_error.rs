ee1736613ad0a8975af374ca2cb26c1af4a91447
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-17 00:11:49 +0100

    Some work on match_expression, plus add a match_literal function

--- src/flotalk/parse_error.rs
+++ src/flotalk/parse_error.rs
@@ -6,6 +6,12 @@ pub enum TalkParseError {
     /// A fallback error for when we don't have a specific cause of the issue
     GenericError,
 
+    /// A character was unexpected (with an optional list of expected characters at this point)
+    UnexpectedCharacter(char),
+
     /// A '"' comment had no closing '"'
     UnclosedDoubleQuoteComment,
+
+    /// Unexpected end of stream
+    ExpectedMoreCharacters,
 }
--- src/flotalk/parser.rs
+++ src/flotalk/parser.rs
@@ -6,10 +6,8 @@ use super::pushback_stream::*;
 use flo_stream::*;
 
 use futures::prelude::*;
-use futures::task::{Poll, Context};
 
 use std::sync::*;
-use std::pin::*;
 
 ///
 /// A parser result
@@ -30,6 +28,12 @@ pub struct ParserResult<TResult> {
 #[inline]
 fn is_whitespace(c: char) -> bool {
     c.is_whitespace()
+}
+
+/// True if the specified character is a number character
+#[inline]
+fn is_number(c: char) -> bool {
+    c.is_numeric()
 }
 
 impl<TStream> PushBackStream<TStream>
@@ -79,13 +83,105 @@ where
     }
 
     ///
-    /// Matches and returns the next expression on this stream
+    /// With the stream at the first character in a literal, matches and consumes that literal
+    ///
+    async fn match_literal(&mut self) -> Result<ParserResult<TalkLiteral>, ParserResult<TalkParseError>> {
+        let start_location      = self.location();
+        let mut matched         = String::new();
+
+        // Read the first character of the literal (error if we're at the end of the file)
+        let chr = self.peek().await;
+        let chr = if let Some(chr) = chr { 
+            chr
+        } else { 
+            return Err(ParserResult { value: TalkParseError::ExpectedMoreCharacters, location: start_location, matched: Arc::new(matched) }); 
+        };
+
+        // Match the literal basedon the first character
+        if chr == '[' {
+            // Block
+            todo!("Block")
+        } else if chr == '$' {
+            // Character
+            todo!("Character")
+        } else if chr == '\'' {
+            // String
+            todo!("String")
+        } else if chr == '#' {
+            // Array if #( or symbol if #' or #<alphanum>
+            todo!("Array_or_symbol")
+        } else if is_number(chr) {
+            // Number
+            todo!("Numbers")
+        } else if chr == '-' {
+            // Might be number, depends on next character
+            todo!("Negative numbers")
+        } else {
+            // Unexpected character
+            Err(ParserResult { value: TalkParseError::UnexpectedCharacter(chr), location: start_location, matched: Arc::new(matched) })
+        }
+    }
+
+    ///
+    /// Matches and returns the next expression on this stream (skipping whitespace and comments). Returns None if there are no more
+    /// expressions.
     ///
     async fn match_expression(&mut self) -> Option<Result<ParserResult<TalkExpression>, ParserResult<TalkParseError>>> {
         // Eat up as much whitespace as possible
         self.consume_whitespace().await;
 
-        None
+        // This point counts as the start of the expression
+        let start_location      = self.location();
+        let mut initial_comment = None;
+
+        loop {
+            if let Some(new_comment) = self.consume_comment().await {
+                let new_comment = match new_comment {
+                    Ok(comment) => comment,
+                    Err(err)    => return Some(Err(err))
+                };
+
+                // Amend the initial comment
+                initial_comment = match initial_comment {
+                    None                    => Some(new_comment.matched),
+                    Some(mut old_comment)   => {
+                        Arc::make_mut(&mut old_comment).push_str(&*new_comment.matched);
+                        Some(old_comment)
+                    }
+                };
+
+                // Consume whitespace following the comment
+                self.consume_whitespace().await;
+            } else {
+                // No longer at a comment
+                break;
+            }
+        }
+
+        // Decide what type of expression is following
+        let chr = self.peek().await;
+        let chr = if let Some(chr) = chr { chr } else { return None; };
+
+        let lhs = if chr == '.' {
+            // End of expression/empty expression
+            self.next().await;
+            todo!("Empty expression")
+        } else if chr == '(' {
+            // Nested expression
+            todo!("Brackets")
+        } else if chr == '|' {
+            // Variable declaration
+            todo!("Variable declaration")
+        } else {
+            // Should be a literal
+            let literal = self.match_literal().await;
+            match literal {
+                Ok(literal) => TalkExpression::Literal(literal.value),
+                Err(err)    => { return Some(Err(err)); }
+            }
+        };
+
+        todo!()
     }
 }
 
