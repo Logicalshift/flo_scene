43e3e777804cc8875842a40be467852b9f5d2e96
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-16 16:18:40 +0100

    Add a 'flotalk' module
    SmallTalk seems well suited for a scripting language but I don't think that there are any implementations that are ready for adaptation so I'm going to try implementing my own.

---
+++ src/flotalk/mod.rs
@@ -0,0 +1,6 @@
+//!
+//! # The flotalk scripting language
+//!
+//! `flotalk` is a scripting language for `flo_scene`, based on SmallTalk-80.
+//!
+
--- src/lib.rs
+++ src/lib.rs
@@ -133,3 +133,4 @@ pub use self::standard_components::*;
 #[cfg(feature="test-scene")] pub use self::ergonomics::test;
 #[cfg(feature="properties")] pub use flo_binding as binding;
 #[cfg(feature="properties")] pub use flo_rope as rope;
+#[cfg(feature="flotalk")] pub mod flotalk;
