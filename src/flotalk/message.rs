73be2714c76b48fac2eb386cf7080f53c777b32e
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-29 22:54:31 +0100

    Add a way to convert a message to its signature

--- src/flotalk/message.rs
+++ src/flotalk/message.rs
@@ -14,3 +14,22 @@ pub enum TalkMessage {
     /// A message with named arguments
     WithArguments(SmallVec<[(TalkSymbol, TalkReference); 4]>),
 }
+
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum TalkMessageSignature {
+    Unary(TalkSymbol),
+    Arguments(SmallVec<[TalkSymbol; 4]>),
+}
+
+impl TalkMessage {
+    ///
+    /// Converts a message to its signature
+    ///
+    #[inline]
+    pub fn signature(&self) -> TalkMessageSignature {
+        match self {
+            TalkMessage::Unary(symbol)          => TalkMessageSignature::Unary(*symbol),
+            TalkMessage::WithArguments(args)    => TalkMessageSignature::Arguments(args.iter().map(|(sym, _)| *sym).collect())
+        }
+    }
+}
