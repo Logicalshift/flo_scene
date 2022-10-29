3499625afe44d457ff6665c58fcb9bc65a88574f
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-29 22:54:40 +0100

    Add a 'no such message' error

--- src/flotalk/error.rs
+++ src/flotalk/error.rs
@@ -8,6 +8,9 @@ pub enum TalkError {
     /// Error with a FloTalk object
     Object(TalkReference),
 
+    /// A class message was not supported
+    MessageNotSupported,
+
     /// The runtime was dropped before a future could completed
     RuntimeDropped,
 }
