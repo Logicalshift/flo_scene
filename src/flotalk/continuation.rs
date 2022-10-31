2310d2a59f8651572e32fe86c9b9da72d67008b3
Andrew Hunter <andrew@logicalshift.co.uk>
2022-10-31 23:48:36 +0000

    Add a 'poll' function for TalkContinuation

--- src/flotalk/continuation.rs
+++ src/flotalk/continuation.rs
@@ -13,3 +13,18 @@ pub enum TalkContinuation {
     /// A value that is ready when a future completes
     Later(Box<dyn Send + FnMut(&mut TalkContext, &mut Context) -> Poll<TalkValue>>),
 }
+
+impl TalkContinuation {
+    ///
+    /// Polls this continuation for a result
+    ///
+    #[inline]
+    pub fn poll(&mut self, talk_context: &mut TalkContext, future_context: &mut Context) -> Poll<TalkValue> {
+        use TalkContinuation::*;
+
+        match self {
+            Ready(value)    => Poll::Ready(value.clone()),
+            Later(poll_fn)  => poll_fn(talk_context, future_context),
+        }
+    }
+}
