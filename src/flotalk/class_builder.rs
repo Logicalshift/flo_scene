d9f8ebf0b2cbef2bdc6752a349eebcf562ba05d6
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-03 10:04:44 +0000

    Add an initial version of a way to add an instance function
    (This approach won't quite work as we can't extend it to functions with more arguments easily)

--- src/flotalk/class_builder.rs
+++ src/flotalk/class_builder.rs
@@ -1,6 +1,7 @@
 use super::allocator::*;
 use super::class::*;
 use super::context::*;
+use super::continuation::*;
 use super::message::*;
 use super::value::*;
 
@@ -10,10 +11,95 @@ use std::collections::{HashMap};
 /// The class builder can be used to build a FloTalk class quickly from Rust
 ///
 pub struct TalkClassBuilder<TDataType> {
+    /// The name of the class that is being built
+    name: String,
+
     /// Supported instance messages
-    instance_messages:  HashMap<TalkMessageSignatureId, Box<dyn Send + Sync + Fn(&mut TalkContext, TalkMessage, &mut TDataType) -> TalkValue>>,
+    instance_messages:  HashMap<TalkMessageSignatureId, Box<dyn Send + Sync + for<'a> Fn(TalkMessage, &'a mut TDataType) -> TalkContinuation>>,
 
     /// Supported class messages
-    class_messages:     HashMap<TalkMessageSignatureId, Box<dyn Send + Sync + Fn(&mut TalkStandardAllocator<TDataType>, TalkMessage) -> TalkValue>>,
+    class_messages:     HashMap<TalkMessageSignatureId, Box<dyn Send + Sync + for<'a> Fn(&'a mut TalkStandardAllocator<TDataType>, TalkMessage) -> TalkContinuation>>,
 }
 
+///
+/// Trait implemented by things that can be converted into a class instance function
+///
+pub trait TalkIntoInstanceFn<TDataType> {
+    /// The number of arguments accepted for the message for this instance function
+    fn num_arguments(&self) -> usize;
+
+    /// Creates an instance function for this function
+    fn into_instance_fn(self) -> Box<dyn Send + Sync + for<'a> Fn(TalkMessage, &'a mut TDataType) -> TalkContinuation>;
+}
+
+///
+/// Trait implemented by things that can be converted into a class instance function
+///
+pub trait TalkIntoClassFn<TDataType> {
+    /// The number of arguments accepted for the message for this instance function
+    fn num_arguments(&self) -> usize;
+
+    /// Creates an instance function for this function
+    fn into_class_fn(self) -> Box<dyn Send + Sync + for<'a> Fn(&'a mut TalkStandardAllocator<TDataType>, TalkMessage) -> TalkContinuation>;
+}
+
+impl<TDataType> TalkClassBuilder<TDataType> {
+    ///
+    /// Begins building a class
+    ///
+    pub fn new_class(name: &str) -> TalkClassBuilder<TDataType> {
+        TalkClassBuilder {
+            name:               name.to_string(),
+            instance_messages:  HashMap::new(),
+            class_messages:     HashMap::new(),
+        }
+    }
+
+    ///
+    /// Adds an instance method to this class definition
+    ///
+    pub fn with_method(&mut self, signature: impl Into<TalkMessageSignatureId>, new_method: impl TalkIntoInstanceFn<TDataType>) -> &mut Self {
+        self.instance_messages.insert(signature.into(), new_method.into_instance_fn());
+
+        self
+    }
+}
+
+impl<TDataType, TContinuation, TFn> TalkIntoInstanceFn<TDataType> for TFn
+where
+    TFn:            'static + Send + Sync + Fn(&mut TDataType) -> TContinuation,
+    TContinuation:  Into<TalkContinuation>,
+{
+    fn num_arguments(&self) -> usize {
+        0
+    }
+
+    fn into_instance_fn(self) -> Box<dyn Send + Sync + for<'a> Fn(TalkMessage, &'a mut TDataType) -> TalkContinuation> {
+        Box::new(move |_msg, data| {
+            (self)(data).into()
+        })
+    }
+}
+
+/* -- TODO, conflicts with above but we want to extend the implementation this way
+impl<TDataType, TContinuation, TFn> TalkIntoInstanceFn<TDataType> for TFn
+where
+    TFn:            'static + Send + Sync + Fn(&mut TDataType, TalkValue) -> TContinuation,
+    TContinuation:  Into<TalkContinuation>,
+{
+    fn num_arguments(&self) -> usize {
+        1
+    }
+
+    fn into_instance_fn(self) -> Box<dyn Send + Sync + for<'a> Fn(TalkMessage, &'a mut TDataType) -> TalkContinuation> {
+        Box::new(move |msg, data| {
+            let arg = match msg {
+                TalkMessage::Unary(_)               => TalkValue::Nil,
+                TalkMessage::WithArguments(_, args) => args[0],
+            };
+
+            (self)(data, arg).into()
+        })
+    }
+}
+*/
