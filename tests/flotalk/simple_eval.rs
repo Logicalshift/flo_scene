5726026fd58453fe8f0dada8d5df7f8c352de9f0
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-10 23:17:02 +0000

    Add a test for calling a block with arguments

--- tests/flotalk/simple_eval.rs
+++ tests/flotalk/simple_eval.rs
@@ -77,3 +77,20 @@ fn call_block() {
         assert!(result == TalkValue::Int(42));
     });
 }
+
+#[test]
+fn call_block_with_arguments() {
+    let test_source     = "[ x | x ] value: 42";
+    let runtime         = TalkRuntime::empty();
+    let root_values     = vec![Arc::new(Mutex::new(TalkValueStore::default()))];
+
+    executor::block_on(async { 
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_continuation(talk_evaluate_simple(root_values, Arc::new(instructions))).await;
+
+        assert!(result == TalkValue::Int(42));
+    });
+}
