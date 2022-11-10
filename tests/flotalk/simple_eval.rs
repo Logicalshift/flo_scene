21dda112bcc54d2d16e3295cf334cd00e5e29465
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-10 22:27:18 +0000

    Add a 'call block' test
    This is probably the simplest 'real' program

--- tests/flotalk/simple_eval.rs
+++ tests/flotalk/simple_eval.rs
@@ -21,3 +21,20 @@ fn evaluate_number() {
         assert!(result == TalkValue::Int(42));
     });
 }
+
+#[test]
+fn call_block() {
+    let test_source     = "[ 42 ] value";
+    let runtime         = TalkRuntime::empty();
+    let root_values     = Arc::new(Mutex::new(TalkValueStore::default()));
+
+    executor::block_on(async { 
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_continuation(talk_evaluate_simple(root_values, Arc::new(instructions))).await;
+
+        assert!(result == TalkValue::Int(42));
+    });
+}
