13f3d4aafca128e034b2e068e213f030f578b7dc
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-12 21:47:32 +0000

    Add some errors for items of the wrong type

--- src/flotalk/error.rs
+++ src/flotalk/error.rs
@@ -16,6 +16,24 @@ pub enum TalkError {
     /// The runtime was dropped before a future could completed
     RuntimeDropped,
 
+    /// A value that was not supposed to be nil is nil
+    IsNil,
+
+    /// A value that was expected to be a reference to something was not a reference
+    NotAReference,
+
+    /// A value that was expected to be a boolean was not a boolean
+    NotABoolean,
+
+    /// A value that was expected to be a number was not a number
+    NotANumber,
+
+    /// A value that was expected to be an integer number was not an integer number
+    NotAnInteger,
+
+    /// A value that was expected to be a float was not a float
+    NotAFloat,
+
     /// A value that looked like a floating point number could not be interpreted as such
     InvalidFloatingPointNumber(String),
 
