9999bb631e458b810ad73fe24b6d19f9566a78e0
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-12 20:29:18 +0000

    Implement the dispatch table

--- src/flotalk/dispatch_table.rs
+++ src/flotalk/dispatch_table.rs
@@ -1,6 +1,48 @@
+use super::continuation::*;
+use super::error::*;
+use super::message::*;
+use super::sparse_array::*;
+use super::value::*;
+
+use smallvec::*;
+
 ///
 /// Maps messages to the functions that process them, and other metadata (such as the source code, or a compiled version for the intepreter)
 ///
-pub struct TalkMessageDispatchTable {
+pub struct TalkMessageDispatchTable<TDataType> {
+    /// The action to take for a particular message type
+    message_action: TalkSparseArray<Box<dyn Send + Sync + Fn(TDataType, SmallVec<[TalkValue; 4]>) -> TalkContinuation>>,
+}
 
+impl<TDataType> TalkMessageDispatchTable<TDataType> {
+    ///
+    /// Creates an empty dispatch table
+    ///
+    pub fn empty() -> TalkMessageDispatchTable<TDataType> {
+        TalkMessageDispatchTable {
+            message_action: TalkSparseArray::empty()
+        }
+    }
+
+    ///
+    /// Sends a message to an item in this dispatch table
+    ///
+    #[inline]
+    pub fn send_message(&self, target: TDataType, message: TalkMessage) -> TalkContinuation {
+        let id      = message.signature_id();
+        let args    = message.to_arguments();
+
+        if let Some(action) = self.message_action.get(id.into()) {
+            (action)(target, args)
+        } else {
+            TalkContinuation::Ready(TalkValue::Error(TalkError::MessageNotSupported))
+        }
+    }
+
+    ///
+    /// Defines the action for a message
+    ///
+    pub fn define_message(&mut self, message: TalkMessageSignatureId, action: impl 'static + Send + Sync + Fn(TDataType, SmallVec<[TalkValue; 4]>) -> TalkContinuation) {
+        self.message_action.insert(message.into(), Box::new(action));
+    }
 }
