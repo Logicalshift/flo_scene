388c76ed7ac1ad37e0dbc7857a4c73f9f1eb2ff6
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-12 22:23:14 +0000

    Add a test for adding two numbers

--- tests/flotalk/simple_eval.rs
+++ tests/flotalk/simple_eval.rs
@@ -23,6 +23,23 @@ fn evaluate_number() {
 }
 
 #[test]
+fn add_numbers() {
+    let test_source     = "38 + 4";
+    let runtime         = TalkRuntime::empty();
+    let root_values     = vec![Arc::new(Mutex::new(TalkValueStore::default()))];
+
+    executor::block_on(async { 
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_continuation(talk_evaluate_simple(root_values, Arc::new(instructions))).await;
+
+        assert!(result == TalkValue::Int(42));
+    });
+}
+
+#[test]
 fn retrieve_argument() {
     let test_source     = "x";
     let runtime         = TalkRuntime::empty();
