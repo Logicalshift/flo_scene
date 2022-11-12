97e3d39bb081cb204ede3a92615923245253e6a5
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-12 23:06:10 +0000

    Test dividing numbers

--- tests/flotalk/simple_eval.rs
+++ tests/flotalk/simple_eval.rs
@@ -40,6 +40,23 @@ fn add_numbers() {
 }
 
 #[test]
+fn divide_numbers() {
+    let test_source     = "1021.2 // 24.2";
+    let runtime         = TalkRuntime::empty();
+    let root_values     = vec![Arc::new(Mutex::new(TalkValueStore::default()))];
+
+    executor::block_on(async { 
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_continuation(talk_evaluate_simple(root_values, Arc::new(instructions))).await;
+
+        assert!(result == TalkValue::Int(42));
+    });
+}
+
+#[test]
 fn retrieve_argument() {
     let test_source     = "x";
     let runtime         = TalkRuntime::empty();
