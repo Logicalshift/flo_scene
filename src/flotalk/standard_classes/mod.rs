71cd4bb41722a7e98d97018619f9511b0cecde19
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-29 20:48:21 +0000

    Start putting together the 'script' classes, which use a cell block for storage and can be edited with extra methods

--- src/flotalk/standard_classes/mod.rs
+++ src/flotalk/standard_classes/mod.rs
@@ -1,5 +1,7 @@
 mod simple_evaluator_block;
 mod class_class;
+mod script_class;
 
 pub use self::simple_evaluator_block::*;
 pub use self::class_class::*;
+pub use self::script_class::*;
---
+++ src/flotalk/standard_classes/script_class.rs
@@ -0,0 +1,136 @@
+use crate::flotalk::allocator::*;
+use crate::flotalk::class::*;
+use crate::flotalk::context::*;
+use crate::flotalk::continuation::*;
+use crate::flotalk::error::*;
+use crate::flotalk::message::*;
+use crate::flotalk::reference::*;
+use crate::flotalk::releasable::*;
+use crate::flotalk::symbol_table::*;
+use crate::flotalk::value::*;
+
+use smallvec::*;
+
+///
+/// This class is a factory for other classes: it creates TalkScriptClass objects
+///
+pub struct TalkScriptClassClass {
+
+}
+
+///
+/// This represents an instance of a talk script class
+///
+pub struct TalkScriptClass {
+    /// The ID of the TalkCellBlockClass that this script class is associated with
+    class_id: TalkClass,
+
+    /// If this class has a superclass, the ID of that class
+    superclass_id: Option<TalkClass>,
+
+    /// The instance variables for this class
+    instance_variables: TalkSymbolTable,
+}
+
+///
+/// A cell block class is a class whose data type is a context cell block
+///
+pub struct TalkCellBlockClass;
+
+///
+/// Allocator that creates context cellblocks when requested
+///
+pub struct TalkCellBlockAllocator;
+
+impl TalkReleasable for TalkScriptClass {
+    fn release_in_context(self, _context: &TalkContext) {
+
+    }
+}
+
+impl TalkClassDefinition for TalkScriptClassClass {
+    /// The type of the data stored by an object of this class
+    type Data = TalkScriptClass;
+
+    /// The allocator is used to manage the memory of this class within a context
+    type Allocator = TalkStandardAllocator<TalkScriptClass>;
+
+    ///
+    /// Creates the allocator for this class
+    ///
+    fn create_allocator(&self) -> Self::Allocator {
+        TalkStandardAllocator::empty()
+    }
+
+    ///
+    /// Sends a message to the class object itself
+    ///
+    fn send_class_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<'_, SmallVec<[TalkValue; 4]>>, class_id: TalkClass, allocator: &mut Self::Allocator) -> TalkContinuation<'static> {
+        TalkError::MessageNotSupported(message_id).into()
+    }
+
+    ///
+    /// Sends a message to an instance of this class
+    ///
+    fn send_instance_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<'_, SmallVec<[TalkValue; 4]>>, reference: TalkReference, target: &mut Self::Data) -> TalkContinuation<'static> {
+        TalkError::MessageNotSupported(message_id).into()
+    }
+}
+
+impl TalkClassDefinition for TalkCellBlockClass {
+    /// The type of the data stored by an object of this class
+    type Data = TalkCellBlock;
+
+    /// The allocator is used to manage the memory of this class within a context
+    type Allocator = TalkCellBlockAllocator;
+
+    ///
+    /// Creates the allocator for this class
+    ///
+    fn create_allocator(&self) -> Self::Allocator {
+        TalkCellBlockAllocator
+    }
+
+    ///
+    /// Sends a message to the class object itself
+    ///
+    fn send_class_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<'_, SmallVec<[TalkValue; 4]>>, class_id: TalkClass, allocator: &mut Self::Allocator) -> TalkContinuation<'static> {
+        TalkError::MessageNotSupported(message_id).into()
+    }
+
+    ///
+    /// Sends a message to an instance of this class
+    ///
+    fn send_instance_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<'_, SmallVec<[TalkValue; 4]>>, reference: TalkReference, target: &mut Self::Data) -> TalkContinuation<'static> {
+        TalkError::MessageNotSupported(message_id).into()
+    }
+}
+
+impl TalkClassAllocator for TalkCellBlockAllocator {
+    /// The type of data stored for this class
+    type Data = TalkCellBlock;
+
+    ///
+    /// Retrieves a reference to the data attached to a handle (panics if the handle has been released)
+    ///
+    fn retrieve<'a>(&'a mut self, handle: TalkDataHandle) -> &'a mut Self::Data {
+        // The result is just the handle, but we can't really return a mutable reference to it
+        todo!();
+    }
+
+    ///
+    /// Adds to the reference count for a data handle
+    ///
+    fn add_reference(&mut self, handle: TalkDataHandle, context: &TalkContext) {
+        let cell_block = TalkCellBlock(handle.0 as _);
+        context.retain_cell_block(cell_block);
+    }
+
+    ///
+    /// Removes from the reference count for a data handle (freeing it if the count reaches 0)
+    ///
+    fn remove_reference(&mut self, handle: TalkDataHandle, context: &TalkContext) {
+        let cell_block = TalkCellBlock(handle.0 as _);
+        context.release_cell_block(cell_block);
+    }
+}
