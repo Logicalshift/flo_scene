3a08c0999ccfb8291d63be3b487ef9917d71dd7e
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-30 00:10:32 +0000

    Add some tests for unsupported messages; these get stuck too

--- tests/flotalk/script_class_tests.rs
+++ tests/flotalk/script_class_tests.rs
@@ -6,6 +6,32 @@ use futures::executor;
 use std::sync::*;
 
 #[test]
+fn unsupported_message() {
+    let test_source     = "Object unsupported";
+    let runtime         = TalkRuntime::empty();
+
+    executor::block_on(async { 
+        // Manually create the 'object' in this context (by sending 'new' to the script class class)
+        let object = runtime.run_continuation(TalkContinuation::soon(|talk_context| {
+            SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+        })).await;
+
+        // Run the test script with the 'Object' class defined
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_with_symbols(|_| vec![("Object".into(), object.clone())], |symbol_table, cells| talk_evaluate_simple(symbol_table, cells, Arc::new(instructions))).await;
+
+        // Should generate an error
+        assert!(match result {
+            TalkValue::Error(TalkError::MessageNotSupported(_)) => true,
+            _ => false
+        });
+    });
+}
+
+#[test]
 fn create_subclass() {
     let test_source     = "Object subclass";
     let runtime         = TalkRuntime::empty();
@@ -29,6 +55,32 @@ fn create_subclass() {
             TalkValue::Reference(new_class) => new_class.class() == *SCRIPT_CLASS_CLASS,
             _ => false
         });
+    });
+}
+
+#[test]
+fn subclass_unsupported() {
+    let test_source     = "(Object subclass) unsupported";
+    let runtime         = TalkRuntime::empty();
+
+    executor::block_on(async { 
+        // Manually create the 'object' in this context (by sending 'new' to the script class class)
+        let object = runtime.run_continuation(TalkContinuation::soon(|talk_context| {
+            SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+        })).await;
+
+        // Run the test script with the 'Object' class defined
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        //let result          = runtime.run_with_symbols(|_| vec![("Object".into(), object.clone())], |symbol_table, cells| talk_evaluate_simple(symbol_table, cells, Arc::new(instructions))).await;
+
+        // Should generate an error
+        //assert!(match result {
+        //    TalkValue::Error(TalkError::MessageNotSupported(_)) => true,
+        //    _ => false
+        //});
     });
 }
 
