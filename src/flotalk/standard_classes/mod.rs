003f62d33c82934e8db8155624181ca2902781c3
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-30 23:34:05 +0000

    Add a couple of 'message handler' types that we can use for casting the block types to

--- src/flotalk/standard_classes/mod.rs
+++ src/flotalk/standard_classes/mod.rs
@@ -1,7 +1,9 @@
 mod simple_evaluator_block;
 mod class_class;
 mod script_class;
+mod talk_message_handler;
 
 pub use self::simple_evaluator_block::*;
 pub use self::class_class::*;
 pub use self::script_class::*;
+pub use self::talk_message_handler::*;
---
+++ src/flotalk/standard_classes/talk_message_handler.rs
@@ -0,0 +1,32 @@
+use crate::flotalk::class::*;
+use crate::flotalk::continuation::*;
+use crate::flotalk::context::*;
+use crate::flotalk::reference::*;
+use crate::flotalk::releasable::*;
+use crate::flotalk::symbol_table::*;
+use crate::flotalk::value::*;
+
+use smallvec::*;
+
+use std::sync::*;
+
+///
+/// Represents a function that handles a message sent to a class message handler: it's possible to retrieve these from blocks
+///
+/// These are defined with the arguments for the message plus a 'super' value pointing at the superclass
+///
+pub struct TalkClassMessageHandler {
+    /// message_handler(class_id, arguments, super, context)
+    pub (super) message_handler: Box<dyn Send + Sync + for<'a> Fn(TalkClass, TalkOwned<'a, SmallVec<[TalkValue; 4]>>, TalkOwned<'a, TalkValue>, &'a TalkContext) -> TalkContinuation<'static>>
+}
+
+///
+/// Represents a function that handles a message sent to a class message handler
+///
+/// These are defined with the arguments for the message plus the instance variable cell block. The function returned is a function that binds the symbol table
+/// for the instance variables.
+///
+pub struct TalkInstanceMessageHandler {
+    pub (super) bind_message_handler: Box<dyn Send + FnOnce(Arc<Mutex<TalkSymbolTable>>) ->
+        Box<dyn Send + Sync + for<'a> Fn(TalkClass, TalkOwned<'a, SmallVec<[TalkValue; 4]>>, TalkOwned<'a, TalkCellBlock>, &'a TalkContext) -> TalkContinuation<'static>>>
+}
