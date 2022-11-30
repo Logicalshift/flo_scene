0234f3fbe8e172bf0ae2f7c6c87cc50ec7664612
Andrew Hunter <andrew@logicalshift.co.uk>
2022-11-30 10:07:19 +0000

    Need to return the class reference

--- tests/flotalk/script_class_tests.rs
+++ tests/flotalk/script_class_tests.rs
@@ -59,6 +59,33 @@ fn create_subclass() {
 }
 
 #[test]
+fn create_subclass_with_instance_variables() {
+    let test_source     = "Object subclassWithInstanceVariables: #var1:var2:var3:";
+    let runtime         = TalkRuntime::empty();
+
+    executor::block_on(async { 
+        // Manually create the 'object' in this context (by sending 'new' to the script class class)
+        let object = runtime.run_continuation(TalkContinuation::soon(|talk_context| {
+            SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+        })).await;
+
+        // Run the test script with the 'Object' class defined
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_with_symbols(|_| vec![("Object".into(), object.clone())], |symbol_table, cells| talk_evaluate_simple(symbol_table, cells, Arc::new(instructions))).await;
+
+        // Must generate a new class, using the SCRIPT_CLASS_CLASS
+        assert!(result != object);
+        assert!(match result {
+            TalkValue::Reference(new_class) => new_class.class() == *SCRIPT_CLASS_CLASS,
+            _ => false
+        });
+    });
+}
+
+#[test]
 fn subclass_unsupported() {
     let test_source     = "(Object subclass) unsupported";
     let runtime         = TalkRuntime::empty();
