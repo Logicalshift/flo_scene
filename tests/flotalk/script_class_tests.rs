9c4db728d83d0160ee4c8e7463ecf57053cf09ea
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-03 23:14:18 +0000

    Add tests for calling the superclass

--- tests/flotalk/script_class_tests.rs
+++ tests/flotalk/script_class_tests.rs
@@ -254,6 +254,37 @@ fn define_class_method_without_super() {
 }
 
 #[test]
+fn call_superclass_from_class_method() {
+    let test_source     = "
+    [ 
+        | NewClass1 NewClass2 | 
+        NewClass1 := Object subclass. 
+        NewClass1 addClassMessage: #foo: withAction: [ :foo :super | foo ].
+        NewClass2 := NewClass1 subclass.
+        NewClass2 addClassMessage: #bar: withAction: [ :bar :super | super foo: bar ].
+        ^NewClass2 foo: 42
+    ] value";
+    let runtime         = TalkRuntime::empty();
+
+    executor::block_on(async { 
+        // Manually create the 'object' in this context (by sending 'new' to the script class class)
+        let object = runtime.run_continuation(TalkContinuation::soon(|talk_context| {
+            SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+        })).await;
+
+        // Run the test script with the 'Object' class defined
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_with_symbols(|_| vec![("Object".into(), object.clone())], |symbol_table, cells| talk_evaluate_simple(symbol_table, cells, Arc::new(instructions))).await;
+
+        // Should return 42
+        assert!(result == TalkValue::Int(42));
+    });
+}
+
+#[test]
 fn define_instance_message() {
     let test_source     = "
     [ 
@@ -269,6 +300,47 @@ fn define_instance_message() {
         two setVal: 30 .
 
         ^(one getVal) + (two getVal)
+    ] value";
+    let runtime         = TalkRuntime::empty();
+
+    executor::block_on(async { 
+        // Manually create the 'object' in this context (by sending 'new' to the script class class)
+        let object = runtime.run_continuation(TalkContinuation::soon(|talk_context| {
+            SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+        })).await;
+
+        // Run the test script with the 'Object' class defined
+        let test_source     = stream::iter(test_source.chars());
+        let expr            = parse_flotalk_expression(test_source).next().await.unwrap().unwrap();
+        let instructions    = expr.value.to_instructions();
+
+        let result          = runtime.run_with_symbols(|_| vec![("Object".into(), object.clone())], |symbol_table, cells| talk_evaluate_simple(symbol_table, cells, Arc::new(instructions))).await;
+
+        // Should return 42
+        assert!(result == TalkValue::Int(42));
+    });
+}
+
+#[test]
+fn call_superclass_from_instance_method() {
+    let test_source     = "
+    [ 
+        | NewClass1 NewClass2 one two | 
+        NewClass1 := Object subclassWithInstanceVariables: #val. 
+        NewClass1 addInstanceMessage: #setVal: withAction: [ :newVal :self | val := newVal ].
+        NewClass1 addInstanceMessage: #getVal withAction: [ :self | val ].
+
+        NewClass2 := NewClass1 subclass.
+        NewClass2 addInstanceMessage: #setVal2: withAction: [ :newVal :self | super setVal: newVal ].
+        NewClass2 addInstanceMessage: #getVal2 withAction: [ :self | super getVal ].
+
+        one := NewClass1 new.
+        two := NewClass2 new.
+
+        one setVal: 12 .
+        two setVal2: 30 .
+
+        ^(one getVal) + (two getVal2)
     ] value";
     let runtime         = TalkRuntime::empty();
 
