9a98030ea5b689df7b64725967eac030322bb6ae
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-11 21:20:50 +0000

    Want to implement both the TalkMessageType and TalkValueType crates for these types (we need TalkValueType for the values of a struct)

--- flotalk_macros/src/lib.rs
+++ flotalk_macros/src/lib.rs
@@ -10,8 +10,8 @@ use syn::{Ident, Generics, Data, DataEnum, DataStruct};
 fn derive_enum_message(name: &Ident, generics: &Generics, data: &DataEnum) -> TokenStream {
     let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
-    // Implement the type
-    quote! {
+    // An enum value like 'Int(i64)' is converted to a message 'withInt: 64'
+    let talk_message_type = quote! {
         impl #impl_generics ::flo_talk::TalkMessageType for #name #where_clause {
             /// Converts a message to an object of this type
             fn from_message<'a>(message: ::flo_talk::TalkOwned<'a, ::flo_talk::TalkMessage>, context: &'a ::flo_talk::TalkContext) -> Result<Self, ::flo_talk::TalkError> {
@@ -19,17 +19,34 @@ fn derive_enum_message(name: &Ident, generics: &Generics, data: &DataEnum) -> To
             }
 
             /// Converts an object of this type to a message
-            fn to_message<'a>(&self, context: &'a mut ::flo_talk::TalkContext) -> ::flo_talk::TalkOwned<'a, ::flo_talk::TalkMessage> {
+            fn to_message<'a>(&self, context: &'a ::flo_talk::TalkContext) -> ::flo_talk::TalkOwned<'a, ::flo_talk::TalkMessage> {
                 todo!()
             }
         }
+    };
+
+    // We also implement the TalkValueType trait for things that can be messages (they create message objects)
+    let talk_value_type = quote! {
+        impl #impl_generics ::flo_talk::TalkValueType for #name #where_clause {
+            fn try_into_talk_value<'a>(self, context: &'a ::flo_talk::TalkContext) -> Result<::flo_talk::TalkOwned<'a, ::flo_talk::TalkValue>, ::flo_talk::TalkError> {
+                use flo_talk::{TalkOwned, TalkValue};
+
+                Ok(TalkOwned::new(TalkValue::Message(Box::new(self.to_message(context).leak())), context))
+            }
+        }
+    };
+
+    // Final result is bopth items
+    quote! {
+        #talk_message_type
+        #talk_value_type
     }.into()
 }
 
 ///
 /// Implements the `#[derive(TalkMessageType)]` attribute
 ///
-/// This attribute can be applied to types to automatically implement the `TalkMessageType` trait
+/// This attribute can be applied to types to automatically implement the `TalkMessageType` and `TalkValueType` traits
 ///
 #[proc_macro_derive(TalkMessageType)]
 pub fn derive_talk_message(struct_or_enum: TokenStream) -> TokenStream {
