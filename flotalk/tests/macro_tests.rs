badfbc014d6c34cd9511cf70415bcaf0acb3888d
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-11 22:55:27 +0000

    Extend the tests to the floats and the type with several parameters

--- flotalk/tests/macro_tests.rs
+++ flotalk/tests/macro_tests.rs
@@ -4,6 +4,7 @@ use flo_talk::*;
 enum TestEnum {
     Int(i64),
     Float(f64),
+    ManyInts(i64, i64),
 }
 
 #[test]
@@ -12,4 +13,20 @@ fn test_enum_int_to_message() {
     let int_as_message  = TestEnum::Int(42).to_message(&mut context);
 
     assert!(int_as_message.signature_id() == "withInt:".into());
+}
+
+#[test]
+fn test_enum_float_to_message() {
+    let mut context     = TalkContext::empty();
+    let int_as_message  = TestEnum::Float(42.0).to_message(&mut context);
+
+    assert!(int_as_message.signature_id() == "withFloat:".into());
+}
+
+#[test]
+fn test_enum_many_ints_to_message() {
+    let mut context     = TalkContext::empty();
+    let int_as_message  = TestEnum::ManyInts(1, 2).to_message(&mut context);
+
+    assert!(int_as_message.signature_id() == ("withManyInts:", ":").into());
 }
