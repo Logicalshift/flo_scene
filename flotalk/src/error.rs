fb1d229fb30a910883fcf8f488a65ede4f5451e7
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-26 21:44:34 +0000

    Add a 'Busy' error

--- flotalk/src/error.rs
+++ flotalk/src/error.rs
@@ -81,6 +81,9 @@ pub enum TalkError {
 
     /// A symbol was used that's not bound to any value
     UnboundSymbol(TalkSymbol),
+
+    /// An object is already busy servicing another request so the new request cannot be processed
+    Busy,
 }
 
 impl From<TalkParseError> for TalkError {
