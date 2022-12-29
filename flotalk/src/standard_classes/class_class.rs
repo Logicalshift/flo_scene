ee129df6474bace23125257a6a82fa7095ce85bc
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-29 18:26:05 +0000

    Fix warning

--- flotalk/src/standard_classes/class_class.rs
+++ flotalk/src/standard_classes/class_class.rs
@@ -89,7 +89,7 @@ impl TalkClass {
     /// Retrieves the data object for this TalkClass
     ///
     pub fn class_object_in_context(&self, context: &mut TalkContext) -> TalkReference {
-        context.get_callbacks_mut(*CLASS_CLASS);;
+        context.get_callbacks_mut(*CLASS_CLASS);
         TalkReference(*CLASS_CLASS, TalkDataHandle(self.0))
     }
 
