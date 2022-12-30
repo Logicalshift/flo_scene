a098560eb68fb35a390fc148b682a5bb0d0412d6
Andrew Hunter <andrew@logicalshift.co.uk>
2022-12-30 13:16:58 +0000

    Start to define a 'Streaming' class
    (We can do this in FloTalk itself now, I think)

--- flotalk/src/initialization.rs
+++ flotalk/src/initialization.rs
@@ -1,6 +1,7 @@
 use crate::continuation::*;
 use crate::message::*;
 use crate::standard_classes::*;
+use crate::script_continuation::*;
 
 ///
 /// Returns a continuation that will load the standard set of classes into 
@@ -8,6 +9,7 @@ use crate::standard_classes::*;
 pub fn talk_init_standard_classes() -> TalkContinuation<'static> {
     talk_init_object_class()
         .and_then(|_| talk_init_stream_class())
+        .and_then(|_| talk_init_streaming_class())
 }
 
 ///
@@ -16,7 +18,7 @@ pub fn talk_init_standard_classes() -> TalkContinuation<'static> {
 pub fn talk_init_object_class() -> TalkContinuation<'static> {
     TalkContinuation::soon(|talk_context| {
         SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
-    }) .and_then(|script_class| {
+    }).and_then(|script_class| {
         TalkContinuation::soon(move |talk_context| {
             talk_context.set_root_symbol_value("Object", script_class);
             ().into()
@@ -32,5 +34,44 @@ pub fn talk_init_stream_class() -> TalkContinuation<'static> {
         let stream_class_object = STREAM_CLASS.class_object_in_context(talk_context);
         talk_context.set_root_symbol_value("Stream", stream_class_object.into());
         ().into()
+    })
+}
+
+///
+/// Returns a continuation that will create the 'Streaming' class definition
+///
+/// Requires 'Stream' to already be initialised.
+///
+/// `Streaming` is a base class that can be used to create objects that handle messages as a stream rather
+/// usual instance messages.
+///
+pub fn talk_init_streaming_class() -> TalkContinuation<'static> {
+    TalkContinuation::soon(|talk_context| {
+        SCRIPT_CLASS_CLASS.send_message_in_context(TalkMessage::unary("new"), talk_context)
+    }).and_then(|script_class| {
+        // Start from a new base class and call it 'Streaming'
+        TalkContinuation::soon(move |talk_context| {
+            talk_context.set_root_symbol_value("Streaming", script_class);
+            ().into()
+        })
+    }).and_then(|_| {
+        // Define the class methods
+        TalkScript::from("
+            [
+                | OriginalStream |
+                OriginalStream := Stream.
+
+                Streaming addClassMethod: #subclass: withAction: [ :streamBlock |
+                    | subclass |
+
+                    subclass := Streaming subclassWithInstanceVariables: #messageStream:.
+                    subclass addInstanceMethod: #init withAction: [
+                        messageStream := OriginalStream withReceiver: streamBlock.
+                    ].
+
+                    ^subclass
+                ].
+            ] value
+        ").into()
     })
 }
