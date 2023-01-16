c9aa6c36cf3cd9c1b3f98c4b3677440312db0332
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-16 23:21:09 +0000

    Add a test for the basic 'inverted' functionality
    (This isn't working yet as none of the messages are declared)

--- flotalk/tests/inverted_class_tests.rs
+++ flotalk/tests/inverted_class_tests.rs
@@ -12,9 +12,39 @@ fn inverted_subclass() {
         let result  = runtime.run(TalkScript::from(test_source)).await;
 
         // Should not generate an error
+        println!("{:?}", result);
         assert!(match &*result {
             TalkValue::Error(_) => false,
             _                   => true
         });
+    });
+}
+
+#[test]
+fn send_inverted_mesasge() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted invertedInstance object val |
+
+        TestInverted := Inverted subclass.
+        TestInverted addInvertedMessage: #setValInverted: withAction: [ :newVal :sender :self | val := newVal ].
+
+        val                 := 0.
+        invertedInstance    := TestInverted new.
+        object              := Object new.
+
+        invertedInstance receiveFrom: object.
+        object setValInverted: 42.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should set the 'val' variable to 42 via an inverted message
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(42));
     });
 }
