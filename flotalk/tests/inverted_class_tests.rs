486d5790cd68362b730691cabb20913ea8f75ceb
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-17 00:37:48 +0000

    Sending an inverted message when there's no receiver should not generate an error

--- flotalk/tests/inverted_class_tests.rs
+++ flotalk/tests/inverted_class_tests.rs
@@ -49,6 +49,35 @@ fn send_inverted_message_directly() {
 }
 
 #[test]
+fn send_inverted_message_with_no_receiver() {
+    // Create an inverted subclass and send the message with no receiver attached (should produce no result)
+    let test_source = "
+        | TestInverted invertedInstance object val |
+
+        TestInverted := Inverted subclass.
+        TestInverted addInvertedMessage: #setValInverted: withAction: [ :newVal :sender :self | val := newVal ].
+
+        val                 := 0.
+        invertedInstance    := TestInverted new.
+        object              := Object new.
+
+        object setValInverted: 42
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should not generate an error
+        println!("{:?}", result);
+        assert!(match &*result {
+            TalkValue::Error(_) => false,
+            _                   => true
+        });
+    });
+}
+
+#[test]
 fn send_inverted_message() {
     // Create an inverted subclass and send a message to it from a 'normal' object
     let test_source = "
