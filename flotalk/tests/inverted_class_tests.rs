01f6c24e155ad037787a60688b08ce3ccf544b59
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-19 22:59:08 +0000

    Add tests for receiving order (most recent receiveFrom goes first) and the 'unreceived' behaviour

--- flotalk/tests/inverted_class_tests.rs
+++ flotalk/tests/inverted_class_tests.rs
@@ -166,3 +166,164 @@ fn send_inverted_message_to_all() {
         assert!(*result == TalkValue::Int(42));
     });
 }
+
+#[test]
+fn send_inverted_message_to_several_targets() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted invertedInstance1 invertedInstance2 object val |
+
+        TestInverted := Inverted subclass.
+        TestInverted addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := val + 1 ].
+
+        val                 := 0.
+        invertedInstance1   := TestInverted new.
+        invertedInstance2   := TestInverted new.
+        object              := Object new.
+
+        invertedInstance1 receiveFrom: object.
+        invertedInstance2 receiveFrom: object.
+        object invertedMessage.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should set the 'val' variable to 2 as both messages should get processed
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(2));
+    });
+}
+
+#[test]
+fn unreceived_filters_handled_messages() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted invertedInstance1 invertedInstance2 object val |
+
+        TestInverted := Inverted subclass.
+        TestInverted addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := val + 1 ].
+
+        val                 := 0.
+        invertedInstance1   := TestInverted new.
+        invertedInstance2   := TestInverted new.
+        object              := Object new.
+
+        invertedInstance1 receiveFrom: object unreceived.
+        invertedInstance2 receiveFrom: object.
+        object invertedMessage.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should set the 'val' variable to 1 as the 'unreceived' version of the message is processed after the 'received' version
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(1));
+    });
+}
+
+#[test]
+fn unreceived_processes_earliest_messages() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted invertedInstance1 invertedInstance2 object val |
+
+        TestInverted := Inverted subclass.
+        TestInverted addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := val + 1 ].
+
+        val                 := 0.
+        invertedInstance1   := TestInverted new.
+        invertedInstance2   := TestInverted new.
+        object              := Object new.
+
+        invertedInstance1 receiveFrom: object.
+        invertedInstance2 receiveFrom: object unreceived.
+        object invertedMessage.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should set the 'val' variable to 2 as both messages should get processed as the 'unreceived' version of the message should be processed first
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(2));
+    });
+}
+
+#[test]
+fn send_inverted_message_to_several_targets_in_order_1() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted1 TestInverted2 invertedInstance1 invertedInstance2 object val |
+
+        TestInverted1 := Inverted subclass.
+        TestInverted1 addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := 10 ].
+
+        TestInverted2 := Inverted subclass.
+        TestInverted2 addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := 20 ].
+
+        val                 := 0.
+        invertedInstance1   := TestInverted1 new.
+        invertedInstance2   := TestInverted2 new.
+        object              := Object new.
+
+        invertedInstance1 receiveFrom: object.
+        invertedInstance2 receiveFrom: object.
+        object invertedMessage.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should call invertedInstance2 then 1, so the final value is 10 (reverse order for the receiveFrom requests)
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(10));
+    });
+}
+
+#[test]
+fn send_inverted_message_to_several_targets_in_order_2() {
+    // Create an inverted subclass and send a message to it from a 'normal' object
+    let test_source = "
+        | TestInverted1 TestInverted2 invertedInstance1 invertedInstance2 object val |
+
+        TestInverted1 := Inverted subclass.
+        TestInverted1 addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := 10 ].
+
+        TestInverted2 := Inverted subclass.
+        TestInverted2 addInvertedMessage: #invertedMessage withAction: [ :sender :self | val := 20 ].
+
+        val                 := 0.
+        invertedInstance1   := TestInverted1 new.
+        invertedInstance2   := TestInverted2 new.
+        object              := Object new.
+
+        invertedInstance2 receiveFrom: object.
+        invertedInstance1 receiveFrom: object.
+        object invertedMessage.
+
+        val
+    ";
+
+    executor::block_on(async { 
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let result  = runtime.run(TalkScript::from(test_source)).await;
+
+        // Should call invertedInstance1 then 2, so the final value is 20
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(20));
+    });
+}
