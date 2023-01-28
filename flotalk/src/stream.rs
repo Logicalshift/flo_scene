7b75997b3150584602fafb7cc3478a6b6bb78982
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-28 19:41:41 +0000

    Add some extra documentation

--- flotalk/src/stream.rs
+++ flotalk/src/stream.rs
@@ -10,6 +10,21 @@ use futures::prelude::*;
 /// Creates a sender stream and sends it as a message to the result of the `receive_stream` continuation, returning a stream of the messages sent to
 /// that stream, and a continuation to run
 ///
+/// Essentially, the continuation that's passed in should return a block object that takes a single parameter. This function can be used like this:
+///
+/// ```ignore
+/// # // Can't compile this as cargo will say the crate is 'flo_talk' when it's not, which breaks the macro
+/// # #[macro_use] extern crate flo_talk_macros;
+/// # use flo_talk::*;
+/// #[derive(TalkMessageType)]
+/// enum HelloWorld { #[message("helloWorld")] Hello, #[message("goodbye")] Goodbye }
+///
+/// let (mut hello_world_stream, continuation) = talk_stream_from::<HelloWorld>(TalkScript::from("[ :output | output helloWorld. output goodbye. ]"));
+/// ```
+///
+/// The continuation must be run on a runtime before anything can be retrieved from the stream: the function `TalkRuntime::stream_from()` can be used
+/// to create a stream that will automatically execute its code in parallel and report any errors.
+///
 pub fn talk_stream_from<TStreamItem>(receive_sender_stream: impl Into<TalkContinuation<'static>>) -> (impl 'static + Send + Stream<Item=TStreamItem>, TalkContinuation<'static>) 
 where
     TStreamItem: 'static + Send + TalkMessageType,
