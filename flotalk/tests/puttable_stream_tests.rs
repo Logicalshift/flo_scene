2a1d9b2f6685b207cff68b54c37beddbb9261847
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-29 22:38:07 +0000

    Add a test of the puttable stream
    ... seems to show up that we want a 'continuation' variant of talk_fn_block

---
+++ flotalk/tests/puttable_stream_tests.rs
@@ -0,0 +1,56 @@
+use flo_talk::*;
+use flo_talk::protocols::*;
+
+use futures::prelude::*;
+use futures::executor;
+
+use std::sync::*;
+
+#[test]
+fn send_single_character() {
+    executor::block_on(async {
+        use futures::future;
+
+        // Set up the standard runtime
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Create a puttable stream for our test
+        // TODO: the way we do this is currently somewhat inelegant
+        let mut test_stream_value   = Arc::new(Mutex::new(None));
+        let stream_value            = test_stream_value.clone();
+
+        // Create the stream and assign the 'puttable stream value'
+        let (puttable_stream, puttable_continuation) = talk_puttable_character_stream(talk_fn_block(move |new_stream: TalkValue| {
+            (*stream_value.lock().unwrap()) = Some(new_stream);
+        }));
+        runtime.run(puttable_continuation).await;
+
+        // Actually put the stream value in the context
+        // TODO: want a way to do this in one go with a fn_block variant that returns a continuation
+        runtime.run(TalkContinuation::soon(move |talk_context| {
+            talk_context.set_root_symbol_value("testStream", test_stream_value.lock().unwrap().take().unwrap());
+
+            ().into()
+        })).await;
+
+        let mut puttable_stream = puttable_stream;
+        future::join(
+            async {
+                // Send 'a' to the stream we just created
+                let result = runtime.run(TalkScript::from("
+                    testStream nextPut: $a.
+                ")).await;
+
+                println!("Script: {:?}", result);
+                assert!(!result.is_error());
+            },
+            async {
+                // Receive the character
+                let mut next_value = puttable_stream.next().await;
+
+                println!("Stream: {:?}", next_value);
+                assert!(next_value == Some(TalkSimpleStreamRequest::Write("a".into())));
+            }
+        ).await;
+    });
+}
