1781188cbe0ec64f55246182069b379b7e85490e
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-29 13:24:10 +0000

    Can pipe through to generate stream items but this doesn't yet give us the ability to deal with 'put' requests, which will need some kind of 'continuation' pipe

--- flotalk/src/protocols/puttable_stream.rs
+++ flotalk/src/protocols/puttable_stream.rs
@@ -1,5 +1,8 @@
 use crate::*;
 
+use smallvec::*;
+use futures::prelude::*;
+
 ///
 /// FloTalk's puttableStream protocol
 ///
@@ -32,18 +35,34 @@ pub enum TalkSimpleStreamRequest {
     Write(String),
 }
 
-/*
 ///
 /// This creates a TalkPuttableStream and passes it to the result of the receive_stream continuation
 ///
 /// This is the type of stream that SmallTalk uses for the 'transcript' object, and forms the basis of the output mechanism for FloTalk
 ///
-pub fn talk_puttable_character_stream(receive_stream: impl Into<TalkContinuation<'static>>) -> (TalkContinuation<'static>, impl Stream<Item = TalkSimpleStreamRequest>) {
+pub fn talk_puttable_character_stream(receive_stream: impl Into<TalkContinuation<'static>>) -> (impl Stream<Item = TalkSimpleStreamRequest>, TalkContinuation<'static>) {
     // Create a 'puttable' stream and pass it to the block created by the receive_stream continuation
-    let (stream, continuation) = create_talk_stream::<TalkPuttableStreamRequest>(receive_stream);
+    let (create_sender_continuation, stream) = create_talk_sender::<TalkPuttableStreamRequest>();
 
     // Every value needs to be properly released when done, and we also need to evaluate the characters in the sequence passed to NextPutAll, so we need a stream processing continuation
+    let (simple_stream, simple_continuation) = talk_pipe_stream(stream, |request, talk_context| {
+        // TODO: we need to deal with put all by running another continuation :-/
+        TalkSimpleStreamRequest::Write("".to_string())
+    });
 
-    todo!()
+    // Put the simple continuation in the background
+    let receive_stream  = receive_stream.into();
+    let continuation    = TalkContinuation::soon(move |talk_context| {
+        // Put the simple continuation into the background
+        talk_context.run_in_background(simple_continuation);
+
+        // Create the sender and then the receive stream block, and send the 'value:' message to it
+        create_sender_continuation.and_then_if_ok(move |sender_value| {
+            receive_stream.and_then_soon(move |receive_stream, talk_context| {
+                receive_stream.send_message_in_context(TalkMessage::WithArguments(*TALK_MSG_VALUE_COLON, smallvec![sender_value]), talk_context)
+            })
+        })
+    });
+
+    (simple_stream, continuation)
 }
-*/
