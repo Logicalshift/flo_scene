750b9367ef9e406327278db6e34b01f1d34cc8d8
Andrew Hunter <andrew@logicalshift.co.uk>
2023-01-30 21:32:26 +0000

    Simplify the test cases using the continuation fn block

--- flotalk/tests/puttable_stream_tests.rs
+++ flotalk/tests/puttable_stream_tests.rs
@@ -14,24 +14,15 @@ fn send_single_character() {
         // Set up the standard runtime
         let runtime = TalkRuntime::with_standard_symbols().await;
 
-        // Create a puttable stream for our test
-        // TODO: the way we do this is currently somewhat inelegant
-        let test_stream_value   = Arc::new(Mutex::new(None));
-        let stream_value        = test_stream_value.clone();
-
         // Create the stream and assign the 'puttable stream value'
-        let (puttable_stream, puttable_continuation) = talk_puttable_character_stream(talk_fn_block(move |new_stream: TalkValue| {
-            (*stream_value.lock().unwrap()) = Some(new_stream);
+        let (puttable_stream, puttable_continuation) = talk_puttable_character_stream(talk_fn_block_continuation(|new_stream: TalkValue| {
+            TalkContinuation::soon(move |talk_context| {
+                talk_context.set_root_symbol_value("testStream", new_stream);
+
+                ().into()
+            })
         }));
         runtime.run(puttable_continuation).await;
-
-        // Actually put the stream value in the context
-        // TODO: want a way to do this in one go with a fn_block variant that returns a continuation
-        runtime.run(TalkContinuation::soon(move |talk_context| {
-            talk_context.set_root_symbol_value("testStream", test_stream_value.lock().unwrap().take().unwrap());
-
-            ().into()
-        })).await;
 
         let mut puttable_stream = puttable_stream;
         future::select(
@@ -72,24 +63,15 @@ fn send_string() {
         // Set up the standard runtime
         let runtime = TalkRuntime::with_standard_symbols().await;
 
-        // Create a puttable stream for our test
-        // TODO: the way we do this is currently somewhat inelegant
-        let test_stream_value   = Arc::new(Mutex::new(None));
-        let stream_value        = test_stream_value.clone();
-
         // Create the stream and assign the 'puttable stream value'
-        let (puttable_stream, puttable_continuation) = talk_puttable_character_stream(talk_fn_block(move |new_stream: TalkValue| {
-            (*stream_value.lock().unwrap()) = Some(new_stream);
+        let (puttable_stream, puttable_continuation) = talk_puttable_character_stream(talk_fn_block_continuation(|new_stream: TalkValue| {
+            TalkContinuation::soon(move |talk_context| {
+                talk_context.set_root_symbol_value("testStream", new_stream);
+
+                ().into()
+            })
         }));
         runtime.run(puttable_continuation).await;
-
-        // Actually put the stream value in the context
-        // TODO: want a way to do this in one go with a fn_block variant that returns a continuation
-        runtime.run(TalkContinuation::soon(move |talk_context| {
-            talk_context.set_root_symbol_value("testStream", test_stream_value.lock().unwrap().take().unwrap());
-
-            ().into()
-        })).await;
 
         let mut puttable_stream = puttable_stream;
         future::select(
