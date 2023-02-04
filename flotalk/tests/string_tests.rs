1c96206c7d4c43b8212e63c5282d6618750d1b36
Andrew Hunter <andrew@logicalshift.co.uk>
2023-02-04 23:20:51 +0000

    Add printString tests

--- flotalk/tests/string_tests.rs
+++ flotalk/tests/string_tests.rs
@@ -2,6 +2,8 @@ use flo_talk::*;
 
 use futures::executor;
 
+use std::sync::*;
+
 #[test]
 fn string_do_count_characters() {
     executor::block_on(async {
@@ -21,4 +23,79 @@ fn string_do_count_characters() {
         println!("{:?}", result);
         assert!(*result == TalkValue::Int(11));
     });
+}
+
+#[test]
+fn string_print_string() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Write out a string as as tring
+        let result = runtime.run(TalkScript::from("
+            'Test string' printString
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::String(Arc::new("Test string".into())));
+    })
+}
+
+#[test]
+fn string_print_character() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Write out a string as as tring
+        let result = runtime.run(TalkScript::from("
+            $a printString
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::String(Arc::new("a".into())));
+    })
+}
+
+#[test]
+fn string_print_int() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Write out a string as as tring
+        let result = runtime.run(TalkScript::from("
+            42 printString
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::String(Arc::new("42".into())));
+    })
+}
+
+#[test]
+fn string_print_array() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Write out a string as as tring
+        let result = runtime.run(TalkScript::from("
+            #(1 2 3) printString
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::String(Arc::new("#(1 2 3)".into())));
+    })
+}
+
+#[test]
+fn string_print_messages() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Write out a string as as tring
+        let result = runtime.run(TalkScript::from("
+            (#foo:bar: withArguments: #(1 2)) printString
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::String(Arc::new("##foo: 1 bar: 2".into())));
+    })
 }
