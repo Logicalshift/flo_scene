07f288d7191e27296e3d4b421f03c072f2efaa16
Andrew Hunter <andrew@logicalshift.co.uk>
2023-02-06 23:18:12 +0000

    Add a 'new' method to the evaluator so we can create instances

--- flotalk/src/standard_classes/evaluate_class.rs
+++ flotalk/src/standard_classes/evaluate_class.rs
@@ -25,11 +25,40 @@ pub (crate) static EVALUATE_CLASS: Lazy<TalkClass> = Lazy::new(|| TalkClass::cre
 /// The first version will evaluate the statement immediately, and the second will return a block which will evaluate the statement whenever
 /// the `value` message is sent to it.
 ///
+/// Instances of the `Evaluate` class can be used to evaluate statements using a copy of the current root namespace, for example:
+///
+/// ```smalltalk
+/// | eval |
+/// eval := Evaluate new.
+/// eval define: #'test' as: 10.
+/// eval statement: 'test'
+/// ```
+///
+/// `Evaluate new` will copy the root namespace of the current context (forming an independent root namespace) and `Evaluate newEmpty` will
+/// create an evaluator with an empty root namespace. The `define:` call can be used to define values in the root namespace of the evaluator.
+///
 pub struct TalkEvaluateClass;
+
+///
+/// Evaluate instance data
+///
+pub struct TalkEvaluate {
+    /// The cell block containing the values for the root symbol table
+    root_cell_block: TalkCellBlock,
+
+    /// The 'root' symbol table, which can be used for binding symbols when they have no symbol table of their own
+    root_symbol_table: Arc<Mutex<TalkSymbolTable>>,
+}
+
+impl TalkReleasable for TalkEvaluate {
+    fn release_in_context(self, context: &TalkContext) {
+        self.root_cell_block.release_in_context(context);
+    }
+}
 
 impl TalkClassDefinition for TalkEvaluateClass {
     /// The type of the data stored by an object of this class
-    type Data = ();
+    type Data = TalkEvaluate;
 
     /// The allocator is used to manage the memory of this class within a context
     type Allocator = TalkStandardAllocator<Self::Data>;
@@ -46,18 +75,21 @@ impl TalkClassDefinition for TalkEvaluateClass {
     ///
     /// Sends a message to the class object itself
     ///
-    fn send_class_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<SmallVec<[TalkValue; 4]>, &'_ TalkContext>, _class_id: TalkClass, _allocator: &Arc<Mutex<Self::Allocator>>) -> TalkContinuation<'static> {
+    fn send_class_message(&self, message_id: TalkMessageSignatureId, args: TalkOwned<SmallVec<[TalkValue; 4]>, &'_ TalkContext>, class_id: TalkClass, allocator: &Arc<Mutex<Self::Allocator>>) -> TalkContinuation<'static> {
         static MSG_STATEMENT: Lazy<TalkMessageSignatureId>      = Lazy::new(|| "statement:".into());
         static MSG_CREATE_BLOCK: Lazy<TalkMessageSignatureId>   = Lazy::new(|| "createBlock:".into());
 
         if message_id == *MSG_STATEMENT {
+
             let statement = args[0].try_as_string();
 
             match statement {
                 Err(err)        => err.into(),
                 Ok(statement)   => continuation_from_script(statement),
             }
+
         } else if message_id == *MSG_CREATE_BLOCK {
+
             // Fetch the statement
             let statement = args[0].try_as_string();
             let statement = match statement {
@@ -89,6 +121,42 @@ impl TalkClassDefinition for TalkEvaluateClass {
                     create_simple_evaluator_block_in_context(talk_context, vec![], vec![eval_symbol_block, root_symbol_block], eval_symbol_table, statement, None).into()
                 })
             })
+
+        } else if message_id == *TALK_MSG_NEW {
+
+            let allocator       = Arc::clone(allocator);
+
+            // Clone the symbol table
+            let symbol_table    = (*args.context().root_symbol_table().lock().unwrap()).clone();
+            let symbol_table    = Arc::new(Mutex::new(symbol_table));
+
+            // Clone the cells in the root
+            let root_cells      = args.context().cell_block(&*args.context().root_symbol_table_cell_block());
+            let mut root_cells  = root_cells.iter().map(|cell| cell.clone_in_context(args.context())).collect::<Vec<_>>();
+
+            TalkContinuation::soon(move |talk_context| {
+                // Create a new cell block
+                let cell_block  = talk_context.allocate_cell_block(root_cells.len());
+
+                // Copy in the root cells
+                let cells       = talk_context.cell_block_mut(&cell_block);
+                for (idx, val) in root_cells.drain(..).enumerate() {
+                    cells[idx] = val;
+                }
+
+                // Create the evaluator object
+                let evaluate = TalkEvaluate {
+                    root_cell_block:    cell_block,
+                    root_symbol_table:  symbol_table
+                };
+
+                // Store in the allocator
+                let data_handle = allocator.lock().unwrap().store(evaluate);
+                let reference   = TalkReference(class_id, data_handle);
+
+                reference.into()
+            })
+
         } else {
             TalkError::MessageNotSupported(message_id).into()
         }
