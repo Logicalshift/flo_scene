6944b5dfd6122a81e84620a8249beea3ef733514
Andrew Hunter <andrew@logicalshift.co.uk>
2023-02-06 23:19:27 +0000

    Add tests for using an Evaluate instance

--- flotalk/tests/evaluate_tests.rs
+++ flotalk/tests/evaluate_tests.rs
@@ -55,3 +55,118 @@ fn block_read_defined_value() {
         assert!(*msg == TalkValue::Int(42));
     })
 }
+
+#[test]
+fn instance_read_defined_value() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        runtime.run(TalkContinuation::from(42).define_as("test")).await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator |
+
+            evaluator := Evaluate new.
+
+            evaluator statement: 'test'
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(*msg == TalkValue::Int(42));
+    })
+}
+
+#[test]
+fn instance_redefine_value_1() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        runtime.run(TalkContinuation::from(20).define_as("test")).await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator |
+
+            evaluator := Evaluate new.
+            evaluator define: #'test' as: 22.
+
+            (evaluator statement: 'test') + test
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(*msg == TalkValue::Int(42));
+    })
+}
+
+#[test]
+fn instance_redefine_value_2() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        runtime.run(TalkContinuation::from(0).define_as("test")).await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator |
+
+            evaluator := Evaluate new.
+            evaluator define: #'test' as: 22.
+
+            test := 20.
+
+            (evaluator statement: 'test') + test
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(*msg == TalkValue::Int(42));
+    })
+}
+
+#[test]
+fn instance_redefine_value_3() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator test |
+
+            evaluator := Evaluate new.
+            evaluator define: #'test' as: 22.
+
+            test := 20.
+
+            (evaluator statement: 'test') + test
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(*msg == TalkValue::Int(42));
+    })
+}
+
+#[test]
+fn instance_redefine_value_from_empty() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        runtime.run(TalkContinuation::from(20).define_as("test")).await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator |
+
+            evaluator := Evaluate newEmpty.
+            evaluator define: #'test' as: 22.
+
+            (evaluator statement: 'test') + test
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(*msg == TalkValue::Int(42));
+    })
+}
+
+#[test]
+fn instance_empty_state() {
+    executor::block_on(async {
+        let runtime = TalkRuntime::with_standard_symbols().await;
+        runtime.run(TalkContinuation::from(20).define_as("test")).await;
+        let msg     = runtime.run(TalkScript::from("
+            | evaluator |
+
+            evaluator := Evaluate newEmpty.
+
+            evaluator statement: 'test'
+        ")).await;
+
+        println!("{:?}", msg);
+        assert!(msg.is_error());
+    })
+}
