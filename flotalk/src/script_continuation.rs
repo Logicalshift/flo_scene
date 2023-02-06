9cacec6f3a3470060a09ec627a319600d7e4091b
Andrew Hunter <andrew@logicalshift.co.uk>
2023-02-06 23:59:40 +0000

    Add a way to run a script with specific frames on the stack

--- flotalk/src/script_continuation.rs
+++ flotalk/src/script_continuation.rs
@@ -3,6 +3,7 @@ use super::expression::*;
 use super::instruction::*;
 use super::parse_error::*;
 use super::parser::*;
+use super::reference::*;
 use super::simple_evaluator::*;
 use super::symbol::*;
 use super::symbol_table::*;
@@ -74,6 +75,53 @@ impl TalkScript {
 }
 
 ///
+/// Creates a continuation that runs the specified script with a set of frames (assumed to be retained)
+///
+pub fn continuation_from_script_with_symbol_table<'a>(script: impl Into<TalkScript>, symbol_table: Arc<Mutex<TalkSymbolTable>>, frames: Vec<TalkCellBlock>) -> TalkContinuation<'a> {
+    let TalkScript(source) = script.into();
+
+    TalkContinuation::future_soon(async move { 
+        // Parse all the parts of the script
+        let source_stream   = stream::iter(source.chars());
+        let mut expressions = parse_flotalk_expression(source_stream);
+        let mut program     = vec![];
+
+        while let Some(next_expression) = expressions.next().await {
+            match next_expression {
+                Ok(next_expression) => { program.push(next_expression.value); },
+                Err(parser_error)   => { return TalkValue::Error(parser_error.value.into()).into(); }
+            }
+        }
+
+        // Convert to instructions
+        let instructions = TalkExpression::sequence_to_instructions(program);
+        let instructions = Arc::new(instructions);
+
+        // Run the instructions
+        TalkContinuation::soon(move |talk_context| {
+            // Any new symbols are local to the evaluation
+            let eval_symbol_table = Arc::new(Mutex::new(TalkSymbolTable::with_parent_frame(symbol_table)));
+            let eval_symbol_block = talk_context.allocate_cell_block(1);
+
+            let mut frames = frames;
+            frames.insert(0, eval_symbol_block);
+
+            let release_frames = frames.clone();
+
+            // Evaluate the expression, then release the cell blocks
+            talk_evaluate_simple(eval_symbol_table, frames, instructions)
+                .and_then(move |result| {
+                    TalkContinuation::soon(move |talk_context| {
+                        release_frames.into_iter().for_each(|cell_block| { talk_context.release_cell_block(cell_block); });
+
+                        result.into()
+                    })
+                })
+        })
+    })
+}
+
+///
 /// Creates a continuation that can run the specified script
 ///
 pub fn continuation_from_script<'a>(script: impl Into<TalkScript>) -> TalkContinuation<'a> {
@@ -96,7 +144,7 @@ pub fn continuation_from_script<'a>(script: impl Into<TalkScript>) -> TalkContin
         let instructions = TalkExpression::sequence_to_instructions(program);
         let instructions = Arc::new(instructions);
 
-        // Run the insutrctions
+        // Run the instructions
         TalkContinuation::soon(move |talk_context| {
             // Run with the root symbol table
             let root_symbol_table = talk_context.root_symbol_table();
