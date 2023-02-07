c4b1c401bd1474dd23f321cbfbe974beab9fc26d
Andrew Hunter <andrew@logicalshift.co.uk>
2023-02-07 00:01:46 +0000

    Fix tests

--- flotalk/tests/context_tests.rs
+++ flotalk/tests/context_tests.rs
@@ -5,7 +5,7 @@ fn allocate_cells() {
     let mut context = TalkContext::empty();
 
     let cell_block = context.allocate_cell_block(20);
-    assert!(context.cell_block(cell_block).len() == 20);
+    assert!(context.cell_block(&cell_block).len() == 20);
     context.release_cell_block(cell_block);
 }
 
@@ -14,11 +14,11 @@ fn reallocate_cells() {
     let mut context = TalkContext::empty();
 
     let cell_block_1 = context.allocate_cell_block(20);
-    assert!(context.cell_block(cell_block_1).len() == 20);
+    assert!(context.cell_block(&cell_block_1).len() == 20);
     context.release_cell_block(cell_block_1);
 
     let cell_block_2 = context.allocate_cell_block(10);
-    assert!(context.cell_block(cell_block_2).len() == 10);
+    assert!(context.cell_block(&cell_block_2).len() == 10);
     context.release_cell_block(cell_block_2);
 
     // As we have only allocated and freed one cell block, it should be the case that the blocks are the same
@@ -33,6 +33,6 @@ fn retain_cells() {
     context.retain_cell_block(cell_block);
     context.release_cell_block(cell_block);
 
-    assert!(context.cell_block(cell_block).len() == 20);
+    assert!(context.cell_block(&cell_block).len() == 20);
     context.release_cell_block(cell_block);
 }
