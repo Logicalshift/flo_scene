62aa149c8ec391081d8045f727b882cf26598b25
Andrew Hunter <andrew@logicalshift.co.uk>
2023-03-05 00:51:54 +0000

    Add some tests for the dictionary class

---
+++ flotalk/tests/dictionary_class_tests.rs
@@ -0,0 +1,81 @@
+use flo_talk::*;
+
+use futures::executor;
+
+#[test]
+fn new_dictionary() {
+    executor::block_on(async {
+        // Set up the standard runtime
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Store a value in the dictionary
+        let result = runtime.run(TalkScript::from("
+            Dictionary new.
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(!result.is_error());
+    });
+}
+
+#[test]
+fn store_value() {
+    executor::block_on(async {
+        // Set up the standard runtime
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Store a value in the dictionary
+        let result = runtime.run(TalkScript::from("
+            | testDictionary |
+
+            testDictionary := Dictionary new.
+            testDictionary at: 'test' put: 42.
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(!result.is_error());
+    });
+}
+
+#[test]
+fn store_and_retrieve_value() {
+    executor::block_on(async {
+        // Set up the standard runtime
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Store and retrieve a value using a string key in a dictionary
+        let result = runtime.run(TalkScript::from("
+            | testDictionary |
+
+            testDictionary := Dictionary new.
+            testDictionary at: 'test' put: 42.
+            testDictionary at: 'test'
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(42));
+    });
+}
+
+#[test]
+fn store_and_retrieve_several_values() {
+    executor::block_on(async {
+        // Set up the standard runtime
+        let runtime = TalkRuntime::with_standard_symbols().await;
+
+        // Store several values and read them back
+        let result = runtime.run(TalkScript::from("
+            | testDictionary |
+
+            testDictionary := Dictionary new.
+            testDictionary at: 'test1' put: 12.
+            testDictionary at: 'test2' put: 20.
+            testDictionary at: 'test3' put: 10.
+
+            (testDictionary at: 'test1') + (testDictionary at: 'test2') + (testDictionary at: 'test3')
+        ")).await;
+
+        println!("{:?}", result);
+        assert!(*result == TalkValue::Int(42));
+    });
+}
