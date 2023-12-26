469f2d1d436b1dadde904a3a936e00f40e431d27
Andrew Hunter <andrew@logicalshift.co.uk>
2023-12-26 20:50:31 +0000

    Add a way to send messages to an input stream

--- scene/src/input_stream.rs
+++ scene/src/input_stream.rs
@@ -20,7 +20,10 @@ pub (crate) struct InputStreamCore<TMessage> {
     waiting_messages: VecDeque<TMessage>,
 
     /// A waker for the future that is waiting for the next message in this stream
-    waker: Option<Waker>,
+    when_message_sent: Option<Waker>,
+
+    /// Wakers for any output streams waiting for slots to become available
+    when_slots_available: VecDeque<Waker>,
 
     /// True if this stream is closed (because the subprogram is ending)
     closed: bool,
@@ -34,13 +37,17 @@ pub struct InputStream<TMessage> {
 }
 
 impl<TMessage> InputStream<TMessage> {
+    ///
+    /// Creates a new input stream
+    ///
     pub (crate) fn new(program_id: SubProgramId, max_waiting: usize) -> Self {
         let core = InputStreamCore {
-            program_id:         program_id,
-            max_waiting:        max_waiting,
-            waiting_messages:   VecDeque::new(),
-            waker:              None,
-            closed:             false,
+            program_id:             program_id,
+            max_waiting:            max_waiting,
+            waiting_messages:       VecDeque::new(),
+            when_message_sent:      None,
+            when_slots_available:   VecDeque::new(),
+            closed:                 false,
         };
 
         InputStream {
@@ -49,18 +56,45 @@ impl<TMessage> InputStream<TMessage> {
     }
 }
 
+impl<TMessage> InputStreamCore<TMessage> {
+    ///
+    /// Adds a message to this core if there's space for it
+    ///
+    pub (crate) fn send(&mut self, message: TMessage) -> Result<(), TMessage> {
+        if self.waiting_messages.len() <= self.max_waiting {
+            self.waiting_messages.push_back(message);
+            Ok(())
+        } else {
+            Err(message)
+        }
+    }
+}
+
 impl<TMessage> Stream for InputStream<TMessage> {
     type Item=TMessage;
 
     fn poll_next(self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
+        use std::mem;
+
         let mut core = self.core.lock().unwrap();
 
         if let Some(message) = core.waiting_messages.pop_front() {
+            // If any of the output sinks are waiting to write a value, wake them up as the queue has reduced
+            let next_available = core.when_slots_available.pop_front();
+            mem::drop(core);
+
+            if let Some(next_available) = next_available {
+                next_available.wake()
+            }
+
+            // Return the message
             Poll::Ready(Some(message))
         } else if core.closed {
+            // Once all the messages are delivered and the core is closed, close the stream
             Poll::Ready(None)
         } else {
-            core.waker = Some(cx.waker().clone());
+            // Wait for the next message to be delivered
+            core.when_message_sent = Some(cx.waker().clone());
             Poll::Pending
         }
     }
