4a936d91e31a8584d290cd0a1ee046f4adc6197c
Andrew Hunter <andrew@logicalshift.co.uk>
2023-12-31 14:21:41 +0000

    'Intern' the strings used for names so that SubProgramIds can be copyable

--- scene/src/subprogram_id.rs
+++ scene/src/subprogram_id.rs
@@ -1,14 +1,44 @@
 use uuid::*;
+use once_cell::sync::{Lazy};
 
-// TODO: store the internal ID referenced via a handle (so SubProgramId can implement Copy + be small/fast to reference)
+use std::collections::*;
+use std::sync::*;
+
+static IDS_FOR_NAMES: Lazy<RwLock<HashMap<String, usize>>> = Lazy::new(|| RwLock::new(HashMap::new()));
+static NAMES_FOR_IDS: Lazy<RwLock<Vec<String>>>            = Lazy::new(|| RwLock::new(vec![]));
+
+fn id_for_name(name: &str) -> usize {
+    if let Some(id) = IDS_FOR_NAMES.read().unwrap().get(name).copied() {
+        // ID already exists
+        id
+    } else {
+        // Create a new ID
+        let id = {
+            let mut names_for_ids   = NAMES_FOR_IDS.write().unwrap();
+            let id                  = names_for_ids.len();
+            names_for_ids.push(name.into());
+
+            id
+        };
+
+        // Store the mapping
+        let mut ids_for_names = IDS_FOR_NAMES.write().unwrap();
+        ids_for_names.insert(name.into(), id);
+
+        id
+    }
+}
 
 ///
 /// A unique identifier for a subprogram in a scene
 ///
-#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Hash, Debug)]
-pub enum SubProgramId {
+#[derive(Copy, Clone, PartialEq, PartialOrd, Eq, Ord, Hash, Debug)]
+pub struct SubProgramId(SubProgramIdValue);
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Eq, Ord, Hash, Debug)]
+enum SubProgramIdValue {
     /// A subprogram identified with a well-known name
-    Named(String),
+    Named(usize),
 
     /// A subprogram identified with a GUID
     Guid(Uuid),
@@ -18,14 +48,16 @@ impl SubProgramId {
     ///
     /// Creates a new unique subprogram id
     ///
+    #[inline]
     pub fn new() -> SubProgramId {
-        SubProgramId::Guid(Uuid::new_v4())
+        SubProgramId(SubProgramIdValue::Guid(Uuid::new_v4()))
     }
 
     ///
     /// Creates a subprogram ID with a well-known name
     ///
+    #[inline]
     pub fn called(name: &str) -> SubProgramId {
-        SubProgramId::Named(name.into())
+        SubProgramId(SubProgramIdValue::Named(id_for_name(name)))
     }
 }
