f4153ef9f40a642bfdc4fcd5f62f9b69a553f977
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-01 21:18:31 +0000

    Add a test that disconnects/reconnects a filtered stream. Want to check that the filter gets dropped properly here.
    (This also checks the connect_programs routine, though this probably deserves its own tests)

--- scene/tests/filter_tests.rs
+++ scene/tests/filter_tests.rs
@@ -63,3 +63,76 @@ fn write_to_filter_target() {
     assert!(recv_messages == vec![1.to_string(), 2.to_string(), 3.to_string(), 4.to_string()], "Test program did not send correct numbers (sent {:?})", recv_messages);
     assert!(has_finished, "Scene did not finish when the programs terminated");
 }
+
+#[test]
+fn disconnect_filter_target() {
+    // List of messages that were received by the subprogram
+    let recv_messages = Arc::new(Mutex::new(vec![]));
+
+    // Create a scene with just this subprogram in it
+    let scene           = Arc::new(Scene::empty());
+    let sent_messages   = recv_messages.clone();
+
+    // Create a filter that converts numbers to strings
+    let usize_to_string = FilterHandle::for_filter(|number_stream: InputStream<usize>| number_stream.map(|num| num.to_string()));
+
+    // Add a program that receives some strings and writes them to recv_messages
+    let string_program = SubProgramId::new();
+    scene.add_subprogram(
+        string_program,
+        move |mut strings: InputStream<String>, _| async move {
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+        },
+        0,
+    );
+
+    // Add another program that outputs some numbers to the first program
+    let number_program  = SubProgramId::new();
+    let scene2          = scene.clone();
+    scene.add_subprogram(
+        number_program, 
+        move |_: InputStream<()>, context| async move {
+            let mut filtered_output = context.send::<usize>(StreamTarget::Any).unwrap();
+
+            // Send first two messages
+            filtered_output.send(1).await;
+            filtered_output.send(2).await;
+
+            // Disconnect the stream
+            scene2.connect_programs(number_program, StreamTarget::None, StreamId::with_message_type::<usize>()).unwrap();
+
+            // Send another two messages into oblivion
+            filtered_output.send(3).await;
+            filtered_output.send(4).await;
+
+            // Reconnect the two programs
+            scene2.connect_programs(number_program, StreamTarget::Filtered(usize_to_string, string_program), StreamId::with_message_type::<usize>()).unwrap();
+
+            // Final two messages
+            filtered_output.send(5).await;
+            filtered_output.send(6).await;
+        }, 
+        0);
+
+    // Start the programs connected
+    scene.connect_programs(number_program, StreamTarget::Filtered(usize_to_string, string_program), StreamId::with_message_type::<usize>()).unwrap();
+
+    // Run the scene
+    let mut has_finished = false;
+    executor::block_on(select(async {
+        scene.run_scene().await;
+        has_finished = true;
+    }.boxed(), Delay::new(Duration::from_millis(5000))));
+
+    // Received output should match the numbers
+    let recv_messages = (*recv_messages.lock().unwrap()).clone();
+    assert!(recv_messages == vec![1.to_string(), 2.to_string(), 5.to_string(), 6.to_string()], "Test program did not send correct numbers (sent {:?})", recv_messages);
+    assert!(has_finished, "Scene did not finish when the programs terminated");
+}
