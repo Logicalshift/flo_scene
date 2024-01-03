b3b84999a93ba4f3ae919b25e12b0ca71e4329fe
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-03 13:50:16 +0000

    Test starting a new subprogram through the control program

--- scene/tests/control_program_tests.rs
+++ scene/tests/control_program_tests.rs
@@ -41,3 +41,49 @@ fn ask_control_to_stop_scene() {
     // Should have stopped the scene and not just timed out
     assert!(has_stopped, "Scene did not stop");
 }
+
+#[test]
+fn ask_control_to_start_program() {
+    let has_started = Arc::new(Mutex::new(false));
+
+    // The default scene has the 'control' program in it
+    let scene           = Scene::default();
+    let notify_started  = has_started.clone();
+    scene.add_subprogram(
+        SubProgramId::new(),
+        move |input: InputStream<()>, context| async move {
+            // Send to the control program
+            let mut control_program = context.send::<SceneControl>(StreamTarget::Any).unwrap();
+
+            // Tell it to start a new program
+            control_program.send(SceneControl::start_program(SubProgramId::new(), move |_: InputStream<()>, context| {
+                let notify_started = notify_started.clone();
+                async move {
+                    // Set the flag to indicate the new program started
+                    *notify_started.lock().unwrap() = true;
+
+                    // Stop the scene as the test is done
+                    context.send::<SceneControl>(StreamTarget::Any).unwrap().send(SceneControl::StopScene).await;
+                }
+            }, 0)).await;
+
+            // Read from our input forever
+            let mut input = input;
+            while let Some(_) = input.next().await {
+
+            }
+        },
+        0,
+    );
+
+    let mut has_stopped = false;
+    executor::block_on(select(async {
+        scene.run_scene().await;
+
+        has_stopped = true;
+    }.boxed(), Delay::new(Duration::from_millis(5000))));
+
+    // Should have stopped the scene and not just timed out
+    assert!(*has_started.lock().unwrap() == true, "Subprogram did not start");
+    assert!(has_stopped, "Scene did not stop");
+}
