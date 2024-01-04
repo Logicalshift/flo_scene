64cfef5a1cba6441e2ec24eb171e5528764e0acd
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-04 19:58:58 +0000

    Add a way to specify the default target for a message type and a way to initialise a message type in a scene

--- scene/src/scene_message.rs
+++ scene/src/scene_message.rs
@@ -1,8 +1,23 @@
+use crate::scene::*;
+use crate::stream_target::*;
+
 ///
 /// Trait implemented by messages that can be sent via a scene
 ///
 pub trait SceneMessage : Sized + Send + Sync + Unpin {
+    ///
+    /// The default target for this message type
+    ///
+    /// This is `StreamTarget::Any` by default, so streams will wait to be connected. This can be set to `StreamTarget::None`
+    /// to throw away messages, or even to a program ID if messages should be sent to a particular program by default.
+    ///
+    fn default_target() -> StreamTarget { StreamTarget::Any }
 
+    ///
+    /// Sets up this message type in a scene. This can be an opportunity to set up default filters and connections for a
+    /// particular message type. This is called the first time that a message is referenced in a scene.
+    ///
+    fn initialise(scene: &Scene) { let _ = scene; }
 }
 
 impl SceneMessage for () { }
