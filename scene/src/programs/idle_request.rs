e7595eeab7fc3e44be451b6c926da2d4634d85ba
Andrew Hunter <andrew@logicalshift.co.uk>
2024-01-31 22:23:06 +0000

    Add equality traits

--- scene/src/programs/idle_request.rs
+++ scene/src/programs/idle_request.rs
@@ -45,6 +45,7 @@ pub enum IdleRequest {
 ///
 /// Message sent when the scene becomes idle, after a request is sent to IdleRequest
 ///
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct IdleNotification;
 
 impl SceneMessage for IdleRequest {
