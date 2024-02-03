474c5a33b1cda51a3c0a41c3960dc99771da6d26
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-03 17:12:43 +0000

    Add a way to set up a test action to send a message

--- scene/src/programs/test.rs
+++ scene/src/programs/test.rs
@@ -1,9 +1,25 @@
 use crate::*;
 
+use futures::prelude::*;
 use futures::executor;
+use futures::future::{BoxFuture};
 
 use std::any::*;
 use std::sync::*;
+
+type ActionFn = Box<dyn FnOnce(InputStream<TestRequest>, &SceneContext) -> BoxFuture<'static, InputStream<TestRequest>>>;
+
+///
+/// Request sent to a test subprogram
+///
+enum TestRequest {
+    /// A converted message from another source
+    AnyMessage(Arc<dyn Send + Sync + Any>),
+}
+
+impl SceneMessage for TestRequest {
+
+}
 
 ///
 /// The test builder can be used to create a test subprogram for a scene
@@ -11,7 +27,8 @@ use std::sync::*;
 /// A test subprogram can send and expect messages in response
 ///
 pub struct TestBuilder {
-
+    /// The actions for the test process to perform
+    actions: Vec<ActionFn>,
 }
 
 impl TestBuilder {
@@ -20,15 +37,35 @@ impl TestBuilder {
     ///
     pub fn new() -> Self {
         TestBuilder {
-
+            actions: vec![],
         }
     }
 
     ///
     /// Adds a test action that sends a message to the scene originating from the test program
     ///
-    pub fn send_message<TMessage: SceneMessage>(self, message: TMessage) -> Self {
-        todo!()
+    pub fn send_message<TMessage: 'static + SceneMessage>(self, message: TMessage) -> Self {
+        self.send_message_to_target((), message)
+    }
+
+    ///
+    /// Adds a test action that sends a message to the scene originating from the test program
+    ///
+    pub fn send_message_to_target<TMessage: 'static + SceneMessage>(mut self, target: impl Into<StreamTarget>, message: TMessage) -> Self {
+        let target = target.into();
+
+        // Add an action that retrieves the target stream and sends the message to it
+        self.actions.push(Box::new(move |input_stream, context| {
+            let mut target_stream = context.send::<TMessage>(target).unwrap();
+
+            async move {
+                target_stream.send(message).await.unwrap();
+
+                input_stream
+            }
+        }.boxed()));
+
+        self
     }
 
     ///
@@ -45,10 +82,13 @@ impl TestBuilder {
     /// Runs the tests and the assertions in a scene
     ///
     pub fn run_in_scene(scene: &Scene, test_subprogram: SubProgramId) {
+        // Set up the filters to read the messages resulting from the test messages
+        todo!();
+
         // Create the test subprogram
         todo!();
 
-        // Run the scene on the current thread
+        // Run the scene on the current thread, until the test actions have been finished
         executor::block_on(async {
             scene.run_scene().await;
         });
