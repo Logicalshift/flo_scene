f57a67502f44805497b9d3efe593b2f805f80dc0
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-04 17:19:14 +0000

    Add a test that we can use a 'named' connection with an 'unnamed' connection
    (This isn't working at the moment, which is why the tests are failing)

--- scene/tests/filter_tests.rs
+++ scene/tests/filter_tests.rs
@@ -194,6 +194,65 @@ fn connect_all_to_filter_target() {
 }
 
 #[test]
+fn direct_connection_via_general_filter() {
+    // List of messages that were received by the subprogram
+    let recv_messages = Arc::new(Mutex::new(vec![]));
+
+    // Create a scene with just this subprogram in it
+    let scene           = Scene::empty();
+    let sent_messages   = recv_messages.clone();
+
+    // Create a filter that converts numbers to strings
+    let usize_to_string = FilterHandle::for_filter(|number_stream: InputStream<usize>| number_stream.map(|num| num.to_string()));
+
+    // Add a program that receives some strings and writes them to recv_messages
+    let string_program = SubProgramId::new();
+    scene.add_subprogram(
+        string_program,
+        move |mut strings: InputStream<String>, _| async move {
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+            let next_string = strings.next().await.unwrap();
+            sent_messages.lock().unwrap().push(next_string);
+        },
+        0,
+    );
+
+    // Add another program that outputs some numbers to the first program
+    // This directly connections to the first program, but the filter is applied to all connections of that message type
+    let number_program = SubProgramId::new();
+    scene.add_subprogram(
+        number_program, 
+        move |_: InputStream<()>, context| async move {
+            let mut filtered_output = context.send::<usize>(string_program).unwrap();
+
+            filtered_output.send(1).await.unwrap();
+            filtered_output.send(2).await.unwrap();
+            filtered_output.send(3).await.unwrap();
+            filtered_output.send(4).await.unwrap();
+        }, 
+        0);
+
+    scene.connect_programs((), StreamTarget::Filtered(usize_to_string, string_program), StreamId::with_message_type::<usize>()).unwrap();
+
+    // Run the scene
+    let mut has_finished = false;
+    executor::block_on(select(async {
+        scene.run_scene().await;
+        has_finished = true;
+    }.boxed(), Delay::new(Duration::from_millis(5000))));
+
+    // Received output should match the numbers
+    let recv_messages = (*recv_messages.lock().unwrap()).clone();
+    assert!(recv_messages == vec![1.to_string(), 2.to_string(), 3.to_string(), 4.to_string()], "Test program did not send correct numbers (sent {:?})", recv_messages);
+    assert!(has_finished, "Scene did not finish when the programs terminated");
+}
+
+#[test]
 fn connect_all_both_filtered_and_unfiltered() {
     // List of messages that were received by the subprogram
     let recv_messages = Arc::new(Mutex::new(vec![]));
