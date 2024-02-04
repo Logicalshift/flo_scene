04775bad50519a5551bd3cae8ce66421b99f610a
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-04 13:59:12 +0000

    Add a test of the scene test harness

---
+++ scene/tests/test_tests.rs
@@ -0,0 +1,34 @@
+use flo_scene::*;
+use flo_scene::programs::*;
+
+use futures::prelude::*;
+
+#[test]
+pub fn simple_ping_test_with_test_builder() {
+    struct Ping;
+    impl SceneMessage for Ping {}
+
+    // Create a default scene
+    let scene = Scene::default();
+
+    // Add a ping subprogram that responds to () messages with a 'Ping' response
+    let ping_program = SubProgramId::new();
+    scene.add_subprogram(ping_program, 
+        |input: InputStream<()>, context| async move {
+            let mut input = input.messages_with_sources();
+
+            while let Some((program_id, _)) = input.next().await {
+                let mut target = context.send(program_id).unwrap();
+
+                target.send(Ping).await.unwrap();
+            }
+        },
+        100);
+    scene.connect_programs((), ping_program, StreamId::with_message_type::<()>()).unwrap();
+
+    // Test it using the test builder
+    TestBuilder::new()
+        .send_message(())
+        .expect_message(|_: Ping| { Ok(()) })
+        .run_in_scene(&scene, SubProgramId::new());
+}
