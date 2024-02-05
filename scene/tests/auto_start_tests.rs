9cd961ab59945d6adac1e00a2a51a2b9f2c0f341
Andrew Hunter <andrew@logicalshift.co.uk>
2024-02-05 09:45:02 +0000

    Auto-start should work if a message type is first encountered in a connect call

--- scene/tests/auto_start_tests.rs
+++ scene/tests/auto_start_tests.rs
@@ -29,3 +29,31 @@ fn auto_start_on_first_message() {
         .send_message(AutoStartMessage)
         .run_in_scene(&scene, SubProgramId::new());
 }
+
+#[test]
+fn auto_start_on_connect() {
+    // Define a message with an initialisation routine that starts the default subprogram
+    struct AutoStartMessage;
+
+    impl SceneMessage for AutoStartMessage {
+        fn initialise(scene: &Scene) {
+            // When the message is initialised, create a program and redirect everything there
+            scene.add_subprogram(SubProgramId::called("AutoStart"),
+                |mut input_stream: InputStream<AutoStartMessage>, _context| async move {
+                    while let Some(_) = input_stream.next().await { }
+                }, 0);
+        }
+    }
+
+    // Create a scene, but don't start the 'auto start' program
+    let scene = Scene::default();
+
+    // Connect the auto-start program as if it's already initialised in the stream
+    scene.connect_programs((), SubProgramId::called("AutoStart"), StreamId::with_message_type::<AutoStartMessage>()).unwrap();
+
+    // Try sending a message to it (should start up when it's first encountered)
+    TestBuilder::new()
+        .send_message(AutoStartMessage)
+        .send_message(AutoStartMessage)
+        .run_in_scene(&scene, SubProgramId::new());
+}
