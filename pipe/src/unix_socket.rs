09b5ada6bae984aefbaf4332276758aff5cfac90
Andrew Hunter <andrew@logicalshift.co.uk>
2024-05-04 13:19:55 +0100

    Add tokio as a dependency
    We'll make the initial version of the pipe code use tokio as we probably want to support it anyway.
    
    One of the things about tokio is that it introduces a bunch of hidden state in the runtime which I kind of dislike, which in turn makes the possibility of implementing a 'raw' IO version tempting, but it's also a bunch of extra effort.

--- pipe/src/unix_socket.rs
+++ pipe/src/unix_socket.rs
@@ -9,6 +9,8 @@ use std::os::unix::net::{UnixListener};
 ///
 /// Creates a sub-program that accepts connections on a unix domain socket that binds at a specified path
 ///
+/// To use this subprogram, the scene must be running inside a tokio runtime.
+///
 pub fn create_unix_socket_program<TInputStream, TOutputStream>(scene: &Scene, program_id: SubProgramId, path: impl AsRef<Path>, 
     create_input_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, u8>) -> TInputStream,
     create_output_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, u8>) -> TOutputStream) 
