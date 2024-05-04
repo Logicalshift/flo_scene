9a2f6d4e569f6961d8097b2897ada8bac1c7fccd
Andrew Hunter <andrew@logicalshift.co.uk>
2024-05-04 17:41:55 +0100

    Implement create_unix_socket_program using the partially implemented function

--- pipe/src/unix_socket.rs
+++ pipe/src/unix_socket.rs
@@ -1,15 +1,24 @@
+use super::socket::*;
+
 use flo_scene::*;
 
 use futures::prelude::*;
 use futures::stream::{BoxStream};
 
+use tokio::net::{UnixListener};
+
+use ::desync::*;
+
 use std::path::*;
-use std::os::unix::net::{UnixListener};
 
 ///
 /// Creates a sub-program that accepts connections on a unix domain socket that binds at a specified path
 ///
 /// To use this subprogram, the scene must be running inside a tokio runtime.
+///
+/// The program will wait for subscribers (the `Subscribe` message) to the `SocketMessage<TInputStream::Item, TOutputStream::Item>`
+/// message. Typically, there's only one subscriber but in the event multiple are connected, they are informed of connections in
+/// a round-robin fashion.
 ///
 pub fn create_unix_socket_program<TInputStream, TOutputStream>(scene: &Scene, program_id: SubProgramId, path: impl AsRef<Path>, 
     create_input_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, u8>) -> TInputStream,
@@ -19,6 +28,21 @@ where
     TInputStream:   'static + Send + Stream,
     TOutputStream:  'static + Send + Stream,
 {
-    // TODO
-    Err(ConnectionError::TargetNotAvailable)
+    // Create the listener for this program
+    let listener = UnixListener::bind(path)
+        .map_err(|tokio_err| ConnectionError::IoError(format!("{}", tokio_err)))?;
+
+    // Add a socket runner subprogram. We don't use the address for anything, ie we accept all connections here
+    let listener = Desync::new(listener);
+    scene.add_subprogram(program_id, move |input, context| socket_listener_subprogram(input.messages_with_sources(), context, move || 
+        listener.future_desync(|listener| async {
+            listener.accept().await
+                .map(|(socket, _addr)| socket.into_split())
+                .map_err(|tokio_err| tokio_err.into())
+        }).map_ok_or_else(|_cancelled| Err(ConnectionError::Cancelled), |ok| ok),
+        create_input_messages,
+        create_output_messages), 0);
+
+    // Success
+    Ok(())
 }
