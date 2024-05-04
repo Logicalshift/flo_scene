a08ec56561651e01423fdbf2a0f407d5ae63bd49
Andrew Hunter <andrew@logicalshift.co.uk>
2024-05-04 11:55:01 +0100

    Start defining a UNIX-domain socket program
    One question is whether or not to just support tokio or use the raw Rust IO library; I suspect it's just easier to use tokio for everything here as we'll end up wanting it anyway

--- pipe/src/lib.rs
+++ pipe/src/lib.rs
@@ -1,6 +1,8 @@
 mod socket;
+mod unix_socket;
 
 pub mod main_scene;
 pub mod sub_scene;
 
 pub use socket::*;
+pub use unix_socket::*;
---
+++ pipe/src/unix_socket.rs
@@ -0,0 +1,22 @@
+use flo_scene::*;
+
+use futures::prelude::*;
+use futures::stream::{BoxStream};
+
+use std::path::*;
+use std::os::unix::net::{UnixListener};
+
+///
+/// Creates a sub-program that accepts connections on a unix domain socket that binds at a specified path
+///
+pub fn create_unix_socket_program<TInputStream, TOutputStream>(scene: &Scene, program_id: SubProgramId, path: impl AsRef<Path>, 
+    create_input_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, u8>) -> TInputStream,
+    create_output_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, u8>) -> TOutputStream) 
+    -> Result<(), ConnectionError> 
+where
+    TInputStream:   'static + Send + Stream,
+    TOutputStream:  'static + Send + Stream,
+{
+    // TODO
+    Err(ConnectionError::TargetNotAvailable)
+}
