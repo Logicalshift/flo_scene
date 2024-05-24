7ae5679ec149349d5184a74ca58546a0e8b736d4
Andrew Hunter <andrew@logicalshift.co.uk>
2024-05-24 09:53:20 +0100

    We should also generate an idle request if the input stream is dropped or never processes anything

--- scene/tests/idle_program_tests.rs
+++ scene/tests/idle_program_tests.rs
@@ -6,10 +6,28 @@
 use flo_scene::*;
 use flo_scene::programs::*;
 
+use futures::future;
+
 #[test]
 fn notify_on_idle() {
     let scene           = Scene::default();
     let test_program    = SubProgramId::new();
+
+    TestBuilder::new()
+        .send_message(IdleRequest::WhenIdle(test_program))
+        .expect_message(|IdleNotification| { Ok(()) })
+        .run_in_scene_with_threads(&scene, test_program, 5);
+}
+
+#[test]
+fn notifies_if_subprogram_drops_input_stream() {
+    let scene           = Scene::default();
+    let test_program    = SubProgramId::new();
+
+    // This will drop the input stream before processing any messages, will happen a lot when we create subprograms that don't process any input
+    scene.add_subprogram(SubProgramId::new(), |_: InputStream<()>, _| async move {
+        future::pending::<()>().await;
+    }, 0);
 
     TestBuilder::new()
         .send_message(IdleRequest::WhenIdle(test_program))
