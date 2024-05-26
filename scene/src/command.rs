cbf586c8eec09155f6e68b1de1ce2e9799fc373d
Andrew Hunter <andrew@logicalshift.co.uk>
2024-05-26 22:18:00 +0100

    Add a way to create commands from simple functions

--- scene/src/command.rs
+++ scene/src/command.rs
@@ -2,6 +2,10 @@ use crate::scene_context::*;
 use crate::scene_message::*;
 
 use futures::prelude::*;
+use futures::future::{BoxFuture};
+use futures::stream::{BoxStream};
+
+use std::marker::{PhantomData};
 
 ///
 /// Commands are spawnable tasks that carry out actions on behalf of a parent subprogram. A command can send multiple messages
@@ -12,4 +16,39 @@ pub trait Command {
     type TOutput: 'static + SceneMessage;
 
     fn run(&self, input: impl 'static + Send + Stream<Item=Self::TInput>, context: SceneContext) -> impl 'static + Send + Future<Output=()>;
+}
+
+///
+/// Basic type of a command that runs a function
+///
+pub struct FnCommand<TInput, TOutput>(PhantomData<TOutput>, Box<dyn 'static + Send + Sync + Fn(BoxStream<'static, TInput>, SceneContext) -> BoxFuture<'static, ()>>);
+
+impl<TInput, TOutput> FnCommand<TInput, TOutput>
+where
+    TInput:     'static + Send,
+    TOutput:    'static + SceneMessage
+{
+    ///
+    /// Creates a new FnCommand with an implementing function
+    ///
+    pub fn new<TFuture>(action: impl 'static + Send + Sync + Fn(BoxStream<'static, TInput>, SceneContext) -> TFuture) -> Self 
+    where
+        TFuture: 'static + Send + Future<Output=()>,
+    {
+        FnCommand(PhantomData, Box::new(move |stream, context| action(stream, context).boxed()))
+    }
+}
+
+impl<TInput, TOutput> Command for FnCommand<TInput, TOutput> 
+where
+    TInput:     'static + Send,
+    TOutput:    'static + SceneMessage
+{
+    type TInput     = TInput;
+    type TOutput    = TOutput;
+
+    #[inline]
+    fn run(&self, input: impl 'static + Send + Stream<Item=Self::TInput>, context: SceneContext) -> impl 'static + Send + Future<Output=()> {
+        self.1(input.boxed(), context)
+    }
 }
