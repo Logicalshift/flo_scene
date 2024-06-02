b6b084b41e8a0703d30ab9efe3b94b1bc8b116cc
Andrew Hunter <andrew@logicalshift.co.uk>
2024-06-02 12:34:11 +0100

    Implement a comment tokenizer

--- pipe/src/parse_command.rs
+++ pipe/src/parse_command.rs
@@ -5,6 +5,7 @@ use regex_automata::dfa::dense;
 use once_cell::sync::{Lazy};
 
 static COMMAND: Lazy<dense::DFA<Vec<u32>>> = Lazy::new(|| dense::DFA::new(r"(\p{L}|[_:-])(\p{L}|\p{N}|[_:-])*").unwrap());
+static COMMENT: Lazy<dense::DFA<Vec<u32>>> = Lazy::new(|| dense::DFA::new(r"(//[^\r\n]*)").unwrap());
 
 ///
 /// Tokens from the command stream
@@ -23,11 +24,31 @@ pub enum CommandToken {
     /// The '=' symbol, used to record a command result in a variable
     Equals,
 
-    /// A '// comment' or a '/* comment */'
+    /// A '// comment'
     Comment,
 
     /// A JSON token
     Json(JsonToken),
+}
+
+impl From<JsonToken> for CommandToken {
+    #[inline]
+    fn from(token: JsonToken) -> Self {
+        CommandToken::Json(token)
+    }
+}
+
+impl TryInto<JsonToken> for CommandToken {
+    type Error = Self;
+
+    #[inline]
+    fn try_into(self) -> Result<JsonToken, Self::Error> {
+        match self {
+            CommandToken::Json(token)   => Ok(token),
+            CommandToken::Comment       => Ok(JsonToken::Whitespace),
+            other                       => Err(other),
+        }
+    }
 }
 
 ///
@@ -35,4 +56,28 @@ pub enum CommandToken {
 ///
 fn match_command(lookahead: &str, eof: bool) -> TokenMatchResult<CommandToken> {
     match_regex(&*COMMAND, lookahead, eof).with_token(CommandToken::Command)
+}
+
+///
+/// Matches against the comment syntax
+///
+fn match_command_comment(lookahead: &str, eof: bool) -> TokenMatchResult<CommandToken> {
+    match_regex(&*COMMENT, lookahead, eof).with_token(CommandToken::Comment)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn match_simple_command() {
+        let match_result = match_command("test::command", true);
+        assert!(match_result == TokenMatchResult::Matches(CommandToken::Command, "test::command".chars().count()));
+    }
+
+    #[test]
+    fn match_simple_comment() {
+        let match_result = match_command_comment("// comment", true);
+        assert!(match_result == TokenMatchResult::Matches(CommandToken::Comment, "// comment".chars().count()));
+    }
 }
