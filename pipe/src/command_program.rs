f7101714bd885894aa3738d4c4455819c7b9c798
Andrew Hunter <andrew@logicalshift.co.uk>
2024-06-04 22:17:34 +0100

    Make the command processor an actual command that implements a trait

--- pipe/src/command_program.rs
+++ pipe/src/command_program.rs
@@ -1,10 +1,12 @@
 use crate::command_stream;
+use crate::command_stream::{CommandResponse};
 use crate::socket::*;
 
 use flo_scene::*;
 use flo_scene::programs::*;
 use flo_scene::commands::*;
 
+use futures::{pin_mut};
 use futures::prelude::*;
 use futures::stream::{BoxStream};
 use futures::channel::mpsc;
@@ -14,7 +16,7 @@ use futures::channel::mpsc;
 ///
 /// The simple command program can just read and write command responses, and cannot provide direct access to the terminal
 ///
-pub type CommandProgramSocketMessage = SocketMessage<Result<command_stream::Command, ()>, command_stream::CommandResponse>;
+pub type CommandProgramSocketMessage = SocketMessage<Result<command_stream::Command, ()>, CommandResponse>;
 
 ///
 /// The command program accepts connections from a socket and will generate command output messages
@@ -24,8 +26,6 @@ pub async fn command_connection_program(input: InputStream<CommandProgramSocketM
     // TODO: this would work a lot better if it was just a straight connection...
     let our_program_id = context.current_program_id().unwrap();
     context.send_message(Subscribe::<CommandProgramSocketMessage>::with_target(our_program_id.into())).await.unwrap();
-
-    let spawn_connection = FnCommand::<Result<command_stream::Command, ()>, command_stream::CommandResponse>::new(command_connection);
 
     // Spawn processor tasks for each connection
     let mut input = input;
@@ -37,8 +37,8 @@ pub async fn command_connection_program(input: InputStream<CommandProgramSocketM
                 let command_input = connection.connect(recv_response);
 
                 // Spawn a reader for the command input
-                if let Ok(responses) = context.spawn_command(spawn_connection.clone(), command_input) {
-                    // ... and another task to send the responses from the command
+                if let Ok(responses) = context.spawn_command(CommandProcessor, command_input) {
+                    // ... and another task to relay the responses back to the socket
                     context.spawn_command(FnCommand::<_, ()>::new(move |responses, _context| { 
                         let mut send_response = send_response.clone(); 
                         async move {
@@ -57,18 +57,30 @@ pub async fn command_connection_program(input: InputStream<CommandProgramSocketM
 }
 
 ///
-/// Runs a command connection
+/// The command processor command, which takes an input of parsed commands, and generates the corresponding responses
 ///
-async fn command_connection(input: BoxStream<'_, Result<command_stream::Command, ()>>, context: SceneContext) {
-    let mut input       = input;
-    let mut responses   = context.send::<command_stream::CommandResponse>(()).unwrap();
+/// This will generate one response per command
+///
+#[derive(Copy, Clone, PartialEq)]
+pub struct CommandProcessor;
 
-    while let Some(next_command) = input.next().await {
-        match next_command {
-            _ => {
-                // Just send error responses
-                if  responses.send(command_stream::CommandResponse::Error(format!("Not implemented yet"))).await.is_err() {
-                    break;
+impl Command for CommandProcessor {
+    type Input  = Result<command_stream::Command, ()>;
+    type Output = CommandResponse;
+
+    fn run(&self, input: impl 'static + Send + Stream<Item=Self::Input>, context: SceneContext) -> impl 'static + Send + Future<Output=()> {
+        async move {
+            pin_mut!(input);
+            let mut responses   = context.send::<CommandResponse>(()).unwrap();
+
+            while let Some(next_command) = input.next().await {
+                match next_command {
+                    _ => {
+                        // Just send error responses
+                        if responses.send(CommandResponse::Error(format!("Not implemented yet"))).await.is_err() {
+                            break;
+                        }
+                    }
                 }
             }
         }
