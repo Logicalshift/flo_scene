86c1566c7d40f0492f36bf4232ecc719415b4dcf
Andrew Hunter <andrew@logicalshift.co.uk>
2024-06-08 17:23:38 +0100

    Add a new message that is a request to run a command (going to use this for the command pipe later on)
    This differs from the command request in that it doesn't require JSON and it only requests one particular command

--- scene/src/commands/mod.rs
+++ scene/src/commands/mod.rs
@@ -1,7 +1,9 @@
 mod fn_command;
 mod pipe_command;
 mod read_command;
+mod run_command;
 
 pub use fn_command::*;
 pub use pipe_command::*;
 pub use read_command::*;
+pub use run_command::*;
---
+++ scene/src/commands/run_command.rs
@@ -0,0 +1,65 @@
+use crate::scene_message::*;
+use crate::stream_target::*;
+use crate::programs::*;
+
+use std::marker::{PhantomData};
+
+///
+/// The RunCommand is a query request that will run a named command with a parameter, returning the result as a stream of responses to a target
+///
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub struct RunCommand<TParameter, TResponse> {
+    target:     StreamTarget,
+    name:       String,
+    parameter:  TParameter,
+    response:   PhantomData<TResponse>,
+}
+
+impl<TParameter, TResponse> RunCommand<TParameter, TResponse>
+where
+    TParameter: Unpin + Send,
+    TResponse:  Unpin + Send
+{
+    ///
+    /// Creates a new 'run command' request. The command with the specified name will be run, and will send its response to the target.
+    ///
+    pub fn new(target: impl Into<StreamTarget>, name: impl Into<String>, parameter: impl Into<TParameter>) -> Self {
+        Self {
+            target:     target.into(),
+            name:       name.into(),
+            parameter:  parameter.into(),
+            response:   PhantomData,
+        }
+    }
+
+    ///
+    /// Returns the program that the response to the command should be setn to
+    ///
+    pub fn target(&self) -> StreamTarget {
+        self.target.clone()
+    }
+}
+
+impl<TParameter, TResponse> SceneMessage for RunCommand<TParameter, TResponse>
+where
+    TParameter: Unpin + Send,
+    TResponse:  Unpin + Send
+{
+}
+
+impl<TParameter, TResponse> QueryRequest for RunCommand<TParameter, TResponse> 
+where
+    TParameter: Unpin + Send,
+    TResponse:  Unpin + Send
+{
+    type ResponseData = TResponse;
+
+    fn with_new_target(self, new_target: StreamTarget) -> Self {
+        RunCommand {
+            target:     new_target,
+            name:       self.name,
+            parameter:  self.parameter,
+            response:   PhantomData
+        }
+    }
+}
