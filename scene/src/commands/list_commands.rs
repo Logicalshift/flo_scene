6e925879735d08e34fdaebebbe4abd1785917c42
Andrew Hunter <andrew@logicalshift.co.uk>
2024-06-15 22:10:37 +0100

    Support serialization/deserialization on the error and list_commands messages
    (We want to create a JSON command stream, so it will need to communicate using a dispatcher)

--- scene/src/commands/list_commands.rs
+++ scene/src/commands/list_commands.rs
@@ -1,10 +1,13 @@
 use crate::scene_message::*;
 
+#[cfg(feature="serde_support")] use serde::*;
+
 ///
 /// As part of a response to a list commands request, this indicates the name of a command supported by the sender. This
 /// is often used with a conversion into the response type of a command.
 ///
 #[derive(Clone, PartialEq, Eq, Debug)]
+#[cfg_attr(feature="serde_support", derive(Serialize, Deserialize))]
 pub struct ListCommandResponse(pub String);
 
 impl SceneMessage for ListCommandResponse { }
