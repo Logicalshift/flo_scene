92721425a881938c83bea0fdc7edfe9f7d2c0f19
Andrew Hunter <andrew@logicalshift.co.uk>
2024-06-17 16:34:18 +0100

    Test the 'error without closing' with a bunch of different scenarios
    Basically broken for numbers and strings at this point

--- pipe/tests/internal_socket_tests.rs
+++ pipe/tests/internal_socket_tests.rs
@@ -64,8 +64,9 @@ fn error_from_internal_socket() {
         .run_in_scene_with_threads(&scene, test_program, 5);
 }
 
-#[test]
-fn error_from_command_without_closing_socket() {
+fn run_expected_error_command_without_closing(command: impl Into<String>) {
+    let test_commands   = command.into();
+
     let scene           = Scene::default();
     let test_program    = SubProgramId::new();
  
@@ -84,10 +85,7 @@ fn error_from_command_without_closing_socket() {
     scene.connect_programs(socket_program, command_program, StreamId::with_message_type::<CommandProgramSocketMessage>()).unwrap();
 
     // Add another program that talks to the command program via a socket connection
-    scene.add_subprogram(SubProgramId::new(), |_input: InputStream<()>, context| async move {
-        // Crete a message to send
-        let test_commands = "test\n";
-
+    scene.add_subprogram(SubProgramId::new(), move |_input: InputStream<()>, context| async move {
         // Also create an internal buffer to write to
         let (our_side, their_side)          = duplex(1024);
         let (command_input, command_output) = split(their_side);
@@ -127,65 +125,33 @@ fn error_from_command_without_closing_socket() {
         .run_in_scene_with_threads(&scene, test_program, 5);
 }
 
+
 #[test]
-fn error_from_command_with_parameter_without_closing_socket() {
-    let scene           = Scene::default();
-    let test_program    = SubProgramId::new();
- 
-    struct TestSucceeded;
-    impl SceneMessage for TestSucceeded { }
+fn error_from_command_without_closing_socket() {
+    run_expected_error_command_without_closing("test\n");
+}
 
-    // The command program accepts connections from the socket and interprets the commands
-    let command_program = SubProgramId::new();
-    scene.add_subprogram(command_program, |input, context| command_connection_program(input, context, ()), 0);
+#[test]
+fn error_from_command_with_string_parameter_without_closing_socket() {
+    run_expected_error_command_without_closing("test \"string\"\n");
+}
 
-    // The internal socket program lets us stream commands and responses via a socket connection
-    let socket_program = SubProgramId::new();
-    start_internal_socket_program(&scene, socket_program, parse_command_stream, display_command_responses).unwrap();
+#[test]
+fn error_from_command_with_number_parameter_without_closing_socket() {
+    run_expected_error_command_without_closing("test -1234\n");
+}
 
-    // Socket program is connected to the command program using the command program socket message (which generates connections)
-    scene.connect_programs(socket_program, command_program, StreamId::with_message_type::<CommandProgramSocketMessage>()).unwrap();
+#[test]
+fn error_from_command_with_const_parameter_without_closing_socket() {
+    run_expected_error_command_without_closing("test true\n");
+}
 
-    // Add another program that talks to the command program via a socket connection
-    scene.add_subprogram(SubProgramId::new(), |_input: InputStream<()>, context| async move {
-        // Crete a message to send
-        let test_commands = "test \"hello\"\n";
+#[test]
+fn error_from_command_with_array_parameter_without_closing_socket() {
+    run_expected_error_command_without_closing("test [ 1, 2, 3, 4 ]\n");
+}
 
-        // Also create an internal buffer to write to
-        let (our_side, their_side)          = duplex(1024);
-        let (command_input, command_output) = split(their_side);
-        let (read_result, write_command)    = split(our_side);
-
-        // Request that the socket program read from the test commands and writes to the internal buffer
-        let mut socket_program = context.send(socket_program).unwrap();
-        socket_program.send(InternalSocketMessage::CreateInternalSocket(Box::new(command_input), Box::new(command_output))).await.ok().unwrap();
-
-        // Send the test command to the writer
-        let mut write_command = write_command;
-        println!("> {:?}", test_commands);
-        write_command.write_all(&test_commands.bytes().collect::<Vec<u8>>()).await.unwrap();
-
-        // Read the interal buffer to get the final result
-        let mut read_result = read_result;
-        while let Ok(msg) = read_result.read_u8().await {
-            println!("{:?}", msg as char);
-
-            // Read until the '!' indicating an error
-            if msg == b'!' {
-                break;
-            }
-        }
-
-        write_command.shutdown().await.unwrap();
-        println!("DONE");
-
-        // Indicate successs
-        context.send_message(TestSucceeded).await.ok();
-    }, 0);
-
-    // Wait for the test program to indicate that it succeeded
-    TestBuilder::new()
-        .redirect_input(StreamId::with_message_type::<TestSucceeded>())
-        .expect_message(|_: TestSucceeded| Ok(()))
-        .run_in_scene_with_threads(&scene, test_program, 5);
+#[test]
+fn error_from_command_with_object_parameter_without_closing_socket() {
+    run_expected_error_command_without_closing("test { \"test\": 2 }\n");
 }
