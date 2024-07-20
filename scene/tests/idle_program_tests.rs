6dba159d0be9337e2e958ec3545fed2046ad48b5
Andrew Hunter <andrew@logicalshift.co.uk>
2024-07-20 20:41:37 +0100

    Add a basic test of the 'wait for idle' routine (which is blocking the dispatcher)
    (It works for this case)

--- scene/tests/idle_program_tests.rs
+++ scene/tests/idle_program_tests.rs
@@ -6,6 +6,7 @@
 use flo_scene::*;
 use flo_scene::programs::*;
 
+use futures::prelude::*;
 use futures::future;
 
 #[test]
@@ -31,6 +32,23 @@ fn notifies_if_subprogram_drops_input_stream() {
 
     TestBuilder::new()
         .send_message(IdleRequest::WhenIdle(test_program))
+        .expect_message(|IdleNotification| { Ok(()) })
+        .run_in_scene_with_threads(&scene, test_program, 5);
+}
+
+#[test]
+fn wait_for_idle_then_send_message() {
+    let scene           = Scene::empty();
+    let test_program    = SubProgramId::new();
+
+    scene.add_subprogram(SubProgramId::new(), move |_input: InputStream<()>, context| async move {
+        context.wait_for_idle(1000).await;
+
+        context.send(test_program).unwrap()
+            .send(IdleNotification).await.unwrap();
+    }, 1);
+
+    TestBuilder::new()
         .expect_message(|IdleNotification| { Ok(()) })
         .run_in_scene_with_threads(&scene, test_program, 5);
 }
