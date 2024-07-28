50a9b49d0d4342d0a9773ca3e632c399383e76df
Andrew Hunter <andrew@logicalshift.co.uk>
2024-07-28 22:07:11 +0100

    Improve the implementation of `stream_raw` so that the output won't block
    (It does go through an extra step, which might be an issue)

--- pipe/src/commands/command_socket.rs
+++ pipe/src/commands/command_socket.rs
@@ -13,6 +13,7 @@ use futures::{pin_mut};
 use serde_json;
 
 use std::iter;
+use std::sync::*;
 
 ///
 /// Data intended to be sent to a command socket (a command socket sends and receives the bytes directly)
@@ -69,65 +70,78 @@ impl CommandSocket {
     ///
     /// Takes over the socket to send raw u8 data
     ///
-    /// This allows commands to perform an interactive session with a user, directly interacting with their connection. It's up to the command when
-    /// to close the streams: we will stop listening for raw data from the other side when the returned input stream is closed
+    /// The `activity_fn` is called back to perform whatever activity is needed on the stream: control is returned once this function has completed.
     ///
-    pub fn stream_raw<'a>(&'a mut self, raw_output_stream: impl 'a + Send + Stream<Item=Vec<u8>>) -> impl 'a + Unpin + Send + Stream<Item=Vec<u8>> {
+    pub async fn stream_raw<'a, TFuture>(&'a mut self, activity_fn: impl 'a + FnOnce(BoxStream<'a, Vec<u8>>, mpsc::Sender<Vec<u8>>) -> TFuture) -> TFuture::Output
+    where
+        TFuture: 'a + Future,
+    {
         use std::mem;
 
         // Fetch the input and output streams
         // TODO: if the buffer is not emptied, then we need to preserve it in the input stream on return
         let mut buffer      = vec![];
         mem::swap(&mut buffer, &mut self.buffer);
+        let buffer          = Arc::new(Mutex::new(buffer));
         let input_stream    = &mut self.input_stream;
         let output_stream   = &mut self.output_stream;
 
-        // Create a future to send the data from the raw stream to the output
-        let send_output_future = async move {
-            pin_mut!(raw_output_stream);
+        // Create a variant of the input stream that reads from the internal buffer first
+        let input_stream_buffer = Arc::clone(&buffer);
 
-            // Read output...
-            while let Some(data) = raw_output_stream.next().await {
-                // ... send to the output stream
-                let send_result = output_stream.send(CommandData(data)).await;
+        let input_stream = stream::poll_fn(move |context| {
+            let mut buffer = input_stream_buffer.lock().unwrap();
+
+            if !buffer.is_empty() {
+                let mut ready_buffer = vec![];
+                mem::swap(&mut ready_buffer, &mut *buffer);
+
+                Poll::Ready(Some(ready_buffer))
+            } else if let Poll::Ready(data) = input_stream.poll_next_unpin(context) {
+                Poll::Ready(data.map(|CommandData(data)| data))
+            } else {
+                Poll::Pending
+            }
+        }).boxed();
+
+        // Create a future for forwarding the output (we could also expose the output sender directly here, note that there's an extra item allowed to be queued for backpressure purposes)
+        let (send_output, recv_output) = mpsc::channel(0);
+
+        let output_relay = async move {
+            // Copy output until the output stream closes
+            let mut recv_output = recv_output;
+            while let Some(output) = recv_output.next().await {
+                let send_result = output_stream.send(CommandData(output)).await;
                 if send_result.is_err() {
-                    // Stop early if there is no more output to send from the raw stream
                     break;
                 }
             }
         };
 
-        // The returned stream reads from the input or the buffer
-        let mut send_output_future = Some(Box::pin(send_output_future));
+        // Start the activity
+        let activity = activity_fn(input_stream, send_output);
 
-        let raw_stream = stream::poll_fn(move |context| {
-            // Poll the output future so any generated output is sent
-            // TODO: this might cause a blockage if the input is not polled when something is trying to send to the output stream
-            if let Some(future) = &mut send_output_future {
-                match future.poll_unpin(context) {
-                    Poll::Ready(()) => { send_output_future = None; }
-                    Poll::Pending   => { }
+        // Run the activity and the output relay as a future
+        let mut output_relay = Some(Box::pin(output_relay));
+        pin_mut!(activity);
+
+        let result = future::poll_fn(move |context| {
+            if let Some(output_relay_future) = &mut output_relay {
+                match output_relay_future.poll_unpin(context) {
+                    Poll::Ready(()) => { output_relay = None; },
+                    Poll::Pending   => { },
                 }
             }
 
-            // Check for any activity on the input stream
-            if !buffer.is_empty() {
-                // If there are buffered bytes, then return those as the very first piece of input
-                let mut ready_buffer = vec![];
-                mem::swap(&mut buffer, &mut ready_buffer);
+            activity.poll_unpin(context)
+        }).await;
 
-                Poll::Ready(Some(ready_buffer))
-            } else if let Poll::Ready(data) = input_stream.poll_next_unpin(context) {
-                // Data is ready on the input stream
-                Poll::Ready(data.map(|CommandData(data)| data))
-            } else {
-                // No data is ready
-                Poll::Pending
-            }
-        });
+        // If the buffer wasn't read, return it to this object
+        let mut buffer = buffer.lock().unwrap();
+        mem::swap(&mut *buffer, &mut self.buffer);
 
-        // Result is the stream from the future
-        raw_stream
+        // Return the value returned from the activity future
+        result
     }
 
     ///
