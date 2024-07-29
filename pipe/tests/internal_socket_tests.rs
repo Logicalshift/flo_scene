5cb2e15047739d54102fe762f5af564ed5be1e2b
Andrew Hunter <andrew@logicalshift.co.uk>
2024-07-29 23:16:59 +0100

    Add a test that we can send raw data to a command stream
    (We aren't generating the 'finish' message yet so this isn't quite working...)

--- pipe/tests/internal_socket_tests.rs
+++ pipe/tests/internal_socket_tests.rs
@@ -233,6 +233,80 @@ fn read_from_background_stream_iteration() {
 }
 
 #[test]
+fn send_raw_data_to_command_connection() {
+    let test_commands   = "test\n".to_string();
+
+    let scene           = Scene::default();
+    let test_program    = SubProgramId::new();
+ 
+    struct TestSucceeded;
+    impl SceneMessage for TestSucceeded { }
+
+    // The command program accepts connections from the socket and interprets the commands
+    let command_program = SubProgramId::new();
+    scene.add_subprogram(command_program, |input, context| command_connection_program(input, context, ()), 0);
+
+    // The internal socket program lets us stream commands and responses via a socket connection
+    let socket_program = SubProgramId::new();
+    start_internal_socket_program(&scene, socket_program, read_command_data, write_command_data).unwrap();
+
+    // Create a test command that sends some data to an interactive stream
+    scene.add_subprogram(SubProgramId::new(), 
+        CommandLauncher::json()
+            .with_json_command("test", |_param: (), _context| async move {
+                CommandResponse::InteractiveStream(Box::new(
+                    |_| stream::iter(vec!["\n\nRAW OUTPUT\n\n".bytes().collect::<Vec<_>>()]).boxed())
+                )
+            })
+            .to_subprogram(), 
+        0);
+
+    // Socket program is connected to the command program using the command program socket message (which generates connections)
+    scene.connect_programs(socket_program, command_program, StreamId::with_message_type::<CommandProgramSocketMessage>()).unwrap();
+
+    // Add another program that talks to the command program via a socket connection
+    scene.add_subprogram(SubProgramId::new(), move |_input: InputStream<()>, context| async move {
+        // Also create an internal buffer to write to
+        let (our_side, their_side)          = duplex(1024);
+        let (command_input, command_output) = split(their_side);
+        let (read_result, write_command)    = split(our_side);
+
+        // Request that the socket program read from the test commands and writes to the internal buffer
+        let mut socket_program = context.send(socket_program).unwrap();
+        socket_program.send(InternalSocketMessage::CreateInternalSocket(Box::new(command_input), Box::new(command_output))).await.ok().unwrap();
+
+        // Send the test command to the writer
+        let mut write_command = write_command;
+        println!("> {:?}", test_commands);
+        write_command.write_all(&test_commands.bytes().collect::<Vec<u8>>()).await.unwrap();
+
+        // Read the interal buffer to get the final result
+        let mut read_result = read_result;
+        let mut characters = String::new();
+        while let Ok(msg) = read_result.read_u8().await {
+            characters.push(msg as char);
+            println!("{:?}", msg as char);
+
+            if characters.contains("\nRAW OUTPUT\n") && characters.contains("\n<< RAW <<\n") /* && characters.contains("\n== RAW ==\n") */ {
+                break;
+            }
+        }
+
+        write_command.shutdown().await.unwrap();
+        println!("DONE");
+
+        // Indicate successs
+        context.send_message(TestSucceeded).await.ok();
+    }, 0);
+
+    // Wait for the test program to indicate that it succeeded
+    TestBuilder::new()
+        .redirect_input(StreamId::with_message_type::<TestSucceeded>())
+        .expect_message(|_: TestSucceeded| Ok(()))
+        .run_in_scene_with_threads(&scene, test_program, 5);
+}
+
+#[test]
 fn read_from_background_stream() {
     read_from_background_stream_iteration();
 }
