a1ffd6e84ab5cd9fad35809f315a0aa1a3dac695
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-01 20:17:57 +0100

    Make the send command just echo back its data for now (which lets us test that the IoStream response works properly)

--- pipe/src/standard_json_commands/send.rs
+++ pipe/src/standard_json_commands/send.rs
@@ -3,6 +3,8 @@ use crate::commands::*;
 use flo_scene::*;
 
 use futures::prelude::*;
+use futures::channel::mpsc;
+use futures::channel::oneshot;
 use serde::*;
 
 ///
@@ -31,14 +33,38 @@ pub fn command_send(destination: SendArguments, context: SceneContext) -> impl F
             SendArguments::Type(type_name) => {
                 if let Some(stream_id) = StreamId::with_serialization_type(type_name) {
                     // Send serialized to a generic stream
-                    todo!()
+                    //todo!()
                 } else {
                     // Err(ConnectionError::TargetNotAvailable)
-                    todo!()
+                    // todo!()
                 }
             }
         };
 
-        CommandResponse::Error("Not implemented".into())
+        let (send_responses, recv_responses)    = mpsc::channel(16);
+        let (send_input, recv_input)            = oneshot::channel();
+
+        // Open an IO stream
+        if context.send_message(CommandResponse::IoStream(Box::new(move |input_stream| {
+                send_input.send(input_stream).ok();
+                recv_responses.boxed()
+            }))).await.is_err() {
+            return CommandResponse::Error("Could not create ouput stream".into());
+        }
+
+        // Wait for the stream to open
+        let input_stream = recv_input.await;
+        let input_stream = if let Ok(input_stream) = input_stream { input_stream } else { return CommandResponse::Error("Input stream did not correct".into()); };
+
+        // TODO: actually serialize the data to send: for the moment we just echo it back again
+        let mut input_stream    = input_stream;
+        let mut send_responses  = send_responses;
+        
+        while let Some(msg) = input_stream.next().await {
+            send_responses.send(msg).await.ok();
+        }
+
+        // Finished
+        CommandResponse::Message("OK".into())
     }
 }
