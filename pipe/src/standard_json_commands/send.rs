78213ad3df0269cc183e45ed01c78acc29796fd5
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-01 19:58:26 +0100

    The arguments to the 'send' command should be the target program (we'll open an IO stream to it)
    ... this also makes me think we should be able to receive the background streams during a 'send' so two-way communication is possible if needed.

--- pipe/src/standard_json_commands/send.rs
+++ pipe/src/standard_json_commands/send.rs
@@ -6,18 +6,39 @@ use futures::prelude::*;
 use serde::*;
 
 ///
-/// The arguments to the connect command
+/// The arguments to the 'Send' command, which sends a stream of one or more messages to a target program
 ///
 #[derive(Clone, Serialize, Deserialize)]
-pub struct SendArguments {
+pub enum SendArguments {
+    /// Send to a specific subprogram (using the subprogram's input type, which must support JSON deserialization)
+    SubProgram(SubProgramId),
 
+    /// Send messages of a specific type to the default target, if there is one
+    Type(String),
 }
 
 ///
 /// The `send` command, which sends messags to a subprogram in a scene
 ///
-pub fn command_send(input: SendArguments, context: SceneContext) -> impl Future<Output=CommandResponse> {
+pub fn command_send(destination: SendArguments, context: SceneContext) -> impl Future<Output=CommandResponse> {
     async move {
+        let connection = match destination {
+            SendArguments::SubProgram(subprogram_id) => {
+                // Send to the subprogram using a serialized JSON stream
+                todo!()
+            },
+
+            SendArguments::Type(type_name) => {
+                if let Some(stream_id) = StreamId::with_serialization_type(type_name) {
+                    // Send serialized to a generic stream
+                    todo!()
+                } else {
+                    // Err(ConnectionError::TargetNotAvailable)
+                    todo!()
+                }
+            }
+        };
+
         CommandResponse::Error("Not implemented".into())
     }
 }
