d9da9b2676391cc188b69d62ae685f274da46693
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-06 22:48:32 +0100

    Add a test for the 'send' command
    (This is producing a hidden panic so this isn't working right yet, problem seems to be when the stream ends while in JSON mode)

---
+++ pipe/tests/standard_command_tests.rs
@@ -0,0 +1,101 @@
+use flo_scene::*;
+use flo_scene::commands::*;
+use flo_scene::programs::*;
+use flo_scene_pipe::*;
+use flo_scene_pipe::commands::*;
+
+use futures::prelude::*;
+use serde::*;
+use serde::*;
+use tokio::io::*;
+
+///
+/// Creates an internal socket program in a scene that can be used to send commands
+///
+fn create_internal_command_socket(scene: &Scene, internal_socket_id: SubProgramId) {
+    // The command connection program receives connections from sockets
+    let command_program = SubProgramId::new();
+    scene.add_subprogram(command_program, |input, context| command_connection_program(input, context, ()), 0);
+
+    // The internal socket program lets us receive connections and send messages to the command program as streams of data
+    start_internal_socket_program(scene, internal_socket_id, read_command_data, write_command_data).unwrap();
+
+    // Connect the internal socket program to the command program
+    scene.connect_programs(internal_socket_id, command_program, StreamId::with_message_type::<CommandProgramSocketMessage>()).unwrap();
+}
+
+///
+/// Adds a subprogram that runs some commands using the internal socket program
+///
+fn add_command_runner(scene: &Scene, internal_socket_id: SubProgramId, commands: impl Into<String>, process_results: impl 'static + Send + Fn(String) -> ()) {
+    // Create an arbitrary program ID
+    let program_id  = SubProgramId::new();
+    let commands    = commands.into();
+
+    scene.add_subprogram(program_id, move |_: InputStream<()>, context| async move {
+        // Create a connection via the internal socket
+        let (our_side, their_side)          = duplex(1024);
+        let (command_input, command_output) = split(their_side);
+        let (read_result, write_command)    = split(our_side);
+
+        let mut socket_program = context.send(internal_socket_id).unwrap();
+        socket_program.send(InternalSocketMessage::CreateInternalSocket(Box::new(command_input), Box::new(command_output))).await.ok().unwrap();
+
+        // Future that writes the commands
+        let write_side = async move {
+            println!("In: {}", commands);
+
+            // Send the commands to the write side and then stop
+            let mut write_command = write_command;
+
+            write_command.write_all(&commands.bytes().collect::<Vec<u8>>()).await.unwrap();
+            write_command.shutdown().await.unwrap();
+        };
+
+        // Future that reads the results and processes them
+        let read_side = async move {
+            let mut bytes = vec![];
+
+            let mut read_result = read_result;
+            while let Ok(msg) = read_result.read_u8().await {
+                bytes.push(msg);
+            }
+
+            let string_result = String::from_utf8_lossy(&bytes);
+            println!("\nOut: {}", string_result);
+            process_results(string_result.into());
+        };
+
+        // Wait for both futures together to run the socket
+        future::join(write_side, read_side).await;
+    }, 0)
+}
+
+#[test]
+fn send_test_message() {
+    let scene           = Scene::default().with_standard_json_commands();
+    let internal_socket = SubProgramId::new();
+    let test_program    = SubProgramId::new();
+ 
+    // Create a message we can send to the test program to indicate success
+    #[derive(Serialize, Deserialize)]
+    struct TestSucceeded { message: String }
+    impl SceneMessage for TestSucceeded { }
+
+    scene.with_serializer(|| serde_json::value::Serializer)
+        .with_serializable_type::<TestSucceeded>("test::TestSucceeded");
+
+    // Set up the internal socket and the test case
+    create_internal_command_socket(&scene, internal_socket);
+    add_command_runner(&scene, internal_socket, 
+        r#"send { "Type": "test::TestSucceeded" }
+        { "message": "test" }
+        "#, 
+        |_| { });
+
+    // Create a test program that receives the TestSucceeded message
+    TestBuilder::new()
+        .redirect_input(StreamId::with_message_type::<TestSucceeded>())
+        .expect_message(|_: TestSucceeded| Ok(()))
+        .run_in_scene_with_threads(&scene, test_program, 5);
+}
