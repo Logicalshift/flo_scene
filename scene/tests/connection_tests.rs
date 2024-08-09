3523b95ec52733edd58babcba5866aca5949556f
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-09 09:54:11 +0100

    Add a test that starts a subprogram after creating the stream that communicates with it
    ... This manages to be a problem because the message is being sent to the test program by mistake

--- scene/tests/connection_tests.rs
+++ scene/tests/connection_tests.rs
@@ -389,6 +389,48 @@ pub fn connect_default_subprograms_before_creating() {
 }
 
 #[test]
+pub fn connect_default_subprograms_before_launching() {
+    // Scene with two programs that we'll connect together
+    let scene           = Scene::default();
+    let test_program    = SubProgramId::new();
+    let program_1       = SubProgramId::new();
+    let program_2       = SubProgramId::new();
+
+    // Specify that any strings get sent to program 2, before the scene is started
+    scene.connect_programs((), program_2, StreamId::with_message_type::<String>()).unwrap();
+
+    // Program_1 sends a string message
+    scene.add_subprogram(program_1, |_: InputStream<()>, context| {
+        async move {
+            // Connect to the default target for sending strings. It's configured as program_2 but that's not running yet
+            let mut send_strings = context.send(()).unwrap();
+
+            context.wait_for_idle(100).await;
+
+            // Start the receiver program after we've started sending the strings
+            context.send_message(SceneControl::start_program(program_2, move |input: InputStream<String>, context| {
+                async move {
+                    let mut test_program = context.send(test_program).unwrap();
+
+                    let mut input = input;
+                    while let Some(input) = input.next().await {
+                        panic!("{:?}", input);
+                        test_program.send(input).await.unwrap();
+                    }
+                }
+            }, 0)).await.unwrap();
+
+            send_strings.send("Test".to_string()).await.unwrap();
+        }
+    }, 0);
+
+    // Check that we receive the test message
+    TestBuilder::new()
+        .expect_message(|_msg: String| { Ok(()) })
+        .run_in_scene(&scene, test_program);
+}
+
+#[test]
 pub fn connect_two_subprograms_after_creating_stream_using_filter_target() {
     // Scene with two programs that we'll connect together
     let scene           = Scene::default();
