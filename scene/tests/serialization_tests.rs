6d8c33117224d014ba61d63715a7ebaf1130b386
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-15 22:41:49 +0100

    Test serializing the other way

--- scene/tests/serialization_tests.rs
+++ scene/tests/serialization_tests.rs
@@ -134,6 +134,61 @@ mod with_serde_support {
     }
 
     #[test]
+    fn query_response_serializer() {
+        // Create a scene that will serialize and deserialize the message
+        let scene = Scene::default();
+        scene.with_serializer(|| serde_json::value::Serializer)
+            .with_serializable_type::<TestMessage>("flo_scene::TestMessage");
+
+        // Add a serialized_resender program that sends whatever serialized message it gets to the test program
+        let test_program            = SubProgramId::new();
+        let serialized_resender     = SubProgramId::new();
+        let deserialized_receiver   = SubProgramId::new();
+
+        scene.add_subprogram(serialized_resender, 
+            move |input_stream, context| async move {
+                let mut input_stream = input_stream;
+
+                while let Some(message) = input_stream.next().await {
+                    let mut response: QueryResponse<SerializedMessage<serde_json::Value>> = message;
+
+                    while let Some(message) = response.next().await {
+                        println!("Serialized: {:?}", message.0);
+
+                        context.send(deserialized_receiver).unwrap()
+                            .send(message)
+                            .await
+                            .unwrap();
+                    }
+                }
+            }, 0);
+
+        // The deserialized receiver takes TestMessages and passes them back to the test program
+        scene.add_subprogram(deserialized_receiver, move |input_stream, context| async move {
+            let mut input_stream = input_stream;
+
+            while let Some(message) = input_stream.next().await {
+                let message: TestMessage = message;
+
+                println!("Deserialized: {:?}", message);
+
+                context.send(test_program).unwrap()
+                    .send(message)
+                    .await
+                    .unwrap();
+            }
+        }, 0);
+
+        // Run some tests with a message that gets serialized and deserialized
+        TestBuilder::new()
+            .send_message_to_target(serialized_resender, QueryResponse::with_data(TestMessage::StringValue(format!("Test"))))
+            .expect_message(|msg: TestMessage| {
+                if msg != TestMessage::StringValue(format!("Test")) { Err(format!("Expected 'Test' (got {:?})", msg)) } else { Ok(()) }
+            })
+            .run_in_scene(&scene, test_program);
+    }
+
+    #[test]
     fn query_response_deserializer() {
         // Create a scene that will serialize a message, then deserialize it via a query response deserializer
         let scene = Scene::default();
