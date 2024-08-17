9dc64f00253783e5982103003d601dc57308c7f0
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-17 22:53:10 +0100

    Make the idle request program avoid notifying anything where the request was made after the notification was sent (this can cause some odd behaviour when it happens)

--- scene/src/programs/idle_request.rs
+++ scene/src/programs/idle_request.rs
@@ -65,7 +65,7 @@ impl SceneMessage for IdleNotification {
 ///
 enum IdleProgramMsg {
     Request(SubProgramId, IdleRequest),
-    CoreIsIdle
+    CoreIsIdle(usize)
 }
 
 ///
@@ -86,7 +86,7 @@ pub (crate) async fn idle_subprogram(input_stream: InputStream<IdleRequest>, con
     }
 
     // Merge the notifications (idle notifications and requests)
-    let mut input_stream = stream::select(input_stream.map(|(subprogram_id, msg)| IdleProgramMsg::Request(subprogram_id, msg)), recv_idle.map(|_| IdleProgramMsg::CoreIsIdle));
+    let mut input_stream = stream::select(input_stream.map(|(subprogram_id, msg)| IdleProgramMsg::Request(subprogram_id, msg)), recv_idle.map(|count| IdleProgramMsg::CoreIsIdle(count)));
 
     while let Some(request) = input_stream.next().await {
         use IdleProgramMsg::*;
@@ -95,11 +95,12 @@ pub (crate) async fn idle_subprogram(input_stream: InputStream<IdleRequest>, con
         match request {
             Request(_, WhenIdle(send_message_to)) => {
                 // Add to the list of subprograms to send a message to when an idle request comes in (we'll send multiple notifications if the same program has requested them)
-                pending_notifications.push(send_message_to); 
 
                 // Set the core to notify us when idle
                 if let Some(core) = weak_core.upgrade() {
-                    SceneCore::notify_on_next_idle(&core);
+                    let idle_count = SceneCore::notify_on_next_idle(&core);
+
+                    pending_notifications.push((idle_count, send_message_to)); 
                 }
             },
 
@@ -126,11 +127,28 @@ pub (crate) async fn idle_subprogram(input_stream: InputStream<IdleRequest>, con
                 }
             },
 
-            CoreIsIdle => {
+            CoreIsIdle(idle_count) => {
                 // Do nothing if the notifications are suppressed
                 if suppressions.is_empty() {
+                    // Send any pending idle notification that was queued before we reached idle_count (the value passed in here will be the value returned by the next call to notify_on_next_idle)
+                    let mut ready_notifications         = vec![];
+                    let mut new_pending_notifications   = vec![];
+
+                    for (expected_count, program_id) in pending_notifications.into_iter() {
+                        if expected_count < idle_count {
+                            // Anything in the ready_notifications will be sent
+                            ready_notifications.push(program_id);
+                        } else {
+                            // Anything in the 'new' list will keep running
+                            // Very rarely, it's possible for an idle notification to be added while a previous request is notifying (we should wait for the next request)
+                            new_pending_notifications.push((expected_count, program_id));
+                        }
+                    }
+
+                    pending_notifications = new_pending_notifications;
+
                     // Send notifications to everything that's waiting
-                    future::join_all(pending_notifications.drain(..)
+                    future::join_all(ready_notifications.into_iter()
                         .flat_map(|program_id| context.send(program_id).ok())
                         .map(|mut stream| async move { stream.send(IdleNotification).await.ok(); }))
                         .await;
