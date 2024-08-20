5f691437285f5f5648b3e692c90e12afb0077f9d
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-20 20:42:12 +0100

    Add a very basic futures test
    We'll want to be able to call this from wasmer in order to use wasm subprograms

--- wasm-examples/basic_test/src/lib.rs
+++ wasm-examples/basic_test/src/lib.rs
@@ -1,6 +1,22 @@
+use flo_scene::*;
+use flo_scene::programs::*;
+
+use futures::prelude::*;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn test() {
-
+pub async fn test() -> i32 {
+    future::ready(42).await
 }
+
+/*
+#[wasm_bindgen]
+pub async fn test(input: InputStream<String>, context: SceneContext) {
+    let mut input = input;
+
+    while let Some(msg) = input.next().await {
+        context.send_message(TextOutput::Line(msg)).await;
+    }
+}
+*/
