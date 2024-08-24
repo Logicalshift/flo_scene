30387d1b8ac7050864c8b014eb39b1e801d745c3
Andrew Hunter <andrew@logicalshift.co.uk>
2024-08-24 16:55:35 +0100

    Load modules when requested, send updates to the target if one is provided

--- wasm/src/control_subprogram.rs
+++ wasm/src/control_subprogram.rs
@@ -1,8 +1,11 @@
+use super::module::*;
 use super::wasm_control::*;
 
 use flo_scene::*;
 
 use futures::prelude::*;
+
+use std::collections::{HashMap};
 
 ///
 /// A subprogram that loads and runs subprograms written in WASM
@@ -10,12 +13,44 @@ use futures::prelude::*;
 pub async fn wasm_control_subprogram(input: InputStream<WasmControl>, context: SceneContext) {
     let mut input = input;
 
+    let mut modules = HashMap::new();
+    let mut targets = HashMap::new();
+
     while let Some(instruction) = input.next().await {
         use WasmControl::*;
 
         match instruction {
-            LoadModule(module_id, module_bytes, update_target)  => { }
-            RunModule(module_id, program_id)                    => { }
+            LoadModule(module_id, module_bytes, update_target) => {
+                // Load the module as a bare module
+                let new_module = WasmModule::load_bare_module(&module_bytes);
+
+                // Open a connection to the update target
+                let mut update_stream = update_target.clone().and_then(|target| context.send(target).ok());
+
+                match new_module {
+                    Ok(new_module) => {
+                        // Store the new module
+                        modules.insert(module_id, new_module);
+                        targets.insert(module_id, update_target.clone());
+
+                        // Tell the target about the new module
+                        if let Some(update_stream) = &mut update_stream {
+                            update_stream.send(WasmUpdate::ModuleLoaded(module_id)).await.ok();
+                        }
+                    }
+
+                    Err(err) => {
+                        // Tell the target about the failure
+                        if let Some(update_stream) = &mut update_stream {
+                            update_stream.send(WasmUpdate::CouldNotLoadModule(module_id, err)).await.ok();
+                        }
+                    }
+                }
+            }
+
+            RunModule(module_id, program_id) => {
+
+            }
         }
     }
 }
