1d96ad85ddc499d8dd0c5fb37dd94e877611710c
Andrew Hunter <andrew@logicalshift.co.uk>
2024-09-11 21:11:51 +0100

    Check that the future is started on the first poll, and that it's woken up to send the message

--- scene/tests/guest/send_messages_tests.rs
+++ scene/tests/guest/send_messages_tests.rs
@@ -20,16 +20,27 @@ impl GuestSceneMessage for SimpleTestMessage { }
 pub fn send_json_message_to_runtime() {
     // The results from the guest (we're not doing any isolation stuff so we can share variables this way)
     let received = Arc::new(Mutex::new(vec![]));
+    let woken    = Arc::new(Mutex::new(false));
 
     // Create a runtime that receives messages using the JSON encoder
     let encoder         = GuestJsonEncoder;
     let messages        = Arc::clone(&received);
+    let awake           = Arc::clone(&woken);
     let guest_runtime   = GuestRuntime::with_default_subprogram(encoder, move |input_stream: GuestInputStream<SimpleTestMessage>, _context| async move {
+        (*awake.lock().unwrap()) = true;
+
         let mut input_stream = input_stream;
         while let Some(msg) = input_stream.next().await {
             messages.lock().unwrap().push(msg);
         }
     });
+
+    // Initially shouldn't be woken up
+    assert!(*woken.lock().unwrap() == false);
+
+    // Poll once to make the loop start waiting (we can send messages before this point: want to test that we'll wake the thread up again)
+    let _result = guest_runtime.poll_awake();
+    assert!(*woken.lock().unwrap() == true);
 
     // Enqueue a message for the runtime (the default subprogram always has the same handle)
     let data = SimpleTestMessage { value: "Test".into() }.serialize(serde_json::value::Serializer).unwrap();
@@ -39,7 +50,7 @@ pub fn send_json_message_to_runtime() {
     // Polling the runtime once should clear the pending message
     let _result = guest_runtime.poll_awake();
 
-    // Message should have been received
+    // Message should have been received and properly decoded
     let received = received.lock().unwrap();
     assert!(received.len() == 1, "{:?}", received);
     assert!(received[0] == SimpleTestMessage { value: "Test".into() }, "{:?}", received);
