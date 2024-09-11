174633d0b7598eed5e57dcfcee2937d148187e3c
Andrew Hunter <andrew@logicalshift.co.uk>
2024-09-11 20:44:03 +0100

    Make it so that you can call GuestSubProgramHandle::default() for the initial subprogram

--- scene/src/guest/subprogram_handle.rs
+++ scene/src/guest/subprogram_handle.rs
@@ -3,3 +3,11 @@
 ///
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct GuestSubProgramHandle(pub usize);
+
+/// The default subprogram handle refers to the initial guest subprogram
+impl Default for GuestSubProgramHandle {
+    #[inline]
+    fn default() -> Self {
+        GuestSubProgramHandle(0)
+    }
+}
