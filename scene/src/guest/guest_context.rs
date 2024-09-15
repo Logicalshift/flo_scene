df978c375a83b8ec1e3d920c4e476587b94650d7
Andrew Hunter <andrew@logicalshift.co.uk>
2024-09-15 20:34:30 +0100

    Add in some comments from the scene context for the guest context

--- scene/src/guest/guest_context.rs
+++ scene/src/guest/guest_context.rs
@@ -25,10 +25,26 @@ impl<TEncoder> GuestSceneContext<TEncoder>
 where
     TEncoder: 'static + GuestMessageEncoder,
 {
+    ///
+    /// Returns the currently active subprogram, if there is one
+    ///
+    /// This will return 'None' if the scene that the program was running in is terminated but the
+    /// task is still running, so this is a very rare occurrence. 
+    ///
     pub fn current_program_id(&self) -> Option<SubProgramId> {
         Some(self.subprogram_id)
     }
 
+    ///
+    /// Retrieves a stream for sending messages of the specified type
+    ///
+    /// The target can be used to define the default destination for the stream. If the target is a specific program, that program should
+    /// have an input type that matches the message type. If the target is `None` or `Any`, the stream can be connected by the scene (by the
+    /// `connect_programs()` request), so the exact target does not need to be known.
+    ///
+    /// The `None` target will discard any messages received while the stream is disconnected, but the `Any` target will block until something
+    /// connects the stream. Streams with a specified target will connect to that target immediately.
+    ///
     pub fn send<TMessageType>(&self, target: impl Into<StreamTarget>) -> Result<impl Unpin + Sink<TMessageType, Error=SceneSendError<Vec<u8>>>, ConnectionError>
     where
         TMessageType: 'static + SceneMessage + GuestSceneMessage,
@@ -76,6 +92,9 @@ where
         }))
     }
 
+    ///
+    /// Sends a single message to the default output of that type
+    ///
     pub async fn send_message<TMessageType>(&self, message: TMessageType) -> Result<(), ConnectionError> 
     where
         TMessageType: 'static + SceneMessage + GuestSceneMessage,
