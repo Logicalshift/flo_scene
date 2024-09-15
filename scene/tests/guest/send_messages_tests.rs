aa71275830950934c3ad088974239a436dfe4ae8
Andrew Hunter <andrew@logicalshift.co.uk>
2024-09-15 15:34:07 +0100

    Add a couple of tests that check that we can send messages to a sink from the guest and that the flow control is working properly

--- scene/tests/guest/send_messages_tests.rs
+++ scene/tests/guest/send_messages_tests.rs
@@ -65,3 +65,116 @@ pub fn send_json_message_to_runtime() {
     let result = guest_runtime.poll_awake();
     assert!(!result.contains(&GuestResult::Ready(GuestSubProgramHandle::default())));
 }
+
+#[test]
+pub fn receive_message_from_runtime() {
+    // Create a runtime that sends a message to the host
+    let encoder         = GuestJsonEncoder;
+    let guest_runtime   = GuestRuntime::with_default_subprogram(SubProgramId::new(), encoder, move |_input_stream: GuestInputStream<SimpleTestMessage>, context| async move {
+        // Send the message to the default target
+        let mut message_sink = context.send::<SimpleTestMessage>(()).unwrap();
+        message_sink.send(SimpleTestMessage { value: "From remote".into() }).await.unwrap();
+    });
+
+    // We now need to send the expected responses back for the sink that was just opened up
+
+    // The runtime will request a connection
+    let connect_result      = guest_runtime.poll_awake();
+    let mut connect_request = connect_result.into_iter().filter(|msg| matches!(msg, GuestResult::Connect(_, _))).collect::<Vec<_>>();
+
+    assert!(connect_request.len() == 1);
+
+    let (sink_handle, stream_target) = if let GuestResult::Connect(handle, stream_target) = connect_request.pop().unwrap() { 
+        (handle, stream_target)
+    } else {
+        unreachable!()
+    };
+
+    assert!(stream_target == HostStreamTarget::Any(SimpleTestMessage::stream_id()));
+
+    // We need to send a connection back
+    guest_runtime.sink_ready(sink_handle);
+
+    // Guest should now send us the first message
+    let send_result = guest_runtime.poll_awake();
+    let mut send_result = send_result.into_iter().filter(|msg| matches!(msg, GuestResult::Send(_, _))).collect::<Vec<_>>();
+
+    assert!(send_result.len() != 2, "{:?}", send_result);
+    assert!(send_result.len() == 1, "{:?}", send_result);
+
+    let (send_sink_handle, data) = if let GuestResult::Send(handle, data) = send_result.pop().unwrap() { 
+        (handle, data)
+    } else {
+        unreachable!()
+    };
+
+    assert!(send_sink_handle == sink_handle);
+    let decoded = GuestJsonEncoder.decode::<SimpleTestMessage>(data);
+    assert!(decoded.value == "From remote");
+}
+
+#[test]
+pub fn receive_several_messages_from_runtime() {
+    // Create a runtime that sends a message to the host
+    let encoder         = GuestJsonEncoder;
+    let guest_runtime   = GuestRuntime::with_default_subprogram(SubProgramId::new(), encoder, move |_input_stream: GuestInputStream<SimpleTestMessage>, context| async move {
+        // Send the message to the default target
+        let mut message_sink = context.send::<SimpleTestMessage>(()).unwrap();
+        message_sink.send(SimpleTestMessage { value: "From remote".into() }).await.unwrap();
+        message_sink.send(SimpleTestMessage { value: "Another message".into() }).await.unwrap();
+    });
+
+    // We now need to send the expected responses back for the sink that was just opened up
+
+    // The runtime will request a connection
+    let connect_result      = guest_runtime.poll_awake();
+    let mut connect_request = connect_result.into_iter().filter(|msg| matches!(msg, GuestResult::Connect(_, _))).collect::<Vec<_>>();
+
+    assert!(connect_request.len() == 1);
+
+    let (sink_handle, stream_target) = if let GuestResult::Connect(handle, stream_target) = connect_request.pop().unwrap() { 
+        (handle, stream_target)
+    } else {
+        unreachable!()
+    };
+
+    assert!(stream_target == HostStreamTarget::Any(SimpleTestMessage::stream_id()));
+
+    // We need to send a connection back
+    guest_runtime.sink_ready(sink_handle);
+
+    // Guest should now send us the first message (second won't arrive until we re-signal that we're ready to receive it)
+    let send_result = guest_runtime.poll_awake();
+    let mut send_result = send_result.into_iter().filter(|msg| matches!(msg, GuestResult::Send(_, _))).collect::<Vec<_>>();
+
+    assert!(send_result.len() != 2, "{:?}", send_result);
+    assert!(send_result.len() == 1, "{:?}", send_result);
+
+    let (send_sink_handle, data) = if let GuestResult::Send(handle, data) = send_result.pop().unwrap() { 
+        (handle, data)
+    } else {
+        unreachable!()
+    };
+
+    assert!(send_sink_handle == sink_handle);
+    let decoded = GuestJsonEncoder.decode::<SimpleTestMessage>(data);
+    assert!(decoded.value == "From remote");
+
+    // Indicating 'ready' again should trigger the second message
+    guest_runtime.sink_ready(sink_handle);
+
+    let send_result = guest_runtime.poll_awake();
+    let mut send_result = send_result.into_iter().filter(|msg| matches!(msg, GuestResult::Send(_, _))).collect::<Vec<_>>();
+
+    assert!(send_result.len() == 1);
+
+    let (send_sink_handle, data) = if let GuestResult::Send(handle, data) = send_result.pop().unwrap() { 
+        (handle, data)
+    } else {
+        unreachable!()
+    };
+
+    assert!(send_sink_handle == sink_handle);
+    let decoded = GuestJsonEncoder.decode::<SimpleTestMessage>(data);
+    assert!(decoded.value == "Another message");
+}
