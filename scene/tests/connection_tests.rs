a8789484c22424a6424685f3dbca96f935b87f21
Andrew Hunter <andrew@logicalshift.co.uk>
2024-09-30 09:50:56 +0100

    Add a connection test that connects just a single program to a target
    This is failing, surprised I didn't think of this as a test case before

--- scene/tests/connection_tests.rs
+++ scene/tests/connection_tests.rs
@@ -1088,3 +1088,65 @@ pub fn connect_default_after_creating_stream_using_filter_target() {
         .expect_message(|_msg: TestResult| { Ok(()) })
         .run_in_scene(&scene, test_program);
 }
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct SimpleTestMessage {
+    value: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct SimpleResponseMessage {
+    value: String,
+}
+
+impl SceneMessage for SimpleTestMessage {
+    fn message_type_name() -> String {
+        "flo_scene_tests::guest_subprogram_tests::SimpleTestMessage".into()
+    }
+}
+
+impl SceneMessage for SimpleResponseMessage {
+    fn message_type_name() -> String {
+        "flo_scene_tests::guest_subprogram_tests::SimpleResponseMessage".into()
+    }
+}
+
+#[test]
+fn connect_single_source_to_single_target() {
+    let scene = Scene::default();
+
+    let guest_subprogram_id     = SubProgramId::called("Guest subprogram");
+    let sender_subprogram_id    = SubProgramId::called("Sender subprogram");
+    let test_subprogram_id      = SubProgramId::called("Test subprogram");
+
+    // This is the program we'll run as a guest in the other tests ()
+    scene.add_subprogram(guest_subprogram_id, move |input_stream: InputStream<SimpleTestMessage>, context| async move {
+        // Send responses to the defualt target for the scene
+        let mut response = context.send::<SimpleResponseMessage>(()).unwrap();
+
+        let mut input_stream = input_stream;
+        while let Some(msg) = input_stream.next().await {
+            println!("Received message: {:?}", msg);
+
+            response.send(SimpleResponseMessage { value: msg.value }).await.unwrap();
+
+            println!("Sent message");
+        }
+    }, 10);
+
+    // Run another program to send messages to the first one
+    scene.add_subprogram(sender_subprogram_id, move |_input: InputStream<()>, context| async move {
+        let mut test_messages = context.send(guest_subprogram_id).unwrap();
+
+        test_messages.send(SimpleTestMessage { value: "Hello".into() }).await.unwrap();
+        test_messages.send(SimpleTestMessage { value: "Goodbyte".into() }).await.unwrap();
+    }, 0);
+
+    // Connect the programs
+    scene.connect_programs(guest_subprogram_id, test_subprogram_id, StreamId::with_message_type::<SimpleResponseMessage>()).unwrap();
+
+    TestBuilder::new()
+        .expect_message(|_: SimpleResponseMessage| { Ok(()) })
+        .expect_message(|_: SimpleResponseMessage| { Ok(()) })
+        .run_in_scene(&scene, test_subprogram_id);
+}
