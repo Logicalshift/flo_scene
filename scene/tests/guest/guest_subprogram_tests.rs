9fab3a38605c921d5f6542554151ef9c1c44cf6c
Andrew Hunter <andrew@logicalshift.co.uk>
2024-10-03 20:15:10 +0100

    Add a test case that was failing before, using a specific connection for the guest subprogram

--- scene/tests/guest/guest_subprogram_tests.rs
+++ scene/tests/guest/guest_subprogram_tests.rs
@@ -111,3 +111,47 @@ fn run_basic_guest_subprogram() {
         .expect_message(|msg: SimpleResponseMessage| { if msg.value == "Goodbyte" { Ok(()) } else { Err(format!("Value is {} (should be Goodbyte)", msg.value)) } })
         .run_in_scene(&scene, test_subprogram_id);
 }
+
+#[test]
+fn run_basic_guest_subprogram_using_specific_connection() {
+    let scene = Scene::default();
+
+    let guest_subprogram_id     = SubProgramId::called("Guest subprogram");
+    let sender_subprogram_id    = SubProgramId::called("Sender subprogram");
+    let test_subprogram_id      = SubProgramId::called("Test subprogram");
+
+    // Start a guest runtime that mirrors messages
+    let guest_runtime = GuestRuntime::with_default_subprogram(guest_subprogram_id, GuestJsonEncoder, move |input_stream: GuestInputStream<SimpleTestMessage>, context| async move {
+        // Send responses to the defualt target for the scene
+        let mut response = context.send::<SimpleResponseMessage>(()).unwrap();
+
+        let mut input_stream = input_stream;
+        while let Some(msg) = input_stream.next().await {
+            println!("Received message: {:?}", msg);
+
+            response.send(SimpleResponseMessage { value: msg.value }).await.unwrap();
+
+            println!("Sent message");
+        }
+    });
+
+    // Run the guest in the scene, using the JSON encoder
+    let (sender, receiver) = guest_runtime.as_streams();
+    scene.add_subprogram(guest_subprogram_id, move |input: InputStream<SimpleTestMessage>, context| run_host_subprogram(input, context, GuestJsonEncoder, sender, receiver), 20);
+
+    // Run another program to send messages to the first one
+    scene.add_subprogram(sender_subprogram_id, move |_input: InputStream<()>, context| async move {
+        let mut test_messages = context.send(guest_subprogram_id).unwrap();
+
+        test_messages.send(SimpleTestMessage { value: "Hello".into() }).await.unwrap();
+        test_messages.send(SimpleTestMessage { value: "Goodbyte".into() }).await.unwrap();
+    }, 0);
+
+    // Connect the programs
+    scene.connect_programs(guest_subprogram_id, test_subprogram_id, StreamId::with_message_type::<SimpleResponseMessage>()).unwrap();
+
+    TestBuilder::new()
+        .expect_message(|msg: SimpleResponseMessage| { if msg.value == "Hello" { Ok(()) } else { Err(format!("Value is {} (should be Hello)", msg.value)) } })
+        .expect_message(|msg: SimpleResponseMessage| { if msg.value == "Goodbyte" { Ok(()) } else { Err(format!("Value is {} (should be Goodbyte)", msg.value)) } })
+        .run_in_scene(&scene, test_subprogram_id);
+}
