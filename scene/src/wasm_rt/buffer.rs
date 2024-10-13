b8f2291bc7839b37e5830d2391d5986026d3f735
Andrew Hunter <andrew@logicalshift.co.uk>
2024-10-13 22:34:42 +0100

    Add a way to assign buffer handles and return them to the host

--- scene/src/wasm_rt/buffer.rs
+++ scene/src/wasm_rt/buffer.rs
@@ -3,8 +3,10 @@ use once_cell::sync::{Lazy};
 use std::cell::{UnsafeCell};
 use std::collections::{HashMap};
 use std::sync::*;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 static BUFFERS: Lazy<Mutex<HashMap<BufferHandle, UnsafeCell<Vec<u8>>>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+static NEXT_BUFFER: AtomicUsize = AtomicUsize::new(0);
 
 ///
 /// Handle to a buffer in a scene (these are used for transferring data to and from a webassembly module)
@@ -12,6 +14,24 @@ static BUFFERS: Lazy<Mutex<HashMap<BufferHandle, UnsafeCell<Vec<u8>>>>> = Lazy::
 #[derive(Clone, Copy, PartialEq, Debug, Eq, Hash)]
 #[repr(transparent)]
 pub struct BufferHandle(pub usize);
+
+impl BufferHandle {
+    ///
+    /// Allocates a new buffer
+    ///
+    #[inline]
+    pub fn new() -> Self {
+        BufferHandle(NEXT_BUFFER.fetch_add(1, Ordering::Relaxed))
+    }
+}
+
+///
+/// Creates a new buffer on the guest side (this should be used so that no buffers can clash)
+///
+#[no_mangle]
+pub unsafe extern "C" fn scene_new_buffer() -> BufferHandle {
+    BufferHandle::new()
+}
 
 ///
 /// Borrows a buffer until scene_return_buffer is called to return the buffer to the store
