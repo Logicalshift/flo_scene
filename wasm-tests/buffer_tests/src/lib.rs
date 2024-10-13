39e97b327a9a18c5964c78beeb9096828bc99624
Andrew Hunter <andrew@logicalshift.co.uk>
2024-10-13 11:08:16 +0100

    Add a workspace for compiling the test code for wasm, with a test of the buffering routine to start with

---
+++ wasm-tests/buffer_tests/src/lib.rs
@@ -0,0 +1,12 @@
+use flo_scene::wasm_rt::*;
+
+#[no_mangle]
+pub fn buffer_contents_are_1234() -> bool {
+    let buffer = claim_buffer(1);
+
+    if buffer.len() != 4 || buffer[0] != 1 || buffer[1] != 2 || buffer[2] != 3 || buffer[3] != 4 {
+        false
+    } else {
+        true
+    }
+}
