b7a284cbf80ae59f58c88866b7c9329fd5c84220
Andrew Hunter <andrew@logicalshift.co.uk>
2024-10-13 16:58:43 +0100

    Add an equivalent runtime using postcard (we want to see how much smaller it is...)

--- scene/src/wasm_rt/guest_runtime.rs
+++ scene/src/wasm_rt/guest_runtime.rs
@@ -97,3 +97,80 @@ mod json_runtime {
         runtime.sink_send_error(sink, error);
     }
 }
+
+#[cfg(feature="postcard")]
+pub use postcard_runtime::*;
+
+#[cfg(feature="postcard")]
+mod postcard_runtime {
+    use super::*;
+    use crate::guest::*;
+    use crate::wasm_rt::buffer::*;
+
+    use once_cell::sync::{Lazy};
+
+    use std::collections::{HashMap};
+    use std::sync::*;
+
+    /// Guest runtimes using the Postcard encoding
+    static GUEST_POSTCARD_RUNTIMES: Lazy<Mutex<HashMap<GuestRuntimeHandle, Arc<GuestRuntime<GuestPostcardEncoder>>>>> = Lazy::new(|| Mutex::new(HashMap::new()));
+
+    ///
+    /// Registers a guest runtime and returns the handle which can be passed on to the host side of things
+    ///
+    pub fn register_postcard_runtime(new_runtime: GuestRuntime<GuestPostcardEncoder>) -> GuestRuntimeHandle {
+        // Assign a handle and store in the guest list
+        let handle = allocate_handle();
+        GUEST_POSTCARD_RUNTIMES.lock().unwrap().insert(handle, Arc::new(new_runtime));
+
+        handle
+    }
+
+    ///
+    /// Sends a message to a guest subprogram in a runtime
+    ///
+    #[no_mangle]
+    pub extern "C" fn scene_guest_postcard_send_message(runtime: GuestRuntimeHandle, target: GuestSubProgramHandle, postcard_data: BufferHandle) {
+        // Get the postcard runtime with this ID
+        let runtime = GUEST_POSTCARD_RUNTIMES.lock().unwrap().get(&runtime).unwrap().clone();
+
+        // Retrieve the postcard data buffer from where it was being written by the host
+        let postcard_data = claim_buffer(postcard_data);
+
+        // Send the message to the runtime
+        runtime.send_message(target, postcard_data);
+    }
+
+    ///
+    /// Indicates to a guest subprogram that it is safe to send to a sink
+    ///
+    #[no_mangle]
+    pub extern "C" fn scene_guest_postcard_sink_ready(runtime: GuestRuntimeHandle, sink: HostSinkHandle) {
+        let runtime = GUEST_POSTCARD_RUNTIMES.lock().unwrap().get(&runtime).unwrap().clone();
+        runtime.sink_ready(sink);
+    }
+
+    ///
+    /// Indicates to aguest subprogram that an error ocurred while connecting a sink
+    ///
+    #[no_mangle]
+    pub extern "C" fn scene_guest_postcard_sink_connection_error(runtime: GuestRuntimeHandle, sink: HostSinkHandle, postcard_error: BufferHandle) {
+        let postcard_error  = claim_buffer(postcard_error);
+        let error           = postcard::from_bytes(&postcard_error).unwrap();
+
+        let runtime = GUEST_POSTCARD_RUNTIMES.lock().unwrap().get(&runtime).unwrap().clone();
+        runtime.sink_connection_error(sink, error);
+    }
+
+    ///
+    /// Indicates to a guest subprogram that an error ocurred while sending data to a sink
+    ///
+    #[no_mangle]
+    pub extern "C" fn scene_guest_postcard_sink_send_error(runtime: GuestRuntimeHandle, sink: HostSinkHandle, postcard_error: BufferHandle) {
+        let postcard_error  = claim_buffer(postcard_error);
+        let error           = postcard::from_bytes(&postcard_error).unwrap();
+
+        let runtime = GUEST_POSTCARD_RUNTIMES.lock().unwrap().get(&runtime).unwrap().clone();
+        runtime.sink_send_error(sink, error);
+    }
+}
