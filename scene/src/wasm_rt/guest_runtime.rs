e5751e74fc0f9b8cfec73d5d001d17f04f6347c4
Andrew Hunter <andrew@logicalshift.co.uk>
2024-10-14 22:12:05 +0100

    Put the guest runtime handle in the guest namespace

--- scene/src/wasm_rt/guest_runtime.rs
+++ scene/src/wasm_rt/guest_runtime.rs
@@ -1,14 +1,6 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use crate::guest::*;
 
-///
-/// The guest runtime handle is used by the host side to make requests to a runtime defined on the wasm side.
-/// There can be more than one runtime if needed, though most scenarios can be executed using just a single
-/// runtime. Runtimes can only use one message encoding strategy, so one reason that multiple might be used
-/// is that multiple strategies are in use.
-///
-#[derive(Clone, Copy, PartialEq, Debug, Eq, Hash)]
-#[repr(transparent)]
-pub struct GuestRuntimeHandle(pub usize);
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 ///
 /// Assigns a new guest runtime handle
@@ -27,7 +19,6 @@ pub use json_runtime::*;
 #[cfg(feature="serde_json")]
 mod json_runtime {
     use super::*;
-    use crate::guest::*;
     use crate::wasm_rt::buffer::*;
 
     use once_cell::sync::{Lazy};
@@ -117,7 +108,6 @@ pub use postcard_runtime::*;
 
 mod postcard_runtime {
     use super::*;
-    use crate::guest::*;
     use crate::wasm_rt::buffer::*;
 
     use once_cell::sync::{Lazy};
