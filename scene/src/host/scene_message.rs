556a388e90a11453809b64738e627cb7ec4de9f6
Andrew Hunter <andrew@logicalshift.co.uk>
2024-11-09 13:00:57 +0000

    Remove rkyv from the dependencies
    I really like the idea of using this but it's currently not very practical due to also wanting serde support at the same time.

--- scene/src/host/scene_message.rs
+++ scene/src/host/scene_message.rs
@@ -4,7 +4,6 @@ use crate::host::scene::*;
 use crate::host::serialization::*;
 use crate::host::stream_target::*;
 
-#[cfg(any(feature="json", feature="postcard"))]
 use serde::*;
 
 #[cfg(feature="json")]
