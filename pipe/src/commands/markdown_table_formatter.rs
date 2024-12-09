19087dfb753f977135a1a6de283e55636db61e40
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-09 23:09:30 +0000

    Draw the basic outline of the table

--- pipe/src/commands/markdown_table_formatter.rs
+++ pipe/src/commands/markdown_table_formatter.rs
@@ -13,6 +13,114 @@ use std::cell::{RefCell};
 ///
 /// Formats a table to a target formatter
 ///
-pub fn table_format(target: &mut Formatter, table: &comrak::nodes::NodeTable, table_node_children: comrak::arena_tree::Children<'_, RefCell<comrak::nodes::Ast>>) {
-    
+pub fn table_format<'a>(target: &mut Formatter, table_node: &'a comrak::arena_tree::Node<'a, RefCell<comrak::nodes::Ast>>) {
+    let available_width = target.available_width();
+
+    // Add some padding after the last paragraph
+    target.commit_current_word();
+    target.newline();
+    target.newline();
+
+    //
+    // PASS 1: calculate the widths of the columns in the table
+    //
+    let mut column_widths = vec![];
+
+    for row in table_node.children() {
+        use comrak::nodes::{NodeValue};
+
+        if let NodeValue::TableRow(_) = &row.data.borrow().value {
+            for (column_idx, column) in row.children().enumerate() {
+                // Ensure that there are enough columns to accomodate the current width
+                while column_widths.len() <= column_idx { column_widths.push(0); }
+
+                // Measure the required width of this cell
+                if let NodeValue::TableCell = &column.data.borrow().value {
+                    // Format this cell using the maximum available width
+                    let mut cell_formatter = Formatter::new(available_width, 0);
+                    cell_formatter.format("", "", column.children());
+                    cell_formatter.commit_current_word();
+
+                    column_widths[column_idx] = column_widths[column_idx].max(cell_formatter.max_xpos());
+                }
+            }
+        }
+    }
+
+    // 'No cells' is a weird special case, we just don't do anything for that
+    if column_widths.len() == 0 {
+        return;
+    }
+
+    // TODO: Figure out the actual column widths to use
+    // 3 chars between each column, + 2 chars on the end points
+    let total_width = column_widths.iter().sum::<usize>() + ((column_widths.len()-1) * 3) + 4;
+    let mut last_row_was_header = false;
+
+    //
+    // PASS 2: format the table
+    //
+    for (row_idx, row) in table_node.children().enumerate() {
+        use comrak::nodes::{NodeValue};
+
+        if let NodeValue::TableRow(is_header_row) = &row.data.borrow().value {
+            let mut formatted_cells = vec![];
+
+            // Draw the lines for the header of this row
+            let mut header_lines = String::new();
+            if row_idx == 0 { header_lines.push('\u{256d}'); } else if last_row_was_header { header_lines.push('\u{255e}'); } else { header_lines.push('\u{251c}'); }
+
+            for (idx, width) in column_widths.iter().copied().enumerate() {
+                let is_last_column = (idx+1) == column_widths.len();
+
+                if last_row_was_header { header_lines.extend((0..(width+2)).map(|_| '\u{2550}')); } else { header_lines.extend((0..(width+2)).map(|_| '\u{2500}')); };
+                if !is_last_column {
+                    if row_idx == 0 { header_lines.push('\u{252c}'); } else if last_row_was_header { header_lines.push('\u{256a}'); } else { header_lines.push('\u{253c}'); }
+                }
+            }
+
+            if row_idx == 0 { header_lines.extend("\u{256e}".chars()); } else if last_row_was_header { header_lines.push('\u{2561}'); } else { header_lines.extend("\u{2524}".chars()); }
+
+            target.append_raw(&header_lines, available_width);
+            target.newline();
+
+            // The header row uses a double line for the formatting
+            last_row_was_header = *is_header_row;
+
+            // Format the text for the cells (which may be multi-line)
+            for (column_idx, column) in row.children().enumerate() {
+                // Ensure that there are enough columns to accomodate the current width
+                while column_widths.len() <= column_idx { column_widths.push(0); }
+
+                // Measure the required width of this cell
+                if let NodeValue::TableCell = &column.data.borrow().value {
+                    // Format this cell using the column width we've decided on. Use padding so it fits in the table
+                    let mut cell_formatter = Formatter::new(column_widths[column_idx], 0);
+                    cell_formatter.set_pad_to_width(true);
+                    cell_formatter.format("", "", column.children());
+                    cell_formatter.commit_current_word();
+
+                    // Add to the formatted cells for the current row
+                    formatted_cells.push(cell_formatter.to_string());
+                }
+            }
+        }
+    }
+
+    // Close off the table
+    let mut header_lines = String::new();
+    header_lines.extend("\u{2514}".chars());
+
+    for (idx, width) in column_widths.iter().copied().enumerate() {
+        let is_last_column = (idx+1) == column_widths.len();
+
+        header_lines.extend((0..(width+2)).map(|_| '\u{2500}'));
+        if !is_last_column {
+            header_lines.push('\u{2534}');
+        }
+    }
+
+    header_lines.extend("\u{2518}".chars());
+
+    target.append_raw(&header_lines, available_width);
 }
