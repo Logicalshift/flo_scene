a1aa4a3b0ff0976a0a2ea2244f2899adb5da21a1
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-24 15:06:03 +0000

    Bring over some of the core type
    There's a bunch of things missing that I've commented out for the moment. One issue is what to do about the mutexes, we probably need a way to redefine these so we can switch between wasm with no threads and other things with multiple threads.
    
    Could be an issue with BoxFutures too as they might need to turn into local futures...

---
+++ guest-nostd/src/runtime/core.rs
@@ -0,0 +1,51 @@
+use crate::guest_types::*;
+use crate::host_types::*;
+use crate::guest_result::*;
+
+use futures::future::{BoxFuture};
+use futures::task::{Waker};
+
+use alloc::collections::{BTreeMap, BTreeSet};
+use alloc::vec::*;
+
+pub (crate) struct GuestRuntimeCore {
+    /// The runner for the futures in this core (None while we're polling it)
+    future_runner: Option<BoxFuture<'static, ()>>,
+
+    /// The future pile, which can be used to schedule new futures on this core
+    // pub (super) future_pile: FuturePile,
+
+    /// Set to true if the waker has been triggered for anything in the future pile
+    pile_is_awake: bool,
+
+    /// The input stream cores used in the runtime
+    // input_streams: BTreeMap<usize, Arc<Mutex<GuestInputStreamCore>>>,
+
+    /// Sink handles
+    // sink_handles: BTreeMap<usize, GuestSink>,
+
+    /// The handle to assign to the next input stream we assign
+    next_stream_handle: usize,
+
+    /// The handle to assign to the next sink that we create
+    next_sink_handle: usize,
+
+    /// Actions and results that are waiting to be returned to the host
+    pub (super) pending_results: Vec<GuestResult>,
+
+    /// The next ID to assign to a serializatble stream on the guest side
+    next_serialization_id: usize,
+
+    /// The streams that are marked as ready on the host side
+    pub (super) ready_streams: BTreeSet<SerializationId>,
+
+    /// The streams that are marked closed (and which still exist on the guest side)
+    pub (super) closed_streams: BTreeSet<SerializationId>,
+
+    /// Wakers to notify when a stream becomes ready or is closed
+    pub (super) when_ready: BTreeMap<SerializationId, Option<Waker>>,
+
+    /// The streams with pending data from the host side
+    // pending_streams: BTreeSet<SerializationId, Arc<Mutex<GuestStreamCore>>>,
+    nothing: ()
+}
