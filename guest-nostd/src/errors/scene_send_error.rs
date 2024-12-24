e95f92615150d29068ba24777b27e370053ecba0
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-24 21:04:42 +0000

    Need the map function for the serialization context

--- guest-nostd/src/errors/scene_send_error.rs
+++ guest-nostd/src/errors/scene_send_error.rs
@@ -43,3 +43,71 @@ pub enum SceneSendError<TMessage> {
     /// Cannot send/receive a stream or a function because there is no backing connection for the message
     NoConnection(TMessage),
 }
+
+impl<TMessage> SceneSendError<TMessage> {
+    ///
+    /// Returns `Some(message)` if this error contains the message that failed to send
+    ///
+    /// A message might not be returned if the failure occurred after the message was added to the input queue for the
+    /// target program. Additionally, no message is provided for failures that occur while waiting for the input stream
+    /// to become ready.
+    ///
+    pub fn message(&self) -> Option<&TMessage> {
+        match self {
+            SceneSendError::CouldNotConnect(_)                          => None,
+            SceneSendError::TargetProgramEndedBeforeReady               => None,
+            SceneSendError::StreamClosed(msg)                           => Some(msg),
+            SceneSendError::TargetProgramEnded(msg)                     => Some(msg),
+            SceneSendError::StreamDisconnected(msg)                     => Some(msg),
+            SceneSendError::CannotReEnterTargetProgram                  => None,
+            SceneSendError::CannotAcceptMoreInputUntilSceneIsIdle(msg)  => Some(msg),
+            SceneSendError::CannotSerialize(msg, _)                     => Some(msg),
+            SceneSendError::CannotDeserialize(msg, _)                   => Some(msg),
+            SceneSendError::ErrorAfterDeserialization                   => None,
+            SceneSendError::NoConnection(msg)                           => Some(msg),
+        }
+    }
+
+    ///
+    /// Returns `Some(message)` if this error contains the message that failed to send. This version extract the message
+    /// and discards this object. `message()` will return a reference to the message contained within the object.
+    ///
+    /// A message might not be returned if the failure occurred after the message was added to the input queue for the
+    /// target program. Additionally, no message is provided for failures that occur while waiting for the input stream
+    /// to become ready.
+    ///
+    pub fn to_message(self) -> Option<TMessage> {
+        match self {
+            SceneSendError::CouldNotConnect(_)                          => None,
+            SceneSendError::TargetProgramEndedBeforeReady               => None,
+            SceneSendError::StreamClosed(msg)                           => Some(msg),
+            SceneSendError::TargetProgramEnded(msg)                     => Some(msg),
+            SceneSendError::StreamDisconnected(msg)                     => Some(msg),
+            SceneSendError::CannotReEnterTargetProgram                  => None,
+            SceneSendError::CannotAcceptMoreInputUntilSceneIsIdle(msg)  => Some(msg),
+            SceneSendError::CannotDeserialize(msg, _)                   => Some(msg),
+            SceneSendError::CannotSerialize(msg, _)                     => Some(msg),
+            SceneSendError::ErrorAfterDeserialization                   => None,
+            SceneSendError::NoConnection(msg)                           => Some(msg),
+        }
+    }
+
+    ///
+    /// Maps the content of this error to another message type
+    ///
+    pub fn map<TTarget>(self, map_fn: impl FnOnce(TMessage) -> TTarget) -> SceneSendError<TTarget> {
+        match self {
+            SceneSendError::CouldNotConnect(msg)                        => SceneSendError::CouldNotConnect(msg),
+            SceneSendError::TargetProgramEndedBeforeReady               => SceneSendError::TargetProgramEndedBeforeReady,
+            SceneSendError::StreamClosed(msg)                           => SceneSendError::StreamClosed(map_fn(msg)),
+            SceneSendError::TargetProgramEnded(msg)                     => SceneSendError::TargetProgramEnded(map_fn(msg)),
+            SceneSendError::StreamDisconnected(msg)                     => SceneSendError::StreamDisconnected(map_fn(msg)),
+            SceneSendError::CannotReEnterTargetProgram                  => SceneSendError::CannotReEnterTargetProgram,
+            SceneSendError::CannotAcceptMoreInputUntilSceneIsIdle(msg)  => SceneSendError::CannotAcceptMoreInputUntilSceneIsIdle(map_fn(msg)),
+            SceneSendError::CannotDeserialize(msg, error)               => SceneSendError::CannotDeserialize(map_fn(msg), error),
+            SceneSendError::CannotSerialize(msg, error)                 => SceneSendError::CannotSerialize(map_fn(msg), error),
+            SceneSendError::ErrorAfterDeserialization                   => SceneSendError::ErrorAfterDeserialization,
+            SceneSendError::NoConnection(msg)                           => SceneSendError::NoConnection(map_fn(msg)),
+        }
+    }
+}
