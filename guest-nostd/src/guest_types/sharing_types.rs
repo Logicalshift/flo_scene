8646806a2f50998740a44c849b17f0295a3ec197
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-24 16:58:46 +0000

    Fix the sharing types to work properly with spin

--- guest-nostd/src/guest_types/sharing_types.rs
+++ guest-nostd/src/guest_types/sharing_types.rs
@@ -62,38 +62,29 @@ mod one_thread_sharing_types {
     /// Create a new shared item
     #[inline]
     pub fn share<T>(item: T) -> Shared<T> {
-        Arc::new(RefCell::new(item))
+        Arc::new(Mutex::new(item))
     }
 
     /// Accesses a shared value
     #[inline]
     pub fn with_shared<T, TReturn>(shared: &Shared<T>, action: impl FnOnce(&mut T) -> TReturn) -> TReturn {
-        let contents = shared.lock();
-
-        if let Ok(mut contents) = contents {
-            action(&mut *contents)
-        } else {
-            unreachable!()
-        }
+        let mut contents = shared.lock();
+        action(&mut *contents)
     }
 
     /// Creates a weak version of a shared item
     #[inline]
     pub fn shared_downgrade<T>(shared: &Shared<T>) -> WeakShared<T> {
-        Rc::downgrade(shared)
+        Arc::downgrade(shared)
     }
 
     /// Accesses a weak shared value, if possible
     #[inline]
     pub fn with_weak_shared<T, TReturn>(shared: &WeakShared<T>, action: impl FnOnce(&mut T) -> TReturn) -> Option<TReturn> {
         if let Some(shared) = shared.upgrade() {
-            let contents = shared.lock();
+            let mut contents = shared.lock();
 
-            if let Ok(mut contents) = contents {
-                Some(action(&mut *contents))
-            } else {
-                unreachable!()
-            }
+            Some(action(&mut *contents))
         } else {
             None
         }
