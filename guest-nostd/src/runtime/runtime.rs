da36eaf3a0c0244ebba3814aaa028d842e41c523
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-26 12:16:00 +0000

    Use with_shared instead of locking in the runtime

--- guest-nostd/src/runtime/runtime.rs
+++ guest-nostd/src/runtime/runtime.rs
@@ -1,13 +1,20 @@
+use crate::errors::*;
 use crate::guest_types::*;
 use crate::guest_result::*;
 use crate::guest_action::*;
 use crate::host_types::*;
 use crate::util::*;
 use super::core::*;
+use super::input_stream::*;
 use super::serialization_context::*;
+use super::stream_core::*;
 
+use futures::prelude::*;
+use futures::channel::mpsc;
+
+use alloc::boxed::*;
+use alloc::collections::{BTreeMap, BTreeSet, VecDeque};
 use alloc::vec::*;
-use alloc::collections::{BTreeMap, BTreeSet};
 
 ///
 /// The guest runtime runs a set of guest subprograms (providing GuestInputStream and GuestSceneContext functions),
@@ -29,7 +36,7 @@ impl GuestRuntime {
     ///
     pub fn with_default_subprogram<TMessageType, TFuture>(program_id: SubProgramId, subprogram: impl FnOnce(GuestInputStream<TMessageType>, GuestSceneContext) -> TFuture) -> Self 
     where
-        TMessageType:   SceneMessage,
+        TMessageType:   SceneGuestMessage,
         TFuture:        'static + Send + Future<Output=()>,
     {
         // Create the runtime
@@ -54,26 +61,28 @@ impl GuestRuntime {
         let core = GuestRuntimeCore { future_runner, future_pile, pile_is_awake, input_streams, sink_handles, next_stream_handle, next_sink_handle, next_serialization_id, pending_results, ready_streams, closed_streams, when_ready, pending_streams };
         let core = share(core);
 
-        let runtime             = GuestRuntime { core: Arc::clone(&core) };
+        let runtime             = GuestRuntime { core: core.clone() };
         let serialization_ctxt  = GuestSerializationContext::new(&core, &pile);
 
         // Initialise the initial subprogram
         let (_input_handle, input_stream)   = runtime.create_input_stream();
-        let context                         = GuestSceneContext { core: Arc::clone(&core), subprogram_id: program_id, serialization_context: serialization_ctxt };
+        let context                         = GuestSceneContext { core: core.clone(), subprogram_id: program_id, serialization_context: serialization_ctxt };
         let subprogram                      = subprogram(input_stream, context);
         let subprogram                      = async move {
             // Run the program
             subprogram.await;
 
             // Wait for anything else in the future pile to become idle (in particular, to ensure that any streams finish processing)
-            let future_pile = core.lock().unwrap().future_pile.clone();
+            let future_pile = with_shared(&core, |core| core.future_pile.clone());
             future_pile.idle().await;
 
-            // Notify that it has finished (adding to the results means that the runtime will pick up the message later on)
-            core.lock().unwrap().pending_results.push(GuestResult::EndedSubprogram(program_handle));
+            with_shared(&core, |core| {
+                // Notify that it has finished (adding to the results means that the runtime will pick up the message later on)
+                core.pending_results.push(GuestResult::EndedSubprogram(program_handle));
 
-            // TODO: we don't know for sure the core has stopped here, except that there's currently no way to create another subprogram
-            core.lock().unwrap().pending_results.push(GuestResult::Stopped);
+                // TODO: we don't know for sure the core has stopped here, except that there's currently no way to create another subprogram
+                core.pending_results.push(GuestResult::Stopped);
+            });
         };
 
         pile.add_future(subprogram);
@@ -86,7 +95,7 @@ impl GuestRuntime {
     /// Creates a guest input stream in this runtime, returning the stream and the handle for the stream
     ///
     #[inline]
-    pub fn create_input_stream<TMessageType: SceneMessage>(&self) -> (usize, GuestInputStream<TMessageType>) {
+    pub fn create_input_stream<TMessageType: SceneGuestMessage>(&self) -> (usize, GuestInputStream<TMessageType>) {
         GuestRuntimeCore::create_input_stream(&self.core)
     }
 
@@ -116,9 +125,7 @@ impl GuestRuntime {
     /// Flags that a sink is ready to receive data
     ///
     pub fn sink_ready(&self, HostSinkHandle(sink): HostSinkHandle) {
-        let waker = {
-            let mut core = self.core.lock().unwrap();
-
+        let waker = with_shared(&self.core, |core| {
             if let Some(sink_data) = core.sink_handles.get_mut(&sink) {
                 // Set the sink to ready and wake it up
                 sink_data.status = GuestSinkStatus::Ready;
@@ -127,7 +134,7 @@ impl GuestRuntime {
                 // No sink with this handle is available
                 None
             }
-        };
+        });
 
         // Wake up the future for later polling
         if let Some(waker) = waker {
@@ -139,9 +146,7 @@ impl GuestRuntime {
     /// Indicates that a sink could not be connected
     ///
     pub fn sink_connection_error(&self, HostSinkHandle(sink): HostSinkHandle, error: ConnectionError) {
-        let waker = {
-            let mut core = self.core.lock().unwrap();
-
+        let waker = with_shared(&self.core, |core| {
             if let Some(sink_data) = core.sink_handles.get_mut(&sink) {
                 // Set the sink to the error state
                 sink_data.status = GuestSinkStatus::ConnectionError(error);
@@ -150,7 +155,7 @@ impl GuestRuntime {
                 // No sink with this handle is available
                 None
             }
-        };
+        });
 
         // Wake up the future for later polling
         if let Some(waker) = waker {
@@ -162,9 +167,7 @@ impl GuestRuntime {
     /// Indicates that a message could not be sent on a sink
     ///
     pub fn sink_send_error(&self, HostSinkHandle(sink): HostSinkHandle, error: SceneSendError<Vec<u8>>) {
-        let waker = {
-            let mut core = self.core.lock().unwrap();
-
+        let waker = with_shared(&self.core, |core| {
             if let Some(sink_data) = core.sink_handles.get_mut(&sink) {
                 // Set the sink to the error state
                 sink_data.status = GuestSinkStatus::SendError(error);
@@ -173,7 +176,7 @@ impl GuestRuntime {
                 // No sink with this handle is available
                 None
             }
-        };
+        });
 
         // Wake up the future for later polling
         if let Some(waker) = waker {
@@ -186,25 +189,21 @@ impl GuestRuntime {
     ///
     pub fn send_stream(&self, stream_id: SerializationId, msg: Vec<u8>) {
         // Fetch the stream we're sending to, or create a new one (assuming that the stream will get created later on: we rely on the host not to send more data to a stream once it is woken up)
-        let stream = {
-            let mut core = self.core.lock().unwrap();
-
+        let stream = with_shared(&self.core, |core| {
             core.pending_streams.entry(stream_id)
-                .or_insert_with(|| share(GuestStreamCore{ 
+                .or_insert_with(|| share(GuestStreamCore { 
                     pending:    VecDeque::new(), 
                     waker:      None, 
                     closed:     false 
-                })))
+                }))
                 .clone()
-        };
+        });
 
         // Push data to it and wake it up
-        let waker = {
-            let mut core = stream.lock().unwrap();
-
+        let waker = with_shared(&stream, |core| {
             core.pending.push_back(msg);
             core.waker.take()
-        };
+        });
 
         if let Some(waker) = waker {
             waker.wake();
@@ -216,14 +215,12 @@ impl GuestRuntime {
     ///
     pub fn ready_stream(&self, stream_id: SerializationId) {
         // Add this stream ID to the list that's 'ready', and wake up anything that's waiting
-        let waker = {
-            let mut core = self.core.lock().unwrap();
-
+        let waker = with_shared(&self.core, |core| {
             core.ready_streams.insert(stream_id);
             core.when_ready.get_mut(&stream_id)
                 .map(|waker| waker.take())
                 .unwrap_or(None)
-        };
+        });
 
         if let Some(waker) = waker {
             waker.wake();
@@ -240,9 +237,7 @@ impl GuestRuntime {
         use core::mem;
 
         // Add this stream ID to the list that's 'ready', and wake up anything that's waiting
-        let waker = {
-            let mut core = self.core.lock().unwrap();
-
+        let waker = with_shared(&self.core, |core| {
             if let Some(guest_stream) = core.pending_streams.get_mut(&stream_id).cloned() {
                 // Remove the guest stream and then mark it as closed
                 core.pending_streams.remove(&stream_id);
@@ -259,7 +254,7 @@ impl GuestRuntime {
                     .map(|waker| waker.take())
                     .unwrap_or(None)
             }
-        };
+        });
 
         if let Some(waker) = waker {
             waker.wake();
@@ -312,7 +307,7 @@ impl GuestRuntime {
 
             let maybe_actions = if poll_immediately {
                 // The guest indicated it wanted an immediate callback without waiting (so we do so once all of the results have been processed)
-                Some(vec![])
+                Some(Vec::new())
             } else {
                 // The guest is idle, so we wait until some external action wakes it up
                 action_receiver.next().await
