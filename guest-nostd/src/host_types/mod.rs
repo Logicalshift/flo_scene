dca9c281ce7bb243d68cd2a74c188c2f398aeefc
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-29 20:26:24 +0000

    Rename to match the type name

--- guest-nostd/src/host_types/mod.rs
+++ guest-nostd/src/host_types/mod.rs
@@ -1,11 +1,11 @@
 mod filter_handle;
-mod scene_message;
+mod scene_guest_message;
 mod serialization_id;
 mod stream_target;
 mod subprogram_id;
 
 pub use filter_handle::*;
-pub use scene_message::*;
+pub use scene_guest_message::*;
 pub use serialization_id::*;
 pub use stream_target::*;
 pub use subprogram_id::*;
