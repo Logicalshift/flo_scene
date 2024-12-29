0f15aff53d8d8f44bc049b1f71db64e9615df497
Andrew Hunter <andrew@logicalshift.co.uk>
2024-12-29 18:31:36 +0000

    Enable UUID v4 if 'std' is picked

--- guest-nostd/src/imports.rs
+++ guest-nostd/src/imports.rs
@@ -9,6 +9,7 @@ mod uuid_impl {
         ///
         /// Externally-provided function that generates a UUID for use by this guest program
         ///
+        #[cfg(not(feature="std"))]
         fn scene_request_new_uuid(bytes: &mut [u8; 16]);
     }
 
@@ -21,6 +22,7 @@ mod uuid_impl {
     /// Uuid::new_v4 is not usually available for flo_scene wasm modules as it uses wbindgen to get random valeus and these modules 
     /// are not usually run in a javascript environment.
     ///
+    #[cfg(not(feature="std"))]
     #[inline]
     pub fn new_uuid() -> Uuid {
         let mut bytes = [0; 16];
@@ -28,6 +30,21 @@ mod uuid_impl {
         unsafe { scene_request_new_uuid(&mut bytes); }
 
         Uuid::from_bytes(bytes)
+    }
+
+    ///
+    /// Generates a new UUID
+    ///
+    /// For WASM support we need to use an external function for this, but in other environments we just use `Uuid::new_v4`.
+    /// Calling this function will choose the appropriate implementation for your environment. 
+    ///
+    /// Uuid::new_v4 is not usually available for flo_scene wasm modules as it uses wbindgen to get random valeus and these modules 
+    /// are not usually run in a javascript environment.
+    ///
+    #[cfg(feature="std")]
+    #[inline]
+    pub fn new_uuid() -> Uuid {
+        Uuid::new_v4()
     }
 }
 
