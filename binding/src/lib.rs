0f5af807eed90d5e3925c3ac466a165241111ec7
Andrew Hunter <andrew@logicalshift.co.uk>
2025-08-05 20:24:19 +0100

    Add a binding program to perform actions whenever a binding changes

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -1,6 +1,8 @@
+mod binding_program;
 mod binding_tracker;
 
 pub use binding_tracker::*;
+pub use binding_program::*;
 
 /// The version of flo_scene supported by this library
 pub use flo_scene as scene;
