3b48cbe6d051a1e977555e70cd078aaf5b6f6343
Andrew Hunter <andrew@logicalshift.co.uk>
2025-12-29 22:25:56 +0000

    Use wait for idle to try to avoid generating multiple timeouts

--- binding/tests/animation_tests.rs
+++ binding/tests/animation_tests.rs
@@ -47,10 +47,15 @@ fn linear_animation() {
                     let mut timeout = context.send(program_id).unwrap();
 
                     // Send 4 messages (time=0, time=1 frame, time=1 second, time=10 seconds)
+                    context.wait_for_idle(0).await;
                     timeout.send(TimeOut(timer_id, Duration::from_millis(0))).await.unwrap();
+                    context.wait_for_idle(0).await;
                     timeout.send(TimeOut(timer_id, Duration::from_millis(16))).await.unwrap();
+                    context.wait_for_idle(0).await;
                     timeout.send(TimeOut(timer_id, Duration::from_millis(1000))).await.unwrap();
+                    context.wait_for_idle(0).await;
                     timeout.send(TimeOut(timer_id, Duration::from_millis(10000))).await.unwrap();
+                    context.wait_for_idle(0).await;
                 }
 
                 _ => { }
