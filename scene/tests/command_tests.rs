d7e239a1c3dba7f23ed4393f79eaf1d360392314
Andrew Hunter <andrew@logicalshift.co.uk>
2026-03-23 22:33:42 +0000

    Run the test 1000 times in the same process instead of restarting it
    This doesn't appear to actually fail, though

--- scene/tests/command_tests.rs
+++ scene/tests/command_tests.rs
@@ -32,25 +32,28 @@ fn simple_command() {
 fn simple_command_x1000() {
     let scene = Scene::default();
 
+    // Create a test command that sends some usize values to its output
+    let test_command = FnCommand::<(), usize>::new(|_input, context| async move {
+        // Connect the usize output
+        let mut output = context.send::<usize>(()).unwrap();
+
+        // Send some output data
+        output.send(1).await.unwrap();
+        output.send(2).await.unwrap();
+        output.send(3).await.unwrap();
+        output.send(4).await.unwrap();
+    });
+
+    // Run the command using the test builder
+    let test_program = SubProgramId::new();
+
+    let mut test = TestBuilder::new();
+
     for _ in 0..1000 {
-        // Create a test command that sends some usize values to its output
-        let test_command = FnCommand::<(), usize>::new(|_input, context| async move {
-            // Connect the usize output
-            let mut output = context.send::<usize>(()).unwrap();
-
-            // Send some output data
-            output.send(1).await.unwrap();
-            output.send(2).await.unwrap();
-            output.send(3).await.unwrap();
-            output.send(4).await.unwrap();
-        });
-
-        // Run the command using the test builder
-        let test_program = SubProgramId::new();
-        TestBuilder::new()
-            .run_command(test_command.clone(), vec![], |output| if &output != &vec![1, 2, 3, 4] { Err(format!("Unexpected command output: {:?}", output)) } else { Ok(()) })
-            .run_in_scene_with_threads(&scene, test_program, 5);
+        test = test.run_command(test_command.clone(), vec![], |output| if &output != &vec![1, 2, 3, 4] { Err(format!("Unexpected command output: {:?}", output)) } else { Ok(()) });
     }
+
+    test.run_in_scene_with_threads(&scene, test_program, 5);
 }
 
 #[test]
