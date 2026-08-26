9fcda406f3f15e7646bd39c2d9b57cd498aa3309
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-26 22:42:49 +0100

    Add the unencrypted tcp socket subprogram as a separate function (so it's possible to launch one without having the scene available)

--- pipe/src/tcp_socket.rs
+++ pipe/src/tcp_socket.rs
@@ -27,28 +27,46 @@ where
     TInputStream:   'static + Send + Stream,
     TOutputMessage: 'static + Send,
 {
-    scene.add_subprogram(program_id, move |_input: InputStream<()>, context| async move {
-        // The listener requires an await to start, so we create it as part of the program
-        let listener = TcpListener::bind(address).await
-            .map_err(|tokio_err| ConnectionError::IoError(format!("{}", tokio_err)))
-            .unwrap();
-
-        // Add a socket runner subprogram. We don't use the address for anything, ie we accept all connections here
-        let listener = Desync::new(listener);
-
-        socket_listener_subprogram(context, move || 
-            listener.future_desync(|listener| async {
-                listener.accept().await
-                    .map(|(socket, _addr)| {
-                        socket.set_nodelay(true).ok();
-                        socket.into_split()
-                    })
-                    .map_err(|tokio_err| tokio_err.to_connection_error())
-            }.boxed()).map_ok_or_else(|_cancelled| Err(ConnectionError::Cancelled), |ok| ok),
-            create_input_messages,
-            create_output_messages).await;
-        }, 0);
+    scene.add_subprogram(program_id, move |_input: InputStream<()>, context| unencrypted_tcp_socket_subprogram(context, address, create_input_messages, create_output_messages), 0);
 
     // Success
     Ok(())
+}
+
+///
+/// Implements a sub-program that accepts unencrypted connections on a TCP socket.
+///
+/// The program will wait for subscribers (the `Subscribe` message) to the `SocketMessage<TInputStream::Item, TOutputStream::Item>`
+/// message. Typically, there's only one subscriber but in the event multiple are connected, they are informed of connections in
+/// a round-robin fashion.
+///
+pub async fn unencrypted_tcp_socket_subprogram<TInputStream, TOutputMessage>(
+        context:                SceneContext,
+        address:                impl 'static + Send + ToSocketAddrs, 
+        create_input_messages:  impl 'static + Send + Sync + Fn(BoxStream<'static, Vec<u8>>) -> TInputStream,
+        create_output_messages: impl 'static + Send + Sync + Fn(BoxStream<'static, TOutputMessage>) -> BoxStream<'static, Vec<u8>>
+    )
+where
+    TInputStream:   'static + Send + Stream,
+    TOutputMessage: 'static + Send,
+{
+    // The listener requires an await to start, so we create it as part of the program
+    let listener = TcpListener::bind(address).await
+        .map_err(|tokio_err| ConnectionError::IoError(format!("{}", tokio_err)))
+        .unwrap();
+
+    // Add a socket runner subprogram. We don't use the address for anything, ie we accept all connections here
+    let listener = Desync::new(listener);
+
+    socket_listener_subprogram(context, move || 
+        listener.future_desync(|listener| async {
+            listener.accept().await
+                .map(|(socket, _addr)| {
+                    socket.set_nodelay(true).ok();
+                    socket.into_split()
+                })
+                .map_err(|tokio_err| tokio_err.to_connection_error())
+        }.boxed()).map_ok_or_else(|_cancelled| Err(ConnectionError::Cancelled), |ok| ok),
+        create_input_messages,
+        create_output_messages).await;
 }
