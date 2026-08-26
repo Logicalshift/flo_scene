449af819c6700547f5cdf169637df13a3cb30dc0
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-26 23:13:41 +0100

    TCP socket can't use desync (as desync doesn't understand tokio)

--- pipe/src/tcp_socket.rs
+++ pipe/src/tcp_socket.rs
@@ -7,7 +7,7 @@ use futures::stream::{BoxStream};
 
 use tokio::net::{TcpListener, ToSocketAddrs};
 
-use ::desync::*;
+use std::sync::*;
 
 ///
 /// Starts a sub-program that accepts unencrypted connections on a TCP socket.
@@ -54,19 +54,24 @@ where
     let listener = TcpListener::bind(address).await
         .map_err(|tokio_err| ConnectionError::IoError(format!("{}", tokio_err)))
         .unwrap();
+    let listener = Arc::new(Mutex::new(Some(listener)));
 
     // Add a socket runner subprogram. We don't use the address for anything, ie we accept all connections here
-    let listener = Desync::new(listener);
+    socket_listener_subprogram(context, 
+        move ||  {
+            let listener        = Arc::clone(&listener);
+            let our_listener    = listener.lock().unwrap().take().unwrap();
 
-    socket_listener_subprogram(context, move || 
-        listener.future_desync(|listener| async {
-            listener.accept().await
-                .map(|(socket, _addr)| {
-                    socket.set_nodelay(true).ok();
-                    socket.into_split()
-                })
-                .map_err(|tokio_err| tokio_err.to_connection_error())
-        }.boxed()).map_ok_or_else(|_cancelled| Err(ConnectionError::Cancelled), |ok| ok),
+            async move {
+                let connection = our_listener.accept().await
+                    .map(|(socket, _addr)| socket.into_split())
+                    .map_err(|tokio_err| tokio_err.to_connection_error());
+
+                *listener.lock().unwrap() = Some(our_listener);
+
+                connection
+            }
+        },
         create_input_messages,
         create_output_messages).await;
 }
