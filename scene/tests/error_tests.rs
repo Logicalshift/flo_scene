c36b8f5b69502600e20c2b1203246cdde3c85121
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-30 18:43:36 +0100

    Add a test program for our error program

---
+++ scene/tests/error_tests.rs
@@ -0,0 +1,29 @@
+//!
+//! The error program provides a standard way to report errors (non-fatal or fatal)
+//! to the scene, and a way for other programs to listen to and potentially respond
+//! to errors.
+//!
+
+use flo_scene::*;
+use flo_scene::programs::*;
+
+#[test]
+fn notify_on_error() {
+    let scene           = Scene::default();
+    let test_program    = SubProgramId::new();
+
+    let error_program   = SubProgramId::new();
+
+    scene.add_subprogram(error_program, 
+        move |_: InputStream<()>, context| async move {
+            // Subscribe the test program to receive errors
+            context.send_message(ErrorSubscription::SubscribeToAll(test_program.into())).or_fail().await;
+
+            // Try an 'or error' message
+            async { Result::<(), String>::Err(format!("Goodbye, world")) }.with_report().await.ok();
+        }, 0);
+
+    TestBuilder::new()
+        .expect_message_matching(Error::Error { source: error_program, message: "Goodbye, world".into() }, "Was expecting an error message from our subprogram")
+        .run_in_scene_with_threads(&scene, test_program, 5);
+}
