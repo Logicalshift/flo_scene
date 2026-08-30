39d71aabb4ea2b762bb426e34f457fe914fafb3a
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-31 00:38:08 +0100

    Add tests for panicking: failing for some reason when there's only a single thread

--- scene/tests/error_tests.rs
+++ scene/tests/error_tests.rs
@@ -8,7 +8,13 @@ use flo_scene::*;
 use flo_scene::programs::*;
 
 use futures::prelude::*;
+use futures::executor;
+use futures::future::{select};
+use futures_timer::*;
 use serde::*;
+
+use std::panic::{catch_unwind, AssertUnwindSafe};
+use std::time::{Duration};
 
 #[test]
 fn notify_on_error() {
@@ -85,4 +91,82 @@ fn notify_on_failure() {
         // .expect_message_matching(TestMessage(Error::Failure { source: error_program, message: "\"Goodbye, world\"".into() }), "Was expecting an error message from our subprogram")
         .expect_stopped_scene()
         .run_in_scene_with_threads(&scene, test_program, 5);
+}
+
+#[test]
+fn panic_single_threaded() {
+    let scene           = Scene::default();
+    let panic_program   = SubProgramId::new();
+    let other_program   = SubProgramId::new();
+
+    // This subprogram panics as soon as it's polled
+    scene.add_subprogram(panic_program,
+        move |_: InputStream<()>, _context| async move {
+            panic!("Goodbye, world");
+        }, 0);
+
+    // This subprogram would run forever: if the scene doesn't stop as a result of the panic, this test will time out instead of panicking
+    scene.add_subprogram(other_program,
+        move |input: InputStream<()>, _context| async move {
+            let mut input = input;
+            while let Some(_) = input.next().await { }
+        }, 0);
+
+    let mut timed_out  = false;
+    let panic_result    = catch_unwind(AssertUnwindSafe(|| {
+        executor::block_on(select(async {
+            scene.run_scene().await;
+        }.boxed(), async {
+            Delay::new(Duration::from_millis(5000)).await;
+            timed_out = true;
+        }.boxed()));
+    }));
+
+    assert!(!timed_out, "Scene did not stop after a subprogram panicked");
+    assert!(panic_result.is_err(), "run_scene() should resume panicking once the scene has stopped");
+
+    let panic_payload   = panic_result.unwrap_err();
+    let panic_message   = panic_payload.downcast_ref::<String>().cloned()
+        .or_else(|| panic_payload.downcast_ref::<&str>().map(|msg| msg.to_string()));
+
+    assert_eq!(panic_message.as_deref(), Some("Goodbye, world"), "Expected the original panic message to be resumed");
+}
+
+#[test]
+fn panic_multi_threaded() {
+    let scene           = Scene::default();
+    let panic_program   = SubProgramId::new();
+    let other_program   = SubProgramId::new();
+
+    // This subprogram panics as soon as it's polled
+    scene.add_subprogram(panic_program,
+        move |_: InputStream<()>, _context| async move {
+            panic!("Goodbye, world (with threads)");
+        }, 0);
+
+    // This subprogram would run forever: if the scene doesn't stop as a result of the panic, this test will time out instead of panicking
+    scene.add_subprogram(other_program,
+        move |input: InputStream<()>, _context| async move {
+            let mut input = input;
+            while let Some(_) = input.next().await { }
+        }, 0);
+
+    let mut timed_out  = false;
+    let panic_result    = catch_unwind(AssertUnwindSafe(|| {
+        executor::block_on(select(async {
+            scene.run_scene_with_threads(4).await;
+        }.boxed(), async {
+            Delay::new(Duration::from_millis(5000)).await;
+            timed_out = true;
+        }.boxed()));
+    }));
+
+    assert!(!timed_out, "Scene did not stop after a subprogram panicked");
+    assert!(panic_result.is_err(), "run_scene_with_threads() should resume panicking once the scene has stopped");
+
+    let panic_payload   = panic_result.unwrap_err();
+    let panic_message   = panic_payload.downcast_ref::<String>().cloned()
+        .or_else(|| panic_payload.downcast_ref::<&str>().map(|msg| msg.to_string()));
+
+    assert_eq!(panic_message.as_deref(), Some("Goodbye, world (with threads)"), "Expected the original panic message to be resumed");
 }
