d2ea7c5232fe86005a26226a8185a3318e06b199
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-30 21:43:41 +0100

    The test builder intercepts the error messages if we await them directly, so send them via a relay
    ... this still doesn't work, oddly we're getting the 'finished before tests completed' here. I think probably because the actual test program is getting shut down before it can deliver its 'end of tests' message

--- scene/tests/error_tests.rs
+++ scene/tests/error_tests.rs
@@ -7,6 +7,9 @@
 use flo_scene::*;
 use flo_scene::programs::*;
 
+use futures::prelude::*;
+use serde::*;
+
 #[test]
 fn notify_on_error() {
     let scene           = Scene::default();
@@ -35,19 +38,34 @@ fn notify_on_failure() {
     let test_program    = SubProgramId::new();
 
     let error_program   = SubProgramId::new();
+    let relay_program   = SubProgramId::new();
+
+    #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    struct TestMessage(Error);
+
+    impl SceneMessage for TestMessage { }
 
     // or_fail() shuts the scene down but waits for it to become idle, so we can still receive the message that the error occurred
     scene.add_subprogram(error_program, 
         move |_: InputStream<()>, context| async move {
-            // Subscribe the test program to receive errors
-            context.send_message(ErrorSubscription::SubscribeToAll(test_program.into())).or_fail().await;
+            // Subscribe the relay program to receive errors
+            context.send_message(ErrorSubscription::SubscribeToAll(relay_program.into())).or_fail().await;
 
             // Try an 'or error' message
             async { Result::<(), String>::Err(format!("Goodbye, world")) }.or_fail().await;
         }, 0);
 
+    // Relay program sends errors to the test builder
+    scene.add_subprogram(relay_program,
+        move |input, context| async move {
+            let mut input = input;
+            while let Some(error) = input.next().await {
+                context.send_message(TestMessage(error)).await.ok();
+            }
+        }, 5);
+
     TestBuilder::new()
-        .expect_message_matching(Error::Failure { source: error_program, message: "\"Goodbye, world\"".into() }, "Was expecting an error message from our subprogram")
+        .expect_message_matching(TestMessage(Error::Failure { source: error_program, message: "\"Goodbye, world\"".into() }), "Was expecting an error message from our subprogram")
         .expect_stopped_scene()
         .run_in_scene_with_threads(&scene, test_program, 5);
 }
