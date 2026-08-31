1a32b2a6a61345e8bb3e4876bd7c6dbc8cd18c44
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-31 22:03:17 +0100

    Add the name tag to the list subprograms response

--- pipe/src/standard_json_commands/list_subprograms.rs
+++ pipe/src/standard_json_commands/list_subprograms.rs
@@ -7,6 +7,9 @@ use flo_scene::programs::*;
 use futures::prelude::*;
 use serde::*;
 
+use std::borrow::*;
+use std::collections::*;
+
 ///
 /// A response to a list subprograms request
 ///
@@ -14,6 +17,9 @@ use serde::*;
 pub struct ListSubprogramsResponse {
     /// The ID of this subprogram
     pub id: SubProgramId,
+
+    /// Name for this program
+    pub name: Option<Cow<'static, str>>,
 
     /// The type_name of the input stream for this subprogram (as a Rust type: note that this can vary and is informational only)
     pub rust_type_name: String,
@@ -27,6 +33,17 @@ impl SceneMessage for ListSubprogramsResponse {
     fn message_type_name() -> String { "flo_scene_pipe::ListSubprogramsResponse".into() }
 }
 
+impl Default for ListSubprogramsResponse {
+    fn default() -> Self {
+        ListSubprogramsResponse {
+            id:             SubProgramId::called("Unknown"),
+            name:           None,
+            rust_type_name: "Unknown".into(),
+            serialized_type_name: None,
+        }
+    }
+}
+
 ///
 /// The `list_subprograms` command, which lists the subprograms in the current scene
 ///
@@ -36,25 +53,33 @@ pub fn command_list_subprograms(_input: serde_json::Value, context: SceneContext
         match context.spawn_query(ReadCommand::default(), Query::<SceneUpdate>::with_no_target(), *SCENE_CONTROL_PROGRAM) {
             Ok(updates) => {
                 let mut updates     = updates;
-                let mut responses   = vec![];
+                let mut subprograms = HashMap::new();
 
                 // Read the responses from the updates
                 while let Some(update) = updates.next().await {
                     match update {
                         SceneUpdate::Started(program_id, input_stream_id) => {
-                            // Create a response for every program that's running
-                            responses.push(ListSubprogramsResponse { 
-                                id:                     program_id, 
-                                rust_type_name:         input_stream_id.message_type_name(), 
-                                serialized_type_name:   input_stream_id.serialization_type_name(), 
-                            })
+                            // Fill in the details about this program
+                            let details = subprograms.entry(program_id).or_insert_with(|| ListSubprogramsResponse::default());
+
+                            details.id                      = program_id;
+                            details.rust_type_name          = input_stream_id.message_type_name();
+                            details.serialized_type_name    = input_stream_id.serialization_type_name();
+                        }
+
+                        SceneUpdate::Tagged(program_id, SceneProgramTag::Name(name)) => {
+                            // Add the name for this program
+                            let details = subprograms.entry(program_id).or_insert_with(|| ListSubprogramsResponse::default());
+
+                            details.id      = program_id;
+                            details.name    = details.name.take().or(Some(name));
                         }
 
                         _ => { }
                     }
                 }
 
-                CommandResponseData::Data(responses)
+                CommandResponseData::Data(subprograms.into_iter().map(|(_, response)| response).collect())
             }
 
             Err(error) => {
