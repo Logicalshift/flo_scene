6cd21a2741f042c991cf0db487e2dc84de202e3e
Andrew Hunter <andrew@logicalshift.co.uk>
2026-08-31 17:32:34 +0100

    And a 'with_name' variant
    ... this also passses.

--- scene/tests/log_tests.rs
+++ scene/tests/log_tests.rs
@@ -103,3 +103,25 @@ fn log_failure_immediately() {
         .expect_stopped_scene()
         .run_in_scene_with_threads(&scene, test_subprogram, 10);
 }
+
+#[test]
+fn log_failure_immediately_with_name() {
+    let scene = Scene::default();
+
+    let test_subprogram = SubProgramId::called("test");
+    let log_program     = SubProgramId::called("test log");
+
+    scene.add_subprogram(log_program, 
+        |_: InputStream<()>, context| async move {
+            context.i_am("Test program");
+            context.wait_for_idle(10).await;
+
+            async { Result::<(), _>::Err("Oops") }.or_fail().await;
+        }, 
+        5);
+
+    TestBuilder::new()
+        .expect_message_matching(ErrorOutput::Line("\x1b[1;91m!!Test program            \x1b[0m | \"Oops\"".into()), "Log message did not match")
+        .expect_stopped_scene()
+        .run_in_scene_with_threads(&scene, test_subprogram, 10);
+}
