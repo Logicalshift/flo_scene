7adf667152bece3050c1047618d4a06d01f6a37d
Andrew Hunter <andrew@logicalshift.co.uk>
2026-09-13 11:44:09 +0100

    Wait for the left/right input core to become idle before continuing the main program after queueing a message for them

--- scene/src/host/compositor/extra_message_handler.rs
+++ scene/src/host/compositor/extra_message_handler.rs
@@ -6,6 +6,7 @@ use crate::host::subprogram_id::*;
 
 use futures::prelude::*;
 use futures::future::{BoxFuture};
+use futures::task::{Waker};
 
 use std::sync::*;
 
@@ -34,6 +35,63 @@ impl<TMessage> Clone for MessageForwarder<TMessage> {
             core:       self.core.clone(),
         }
     }
+}
+
+///
+/// Creates a future that wakes up the specified waker every time the input core becomes idle
+///
+fn with_wake_when_idle<'a, TMessage>(future: impl 'a + Send + Future<Output=()>, input_core: Arc<Mutex<InputStreamCore<TMessage>>>, when_idle: Arc<Mutex<Option<Waker>>>) -> impl 'a + Send + Future<Output=()> 
+where 
+    TMessage: SceneMessage,
+{
+    let mut future = Box::pin(future);
+
+    future::poll_fn(move |context| {
+        // Poll the future
+        let result = future.poll_unpin(context);
+
+        // Check if the stream is idle
+        let waker = {
+            let input_core = input_core.lock().unwrap();
+
+            if input_core.is_idle() {
+                when_idle.lock().unwrap().take()                
+            } else {
+                None
+            }
+        };
+
+        // If it is idle and there's a waker, then awaken the waker
+        if let Some(waker) = waker {
+            waker.wake();
+        }
+
+        result
+    })
+}
+
+///
+/// Creates a future that waits for the specified input core to become idle (signalled via the 'when idle' waker)
+///
+fn on_idle<TMessage>(input_core: Arc<Mutex<InputStreamCore<TMessage>>>, when_idle: Arc<Mutex<Option<Waker>>>) -> impl 'static + Send + Future<Output=()> 
+where 
+    TMessage: SceneMessage,
+{
+    future::poll_fn(move |context| {
+        use futures::task::{Poll};
+
+        let input_core = input_core.lock().unwrap();
+        if input_core.is_idle() {
+            // Finish when the core is idle
+            Poll::Ready(())
+        } else {
+            // Wake up when the core becomes idle
+            (*when_idle.lock().unwrap()) = Some(context.waker().clone());
+
+            // Sleep until that happens
+            Poll::Pending
+        }
+    })
 }
 
 impl<TFn, TMessage, TExtraMessage, TOldFuture> WithExtraMessageHandler<TMessage, TExtraMessage, TOldFuture> for TFn
@@ -72,25 +130,39 @@ where
             let left    = (self)(left, context.clone());
             let right   = (extra_message)(right, context.clone(), left_forwarder.clone());
 
+            // Change the futures into ones that reawaken the message consumer when they're idle
+            let wake_when_idle  = Arc::new(Mutex::new(None));
+            let left            = with_wake_when_idle(left, left_core.clone(), wake_when_idle.clone());
+            let right           = with_wake_when_idle(right, right_core.clone(), wake_when_idle.clone());
+
             // Listen to all the futures, forwarding each message as it's received to the appropriate inbox for the two streams
             let main_core_1 = input.core();
             let main_core_2 = input.core();
             future::join3(
                 async move {
+                    // Wait for the left future
                     left.await;
+
+                    // When the left future finishes, stop the main input (which will cause the whole program to stop eventually)
                     main_core_1.lock().unwrap().close();
                 },
                 async move {
+                    // Wait for the right future
                     right.await;
+
+                    // Stop the whole program when the future stops
                     main_core_2.lock().unwrap().close();
                 },
                 async move {
                     let mut input = input.messages_with_sources();
 
+                    // Forward messages to either of the two futures
                     while let Some((source, msg)) = input.next().await {
+                        // After forwarding a message, we wait for the relevant input stream to become idle again before sending the next message (this stops us from processing two messages
+                        // in parallel, and also keeps the overall subprogram in a 'busy' state)
                         match msg {
-                            EitherMessage::Left(msg)    => left_forwarder.forward_with_sender(msg, source).await,
-                            EitherMessage::Right(msg)   => right_forwarder.forward_with_sender(msg, source).await,
+                            EitherMessage::Left(msg)    => { left_forwarder.forward_with_sender(msg, source).await; on_idle(left_core.clone(), wake_when_idle.clone()).await; },
+                            EitherMessage::Right(msg)   => { right_forwarder.forward_with_sender(msg, source).await; on_idle(right_core.clone(), wake_when_idle.clone()).await; },
                         }
                     }
 
